d17e770d56a927be18262ccfd4b1721b7739045f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-21 11:51:19 -0700

    fix: update s3 path

--- aws_eticloud-scratch-c_us-east-2_eks_eks-butterscotch-1_main.tf
+++ aws_eticloud-scratch-c_us-east-2_eks_eks-butterscotch-1_main.tf
@@ -14,9 +14,8 @@ module "eks-dev-3" {
 terraform {
   backend "s3" {
     bucket  = "eticloud-tf-state-nonprod"
-    key     = "terraform-state/eticloud-scratch-c/eks/us-east-2/eks-butterscotch-1.tfstate"
+    key     = "terraform-state/aws/eticloud-scratch-c/us-east-2/eks/eks-butterscotch-1.tfstate"
     region  = "us-east-2"
-    profile = "eticloud"
   }
 }
 
