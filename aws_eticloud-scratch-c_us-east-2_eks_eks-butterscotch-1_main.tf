afb482a58e13c2e6eda948af72042f3d362ee578
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-21 11:47:17 -0700

    fix: update path

---
+++ aws_eticloud-scratch-c_us-east-2_eks_eks-butterscotch-1_main.tf
@@ -0,0 +1,48 @@
+module "eks-dev-3" {
+  source = "../../../../modules/eks"
+}
+
+################################################################################
+# Account
+################################################################################
+
+
+################################################################################
+# Account
+################################################################################
+
+terraform {
+  backend "s3" {
+    bucket  = "eticloud-tf-state-nonprod"
+    key     = "terraform-state/eticloud-scratch-c/eks/us-east-2/eks-butterscotch-1.tfstate"
+    region  = "us-east-2"
+    profile = "eticloud"
+  }
+}
+
+provider "aws" {
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = var.region
+  default_tags {
+    tags = {
+      ApplicationName    = var.application_name
+      CiscoMailAlias     = var.cisco_mail_alias
+      DataClassification = var.data_classification
+      DataTaxonomy       = var.data_taxonomy
+      Environment        = var.environment
+      ResourceOwner      = var.resource_owner
+    }
+  }
+}
+
+provider "vault" {
+  alias     = "eticcprod"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud/eticcprod"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  path     = "secret/eticcprod/infra/eticloud-scratch-c/aws"
+  provider = vault.eticcprod
+}
