3ede2d3b7353bc51364f7bdcb9b92ee0675b3838
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 16:41:01 -0700

    chore: add redis simple terraform resource

--- aws_rosey-test_eu-west-1_eks_rosey-dev-euw1-1_redis-elasticache.tf
+++ aws_rosey-test_eu-west-1_eks_rosey-dev-euw1-1_redis-elasticache.tf
@@ -1,20 +1,3 @@
-provider "vault" {
-  alias     = "eticloud"
-  address   = "https://keeper.cisco.com"
-  namespace = "eticloud"
-}
-
-data "vault_generic_secret" "aws_infra_credential" {
-  provider    = vault.eticloud
-  path        = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
-}
-
-provider "aws" {
-  alias      = "target"
-  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region     = local.region
-}
 resource "aws_elasticache_replication_group" "rosey-dev-euw1-1" {
   replication_group_id       = "rosey-dev-euw1-1"
   description                = "Redis cluster for rosey-dev-euw1-1 ElastiCache"
