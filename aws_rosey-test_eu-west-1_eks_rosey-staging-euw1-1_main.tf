172cc07d1980706e555854c68ebf4e7e07d2879f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 16:46:08 -0700

    Merge branch 'main' into redis-dev

--- aws_rosey-test_eu-west-1_eks_rosey-staging-euw1-1_main.tf
+++ aws_rosey-test_eu-west-1_eks_rosey-staging-euw1-1_main.tf
@@ -3,7 +3,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket  = "eticloud-tf-state-nonprod"                                                   # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key     = "terraform-state/aws/rosey-test/eu-west-1/eks/rosey-dev-euw1-1.tfstate"       # UPDATE ME.
+    key     = "terraform-state/aws/rosey-test/eu-west-1/eks/rosey-staging-euw1-1.tfstate"       # UPDATE ME.
     # This is the region where the backend S3 bucket is located.
     region  = "us-east-2"                                                                   # DO NOT CHANGE.
 
@@ -11,18 +11,18 @@ terraform {
 }
 
 locals {
-  name = "rosey-dev-euw1-1"
+  name = "rosey-staging-euw1-1"
   region = "eu-west-1"
   aws_account_name = "rosey-test"
 }
 
 module "eks_all_in_one" {
-  source            = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest" # Based on v0.0.10
+  source            = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest" # based on v0.1.0
 
   name              = local.name              # EKS cluster name
   region            = local.region            # AWS provider region
   aws_account_name  = local.aws_account_name  # AWS account name
-  cidr              = "10.0.0.0/16"           # VPC CIDR
+  cidr              = "10.1.0.0/16"           # VPC CIDR
   cluster_version   = "1.28"                  # EKS cluster version
 
   # EKS Managed Private Node Group
