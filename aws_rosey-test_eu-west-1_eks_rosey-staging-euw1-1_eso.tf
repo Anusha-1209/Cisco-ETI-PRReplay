0b9fe6daaff4a230b6832003f99ec90ddc62280d
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 16:26:49 -0700

    chore: adding required variable

--- aws_rosey-test_eu-west-1_eks_rosey-staging-euw1-1_eso.tf
+++ aws_rosey-test_eu-west-1_eks_rosey-staging-euw1-1_eso.tf
@@ -2,4 +2,5 @@ module "eso_eticloud" {
   source               = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=matunger-patch-1"
   cluster_name         = "rosey-staging-euw1-1"
   environment          = "staging"
+  venture_name         = "rosey"
 }
