a993662d438d8b8462d5c86868b7d17a7a57a3be
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 09:29:12 -0700

    fix: minor nit

--- aws_rosey-test_eu-west-1_eks_rosey-dev-euw1-1_main.tf
+++ aws_rosey-test_eu-west-1_eks_rosey-dev-euw1-1_main.tf
@@ -3,7 +3,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket  = "eticloud-tf-state-nonprod"                                                   # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key     = "terraform-state/aws/rosey-test/eu-west-1/eks/rosey-dev-euw1-1.tfstate" # UPDATE ME.
+    key     = "terraform-state/aws/rosey-test/eu-west-1/eks/rosey-dev-euw1-1.tfstate"       # UPDATE ME.
     # This is the region where the backend S3 bucket is located.
     region  = "us-east-2"                                                                   # DO NOT CHANGE.
 
