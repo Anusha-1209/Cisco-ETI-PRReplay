5bc27728f93df3e865b0dc6577d0075120102e40
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 16:43:47 -0700

    chore: add redis

--- aws_rosey-test_eu-west-1_redis_redis-elasticache.tf
+++ aws_rosey-test_eu-west-1_redis_redis-elasticache.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "s3" {
+    # This is the name of the backend S3 bucket.
+    bucket  = "eticloud-tf-state-nonprod"                                                   # UPDATE ME.
+    # This is the path to the Terraform state file in the backend S3 bucket.
+    key     = "terraform-state/aws/rosey-test/eu-west-1/redis/rosey-dev-euw1-1.tfstate"       # UPDATE ME.
+    # This is the region where the backend S3 bucket is located.
+    region  = "us-east-2"                                                                   # DO NOT CHANGE.
+
+  }
+}
 locals {
   name = "rosey-dev-euw1-1"
   region = "eu-west-1"
