f0aa955dd49a05d0552c97ff32e84ab9930ba3a3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 09:24:05 -0700

    fix: update CIDR
    Co-authored-by: Sri Aradhyula <sraradhy@cisco.com>

--- aws_rosey-test_eu-west-1_eks_rosey-dev-euw1-1_main.tf
+++ aws_rosey-test_eu-west-1_eks_rosey-dev-euw1-1_main.tf
@@ -15,7 +15,7 @@ module "eks_all_in_one" {
   name              = "rosey-dev-euw1-1"        # EKS cluster name
   region            = "eu-west-1"             # AWS provider region
   aws_account_name  = "rosey-test"    # AWS account name
-  cidr              = "10.59.0.0/16"          # VPC CIDR
+  cidr              = "10.1.0.0/16"          # VPC CIDR
   cluster_version   = "1.28"                  # EKS cluster version
 
   # EKS Managed Private Node Group
