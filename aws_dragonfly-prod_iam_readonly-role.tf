3bbe5b993c3fcdbb2175641091cc013af95be98f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 16:27:47 -0700

    chore: deploy dragonfly readonly role

---
+++ aws_dragonfly-prod_iam_readonly-role.tf
@@ -0,0 +1,24 @@
+resource "aws_iam_role" "readonly" {
+  name = "readonly"
+  assume_role_policy = file("policies/sso_assume_role_policy.json")
+  tags = {
+    Name = "readonly"
+  }
+}
+
+
+# resource "aws_iam_policy" "readonly-policy" {
+#   name        = "read-only-policy"
+#   path        = "/"
+#   description = "Read-Only SSO IAM role access"
+#   policy = file("policies/readonly_policy.json")
+# }
+
+
+
+###### SSO Access #######
+
+resource "aws_iam_role_policy_attachment" "readonly-policy-attachment" {
+  role       = aws_iam_role.readonly.name
+  policy_arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
