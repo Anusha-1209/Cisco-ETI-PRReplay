1dd63e5dbc020ea9b7998485e448e65d3dac8fe1
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 11:25:09 -0700

    chore: refine the tf fmt

--- aws_eticloud-preproduction_us-east-2_eks_eks-staging-1_eso.tf
+++ aws_eticloud-preproduction_us-east-2_eks_eks-staging-1_eso.tf
@@ -5,8 +5,8 @@ provider "vault" {
 }
 
 data "vault_generic_secret" "aws_infra_credential" {
-  provider    = vault.eticloud
-  path        = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
+  provider = vault.eticloud
+  path     = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
 }
 
 provider "aws" {
@@ -16,30 +16,30 @@ provider "aws" {
 }
 
 data "aws_eks_cluster" "cluster" {
-  depends_on = [ module.eks_all_in_one ]
-  name      = local.name
+  depends_on = [module.eks_all_in_one]
+  name       = local.name
 }
 
 data "vault_generic_secret" "cluster_certificate" {
-  depends_on = [ module.eks_all_in_one ]
-  provider = vault.eticloud
-  path = "secret/infra/eks/${local.name}/certificate"
+  depends_on = [module.eks_all_in_one]
+  provider   = vault.eticloud
+  path       = "secret/infra/eks/${local.name}/certificate"
 }
 
 module "eso_eticloud" {
-  source               = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=0.0.2"
+  source = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=0.0.2"
 
-  cluster_name         = local.name
-  vault_namespace      = "eticloud"
-  kubernetes_host      = data.aws_eks_cluster.cluster.endpoint
-  kubernetes_ca        = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
-  policies             = ["external-secrets-staging"]
+  cluster_name    = local.name
+  vault_namespace = "eticloud"
+  kubernetes_host = data.aws_eks_cluster.cluster.endpoint
+  kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
+  policies        = ["external-secrets-staging"]
 }
 module "eso_eticloud_apps_websites" {
-  source               = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=0.0.2"
-  cluster_name         = local.name
-  vault_namespace      = "eticloud/apps/websites"
-  kubernetes_host      = data.aws_eks_cluster.cluster.endpoint
-  kubernetes_ca        = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
-  policies             = ["external-secrets-staging-${local.venture_name}"]
+  source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=0.0.2"
+  cluster_name    = local.name
+  vault_namespace = "eticloud/apps/websites"
+  kubernetes_host = data.aws_eks_cluster.cluster.endpoint
+  kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
+  policies        = ["external-secrets-staging-${local.venture_name}"]
 }
