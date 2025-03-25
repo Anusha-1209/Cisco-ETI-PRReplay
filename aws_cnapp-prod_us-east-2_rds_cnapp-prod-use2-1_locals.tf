800a56e8826ef65fb94d84c1d441c3a9bd7aa2e0
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 11:31:21 -0700

    fix: Forgot to save the locals and remove the old code

--- aws_cnapp-prod_us-east-2_rds_cnapp-prod-use2-1_locals.tf
+++
@@ -1,7 +0,0 @@
-
-locals {
-  name  = "cnapp-prod-use2-1"
-  vpc_name = "rds-cnapp-prod-use2-1"
-  aws_account_name = "cnapp-prod"
-  rds_name  = "rds-cnapp-prod-use2-1"
-}
--- aws_cnapp-prod_us-east-2_rds_unified-agent-prod-use2-1_locals.tf
+++ aws_cnapp-prod_us-east-2_rds_unified-agent-prod-use2-1_locals.tf
@@ -1,6 +1,6 @@
 
 locals {
-  vpc_name = "cnapp-prod-euc1-data"
+  vpc_name = "cnapp-prod-use2-data"
   aws_account_name = "cnapp-prod"
-  rds_name  = "unified-agent-prod-euc1-1"
+  rds_name  = "unified-agent-prod-use2-1"
 }
