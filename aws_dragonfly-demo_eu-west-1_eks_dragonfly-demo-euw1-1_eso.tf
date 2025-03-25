d418eccc4e5e7c0703dca3371ce78bb326ce8520
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 14:19:44 -0700

    chore: rollback

--- aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_eso.tf
+++ aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_eso.tf
@@ -10,12 +10,15 @@ data "vault_generic_secret" "aws_infra_credential" {
 }
 
 provider "aws" {
+  alias      = "target"
   access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
   region     = local.region
 }
+
 data "aws_eks_cluster" "cluster" {
   depends_on = [module.eks_all_in_one]
+  provider   = aws.target
   name       = local.name
 }
 
