3b0c26f64551b170c03ada4aabdc434eacf673a2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 21:45:44 -0700

    addressing David's comments

--- aws_eticloud-scratch-c_eu-west-1_eks_eks-imangass-test_eso.tf
+++ aws_eticloud-scratch-c_eu-west-1_eks_eks-imangass-test_eso.tf
@@ -10,20 +10,17 @@ data "vault_generic_secret" "aws_infra_credential" {
 }
 
 provider "aws" {
-  alias      = "target"
   access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
   region     = local.region
 }
 
 data "aws_eks_cluster" "cluster" {
-  provider  = aws.target
   name      = local.name
   depends_on = [ module.eks_all_in_one ]
 }
 
 data "vault_generic_secret" "cluster_certificate" {
-  provider = vault.eticloud
   path = "secret/infra/eks/${local.name}/certificate"
   depends_on = [ module.eks_all_in_one ]
 }
