9642bcdcbdb381796a2527b484919dd3aa749c33
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 12:42:20 -0700

    chore: remove duplicated provider config block

--- aws_dragonfly-demo_iam_main.tf
+++ aws_dragonfly-demo_iam_main.tf
@@ -44,27 +44,6 @@ locals {
     }
   }
 }
-
-################################################################################
-# Provider configuration
-################################################################################
-provider "vault" {
-  alias     = "eticloud"
-  address   = "https://keeper.cisco.com"
-  namespace = "eticloud"
-}
-
-data "vault_generic_secret" "aws_infra_credential" {
-  provider    = vault.eticloud
-  path        = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
-}
-
-provider "aws" {
-  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region     = "eu-west-1"
-}
-
 module "aws_iam" {
   # EKS cluster partially created as of Jan 29 2024
   source = "git::https://github.com/cisco-eti/sre-tf-module-aws-iam.git?ref=iam-management"
