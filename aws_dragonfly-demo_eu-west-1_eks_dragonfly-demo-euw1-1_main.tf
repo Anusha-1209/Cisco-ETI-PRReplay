d5a253a5e132f8cc8fdf778c3597228ac1057131
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 14:11:17 -0700

    chore: add regiosn

--- aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_main.tf
+++ aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_main.tf
@@ -15,6 +15,24 @@ locals {
   aws_account_name = "dragonfly-demo"
 }
 
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
+}
+
+provider "aws" {
+  alias      = "target"
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = local.region
+}
+
 module "eks_all_in_one" {
   # EKS cluster partially created as of Jan 15 2024
   source = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
