9dd4dab64371dc6111ed4ca806178286ded02f66
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 17:49:53 -0700

    fix: S3 bucket region and wrong naming

--- aws_dragonfly-prod_eu-central-1_msk_dragonfly-msk-prod-eu1_backend.tf
+++ aws_dragonfly-prod_eu-central-1_msk_dragonfly-msk-prod-eu1_backend.tf
@@ -2,6 +2,6 @@ terraform {
   backend "s3" {
     bucket = "eticloud-tf-state-prod"
     key    = "terraform-state/aws-dragonfly-production/msk/eu-central-1/dragonfly-msk-prod-eu1.tfstate"
-    region = "eu-central-1"
+    region = "us-east-2"
   }
 }
