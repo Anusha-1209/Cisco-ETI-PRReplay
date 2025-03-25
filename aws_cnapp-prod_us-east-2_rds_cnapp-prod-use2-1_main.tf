2ea6584012e9a56db4678d635985052fdb4298d2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 22:39:01 -0700

    feat: fix rds repository

--- aws_cnapp-prod_us-east-2_rds_cnapp-prod-use2-1_main.tf
+++ aws_cnapp-prod_us-east-2_rds_cnapp-prod-use2-1_main.tf
@@ -1,5 +1,5 @@
 module "rds" {
-  source            = "git::https://wwwin-github.cisco.com/eti/sre-tf-module-aws-aurora-postgres?ref=1.1.0"
+  source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-aurora-postgres?ref=1.1.0"
   vpc_name          = local.vpc_name
   database_name     = "cnapp_k8s_service"
   db_instance_type  = "db.r5.xlarge"
