9575e3ac72a7f3f27ddf1004f6383927b00b629b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 15:11:14 -0700

    chore: Creating the RDS vowel-dev-1-knowledge-base

---
+++ aws_vowel-genai-dev_us-east-2_rds_vowel-dev-1-knowledge-base_locals.tf
@@ -0,0 +1,7 @@
+
+locals {
+  vpc_name = "rds-vowel-dev-1-vpc"
+  cluster_vpc_name = "vowel-dev-1-knowledge-base"
+  aws_account_name = "vowel-genai-dev"
+  rds_name  = "vowel-dev-1-knowledge-base"
+}
