5a78b60ac1b57c630f5aa59b53dd4ca2460857c8
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 14:13:49 -0700

    chore: add regiosn

--- aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_main.tf
+++ aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_main.tf
@@ -27,7 +27,7 @@ data "vault_generic_secret" "aws_infra_credential" {
 }
 
 provider "aws" {
-  alias      = "target"
+  alias      = "eticloud"
   access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
   region     = local.region
