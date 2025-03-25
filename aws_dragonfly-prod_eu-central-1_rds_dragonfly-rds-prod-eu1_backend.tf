82d8941e2290a4a75df60ec1eac6b3b0169af93e
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 18:23:13 -0700

    fix: module repo and mirror eks tf state path

--- aws_dragonfly-prod_eu-central-1_rds_dragonfly-rds-prod-eu1_backend.tf
+++ aws_dragonfly-prod_eu-central-1_rds_dragonfly-rds-prod-eu1_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "eticloud-tf-state-prod"
-    key    = "terraform-state/aws-dragonfly-production/aurora-postgres/us-east-2/dragonfly-rds-prod-1.tfstate"
+    key    = "terraform-state/aws/dragonfly-prod/eu-central-1/rds/dragonfly-rds-prod-1.tfstate"
     region = "us-east-2"
   }
 }
