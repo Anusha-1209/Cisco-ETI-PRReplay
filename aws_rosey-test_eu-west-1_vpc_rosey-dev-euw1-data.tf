28a4c2629bea91e13c082b2ad32d6df7403882d5
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 19:55:03 -0700

    chore: add redis vpc

--- aws_rosey-test_eu-west-1_vpc_rosey-dev-euw1-data.tf
+++ aws_rosey-test_eu-west-1_vpc_rosey-dev-euw1-data.tf
@@ -54,7 +54,7 @@ provider "aws" {
 # VPC
 ################################################################################
 module "vpc" {
-  source                          = "git::https://github.com/cisco-eti/sre-tf-module-aws-vpc.git?ref=2.0.5"
+  source                          = "git::https://github.com/cisco-eti/sre-tf-module-vpc-allinone.git?ref=vpc"
   region                          = local.aws_region
   vpc_name                        = local.vpc_name
   vpc_cidr                        = local.vpc_cidr
