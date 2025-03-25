9cbf0e50dce2c297ee8e85df60dba11d289039e7
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 18:41:18 -0700

    fix(vars): current naming won't allow to locate resources

--- aws_dragonfly-prod_eu-central-1_os_dragonfly-prod-eu-1-osis_variables.tf
+++ aws_dragonfly-prod_eu-central-1_os_dragonfly-prod-eu-1-osis_variables.tf
@@ -1,26 +1,26 @@
 variable "pipeline_name" {
   description = "The name of the pipeline"
   type        = string
-  default     = "osis-dragonfly-prod-eu-1-kafka"
+  default     = "osis-dragonfly-prod-eu1-kafka"
 }
 
 variable "domain_name" {
   description = "The name of the domain"
   type        = string
-  default     = "os-dragonfly-prod-eu-1"
+  default     = "os-dragonfly-prod-eu1"
 }
 
 variable "msk_cluster_name" {
   description = "The name of the MSK cluster"
   type        = string
-  default     = "dragonfly-msk-prod-eu-1"
+  default     = "dragonfly-msk-prod-eu1"
 }
 
 variable "tags" {
   description = "Tags to apply to all resources"
   type        = map(string)
   default = {
-    ApplicationName    = "osis-dragonfly-prod-eu-1"
+    ApplicationName    = "osis-dragonfly-prod-eu1"
     CiscoMailAlias     = "eti-sre-admins@cisco.com"
     DataClassification = "Cisco Confidential"
     DataTaxonomy       = "Cisco Operations Data"
