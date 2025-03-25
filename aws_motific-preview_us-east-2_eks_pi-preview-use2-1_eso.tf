4bd3e41c6471319f590af3d849de6b6bfb193d7f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 16:18:52 -0700

    Update aws/motific-preview/us-east-2/eks/pi-preview-use2-1/eso.tf

--- aws_motific-preview_us-east-2_eks_pi-preview-use2-1_eso.tf
+++ aws_motific-preview_us-east-2_eks_pi-preview-use2-1_eso.tf
@@ -40,7 +40,7 @@ module "eso_eticloud" {
 module "eso_eticloud_apps_vowel" {
   source               = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
   cluster_name         = local.name
-  vault_namespace      = "eticloud/apps/vowel"
+  vault_namespace      = "eticloud/apps/ppu"
   kubernetes_host      = data.aws_eks_cluster.cluster.endpoint
   kubernetes_ca        = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
   policies             = ["external-secrets-preview"]
