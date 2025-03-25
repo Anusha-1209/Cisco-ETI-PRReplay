51f41c2d6aabc04a6ee1a5b545a3f51f68c7b336
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 14:09:50 -0700

    chore: add regiosn

--- aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_eso.tf
+++ aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_eso.tf
@@ -13,7 +13,7 @@ provider "aws" {
   alias      = "target"
   access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region     = "eu-west-1"
+  region     = local.region
 }
 
 data "aws_eks_cluster" "cluster" {
