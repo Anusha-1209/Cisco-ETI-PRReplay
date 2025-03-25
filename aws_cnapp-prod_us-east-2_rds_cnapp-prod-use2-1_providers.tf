f13419676e2747064a16604c3f9d2583c5657d01
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 22:37:56 -0700

    feat: adding RDS database

---
+++ aws_cnapp-prod_us-east-2_rds_cnapp-prod-use2-1_providers.tf
@@ -0,0 +1,22 @@
+provider "vault" {
+  alias     = "eticloud_eticcprod"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud/eticcprod"
+}
+
+provider "aws" {
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "eu-west-1"
+  max_retries = 3
+  default_tags {
+    tags = {
+      ApplicationName    = "rds-cnapp-prod-use2-1"
+      CiscoMailAlias     = "eti-sre-admins@cisco.com"
+      DataClassification = "Cisco Confidential"
+      DataTaxonomy       = "Cisco Operations Data"
+      Environment        = "Prod"
+      ResourceOwner      = "ETI SRE"
+    }
+  }
+}
