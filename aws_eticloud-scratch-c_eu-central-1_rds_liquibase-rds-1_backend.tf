60a4510aeb48c298bf22c79cfb73f96f979dece5
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 10:58:56 -0700

    fix(state): naming

--- aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_backend.tf
+++ aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "eticloud-tf-state-nonprod"
-    key    = "terraform-state/aws/eticloud-scratch-c/eu-central-1/rds/helloworld.tfstate"
+    key    = "terraform-state/aws/eticloud-scratch-c/eu-central-1/rds/liquibase-rds-1.tfstate"
     region = "us-east-2"
   }
 }
