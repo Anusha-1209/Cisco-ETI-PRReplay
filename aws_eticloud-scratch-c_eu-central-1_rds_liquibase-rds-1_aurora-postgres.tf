61fa93a008cce56ad1bc30e5e89c91aa59f65d86
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 11:03:18 -0700

    fix(vpc): bootstrapped the database in the wrong VPC

--- aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_aurora-postgres.tf
+++ aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_aurora-postgres.tf
@@ -1,9 +1,10 @@
 module "rds" {
   source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-aurora-postgres?ref=1.1.0"
-  vpc_name          = "daboucha-liquibase-2"
+  vpc_name          = "liquibase-rds-test"
   database_name     = "liquibase-rds-1"
   db_instance_type  = "db.t4g.micro"
   cluster_name      = "liquibase-rds-1"
   db_engine_version = "15"
   db_allowed_cidrs  = ["10.19.0.0/16"] # EKS VPC CIDR
+  secret_path       = "secret/eticcprod/infra/aurora-pg/eticloud-scratch-c/eu-central-1/liquibase-rds-1"
 }
