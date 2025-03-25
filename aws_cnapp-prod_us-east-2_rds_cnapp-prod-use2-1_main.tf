dcfd0fcd730e0e6a128a08f7169f1d64667dbb5a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 22:41:23 -0700

    feat: fix rds repository

--- aws_cnapp-prod_us-east-2_rds_cnapp-prod-use2-1_main.tf
+++ aws_cnapp-prod_us-east-2_rds_cnapp-prod-use2-1_main.tf
@@ -4,7 +4,7 @@ module "rds" {
   database_name     = "cnapp_k8s_service"
   db_instance_type  = "db.r5.xlarge"
   cluster_name      = local.rds_name
-  secret_path       = "secret/eticcprod/infra/aurora-pg/eu-west-1/cwpp-staging/rds-cwpp-staging-1"
+  secret_path       = "secret/eticcprod/infra/aurora-pg/us-east-2/cnapp-prod/rds-cnapp-prod-use2-1"
   db_allowed_cidrs  = [data.aws_vpc.cluster_vpc.cidr_block]
   db_engine_version = "15"
 }
