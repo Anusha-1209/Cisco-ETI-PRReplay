5c6ebbabcac5a645458a80740036603ecdf45561
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 11:20:06 -0700

    fix(rds): provision DB subnets

--- aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_aurora-postgres.tf
+++ aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_aurora-postgres.tf
@@ -5,6 +5,16 @@ module "rds" {
   db_instance_type  = "db.t4g.medium"
   cluster_name      = "liquibaserds1"
   db_engine_version = "15.4"
-  db_allowed_cidrs  = ["10.19.0.0/16"] # EKS VPC CIDR
+  db_allowed_cidrs  = ["10.19.0.0/16"] # EKS VPC CIDR, not needed since the DB will live in the same VPC as the EKS cluster
   secret_path       = "secret/eticcprod/infra/aurora-pg/eticloud-scratch-c/eu-central-1/liquibase-rds-1"
+}
+
+resource "aws_db_subnet_group" "aurora_db_subnet_group" {
+  name = "daboucha-liquibase-2-db-subnet-group"
+  # intra subnets in the EKS VPC
+  subnet_ids = ["subnet-0ef13dfb2335a2612", "subnet-05a5f12ba469f9280", "subnet-026ebd609e59b2072"]
+
+  tags = {
+    Name = "DB subnet group to bootstrap an RDS instance in an existing VPC"
+  }
 }
