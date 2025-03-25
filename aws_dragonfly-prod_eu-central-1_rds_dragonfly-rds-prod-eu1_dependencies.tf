717ec9c78d28ff912a0e464276e88e1b40ee5474
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 18:22:14 -0700

    fix: paths and add atlantis project to run tf

---
+++ aws_dragonfly-prod_eu-central-1_rds_dragonfly-rds-prod-eu1_dependencies.tf
@@ -0,0 +1,11 @@
+data "vault_generic_secret" "aws_infra_credential" {
+  path     = "secret/eticcprod/infra/dragonfly-production/aws"
+  provider = vault.eticloud_eticcprod
+}
+
+data "aws_vpc" "eks_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["dragonfly-prod-euc1-1"]
+  }
+}
