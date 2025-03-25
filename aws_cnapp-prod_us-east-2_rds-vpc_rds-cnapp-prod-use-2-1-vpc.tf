a157c86c7fd2df5e5433589fab2300b178b87483
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 22:51:15 -0700

    feat: create an rds instance with vpc peering for cnapp prod

---
+++ aws_cnapp-prod_us-east-2_rds-vpc_rds-cnapp-prod-use-2-1-vpc.tf
@@ -0,0 +1,47 @@
+terraform {
+  backend "s3" {
+    bucket = "eticloud-tf-state-prod"
+    key    = "terraform-state/vpc/us-east-2/rds-cnapp-prod-use2-1-vpc.tfstate"
+    region = "us-east-2"
+  }
+}
+
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+# AWS credentails are the same of cwpp-dev vault secret (we deploy on same aws account)
+data "vault_generic_secret" "aws_infra_credential" {
+  path     = "secret/infra/aws/cnapp-prod/terraform_admin"
+  provider = vault.eticloud
+}
+
+provider "aws" {
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "us-east-2"
+  max_retries = 3
+  default_tags {
+    tags = {
+      ApplicationName    = "rds-cnapp-prod-use2-1-vpc"
+      CiscoMailAlias     = "eti-sre-admins@cisco.com"
+      DataClassification = "Cisco Confidential"
+      DataTaxonomy       = "Cisco Operations Data"
+      Environment        = "Prod"
+      ResourceOwner      = "ETI SRE"
+    }
+  }
+}
+
+module "vpc" {
+  source                          = "git::https://wwwin-github.cisco.com/eti/sre-tf-module-aws-vpc?ref=2.0.4"
+  region                          = "us-east-2"
+  vpc_cidr                        = "10.22.0.0/16"
+  vpc_name                        = "rds-cnapp-prod-use2-1"
+  cluster_name                    = "rds-cnapp-prod-use2-1"
+  create_database_subnet_group    = true
+  create_elasticache_subnet_group = false
+  create_secondary_subnets        = false
+}
--- aws_cnapp-prod_us-east-2_rds_cnapp-prod-use2-1_locals.tf
+++ aws_cnapp-prod_us-east-2_rds_cnapp-prod-use2-1_locals.tf
@@ -1,7 +1,7 @@
 
 locals {
   name  = "cnapp-prod-use2-1"
-  vpc_name = "cnapp-prod-use2-1"
+  vpc_name = "rds-cnapp-prod-use2-1-vpc"
   aws_account_name = "cnapp-prod"
-  rds_name  = "rds-cnapp-prod-1"
+  rds_name  = "rds-cnapp-prod-use2-1"
 }
