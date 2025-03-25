c27aaaa4bd514385654568fd43b865282129f3de
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 12:35:36 -0700

    fix: deploy

--- aws_dragonfly-demo_iam_main.tf
+++ aws_dragonfly-demo_iam_main.tf
@@ -46,6 +46,7 @@ module "aws_iam" {
 
   aws_region       = local.aws_region         # AWS provider region
   aws_account_name = local.aws_account_name   # AWS account name
+  aws_account_id   = local.aws_account_id
   app_name         = local.app_name           # AWS tag app_name
   eks_irsa         = local.eks_irsa           # EKS IRSA
 
