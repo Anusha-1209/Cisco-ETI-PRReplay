b38722d47f4a452dd98855b9d477da68d699ba21
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 15:00:07 -0700

    fix: remove enable_bootstrap_user_data

--- aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_main.tf
+++ aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_main.tf
@@ -19,16 +19,14 @@ module "eks_all_in_one" {
   # EKS cluster partially created as of Jan 15 2024
   source = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
 
-  name             = local.name             # EKS cluster name
-  region           = local.region           # AWS provider region
-  aws_account_name = local.aws_account_name # AWS account name
-  cidr             = "10.3.0.0/16"          # VPC CIDR
-  cluster_version  = "1.28"                 # EKS cluster version
+  name                       = local.name             # EKS cluster name
+  region                     = local.region           # AWS provider region
+  aws_account_name           = local.aws_account_name # AWS account name
+  cidr                       = "10.3.0.0/16"          # VPC CIDR
+  cluster_version            = "1.28"                 # EKS cluster version
 
   # EKS Managed Private Node Group
   ami_type                   = "AMAZON_LINUX_2"# EKS AMI type, required in case non hardened images
-  skip_cisco_hardened_ami    = true            # Skip cisco hardened images
-  enable_bootstrap_user_data = false           # Disable user data bootrap for non hardened images
   instance_types             = ["m5a.2xlarge"] # EKS instance types, prod US uses m5a.2xlarge
   min_size                   = 5               # EKS node group min size
   max_size                   = 10              # EKS node group max size
