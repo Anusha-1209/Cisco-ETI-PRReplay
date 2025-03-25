2893d04ab9c6ecccec5dd05bd3cc8a8ba22bdb7e
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 17:45:30 -0700

    fix: paths

---
+++ aws_dragonfly-prod_eu-central-1_msk_dragonfly-msk-prod-eu1_dependencies.tf
@@ -0,0 +1,55 @@
+data "vault_generic_secret" "aws_infra_credential" {
+  path     = "secret/eticcprod/infra/dragonfly-production/aws"
+  provider = vault.eticcprod
+}
+
+data "aws_iam_policy_document" "msk_secret_policy" {
+  for_each = var.kafka_clients
+
+  statement {
+    sid    = "AWSKafkaResourcePolicy"
+    effect = "Allow"
+
+    principals {
+      type        = "Service"
+      identifiers = ["kafka.amazonaws.com"]
+    }
+
+    actions   = ["secretsmanager:getSecretValue"]
+    resources = [aws_secretsmanager_secret.msk_auth_credentials[each.key].arn]
+  }
+}
+
+data "aws_vpc" "eks_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["dragonfly-prod-euc1-1"]
+  }
+}
+
+data "aws_vpc" "msk_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = ["dragonfly-prod-data-euc1-1"]
+  }
+}
+
+data "aws_subnets" "eks_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.eks_vpc.id]
+  }
+  tags = {
+    Tier = "Private"
+  }
+}
+
+data "aws_subnets" "msk_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.msk_vpc.id]
+  }
+  tags = {
+    Tier = "Private"
+  }
+}
