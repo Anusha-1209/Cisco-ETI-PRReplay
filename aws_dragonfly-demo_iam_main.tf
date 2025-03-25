086e2cfff40577418335fcd2612b7ae20c612257
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 13:08:43 -0700

    chore: update module tag

--- aws_dragonfly-demo_iam_main.tf
+++ aws_dragonfly-demo_iam_main.tf
@@ -47,8 +47,8 @@ locals {
 
 module "aws_iam" {
   # EKS cluster partially created as of Jan 29 2024
-  source = "git::https://github.com/cisco-eti/sre-tf-module-aws-iam.git?ref=iam-management"
-  
+  source = "git::https://github.com/cisco-eti/sre-tf-module-aws-iam.git?ref=main"
+
   aws_region       = local.aws_region         # AWS provider region
   aws_account_name = local.aws_account_name   # AWS account name
   aws_account_id   = local.aws_account_id     # AWS account id
