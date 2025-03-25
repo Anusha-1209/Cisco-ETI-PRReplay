42f1723e78882fd515ea6a0458087ec4bd382608
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 14:44:00 -0700

    chore: Adding RDS for motific dev

---
+++ aws_vowel-genai-dev_us-east-2_rds_motific-dev-use2-1_backend.tf
@@ -0,0 +1,8 @@
+
+terraform {
+  backend "s3" {
+    bucket = "eticloud-tf-state-nonprod" 
+    key    = "terraform-state/aurora-postgres/us-east-2/motific-dev-use2-1.tfstate"
+    region = "us-east-2"
+  }
+}
---
+++ aws_vowel-genai-dev_us-east-2_rds_motific-dev-use2-1_locals.tf
@@ -0,0 +1,7 @@
+
+locals {
+  vpc_name = "motific-dev-use2-data"
+  cluster_vpc_name = "motific-dev-use2-1"
+  aws_account_name = "vowel-genai-dev"
+  rds_name  = "motific-dev-use2-1"
+}
