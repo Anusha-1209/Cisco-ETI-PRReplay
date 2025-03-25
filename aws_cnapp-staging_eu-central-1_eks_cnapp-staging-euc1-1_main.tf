8e5fca9966e4d3120d21857250f11f0395be0f86
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 16:37:24 -0700

    Creating Base Branch for PR 153

---
+++ aws_cnapp-staging_eu-central-1_eks_cnapp-staging-euc1-1_main.tf
@@ -0,0 +1,35 @@
+terraform {
+  backend "s3" {
+    # This is the name of the backend S3 bucket.
+    bucket  = "eticloud-tf-state-nonprod"
+    # This is the path to the Terraform state file in the backend S3 bucket.
+    key     = "terraform-state/aws/cnapp-staging/eu-central-1/eks/cnapp-staging-euc1-1.tfstate"
+    # This is the region where the backend S3 bucket is located.
+    region  = "us-east-2" # DO NOT CHANGE.
+  }
+}
+
+locals {
+  name             = "cnapp-staging-euc1-1"
+  region           = "eu-central-1"
+  aws_account_name = "cnapp-staging"
+}
+
+module "eks_all_in_one" {
+  source            = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
+
+  name              = local.name              # EKS cluster name
+  region            = local.region            # AWS provider region
+  aws_account_name  = local.aws_account_name  # AWS account name
+  cidr              = "10.0.0.0/16"           # VPC CIDR
+  cluster_version   = "1.28"                  # EKS cluster version
+
+  # EKS Managed Private Node Group
+  instance_types    = ["m6a.2xlarge"]           # EKS instance types
+  min_size          = 5                       # EKS node group min size
+  max_size          = 10                      # EKS node group max size
+  desired_size      = 5                       # EKS node group desired size
+
+  # Karpenter
+  create_karpenter_irsa = true                 # Create Karpenter IRSA
+}
