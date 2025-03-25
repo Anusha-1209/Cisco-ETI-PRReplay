ef6523cc4859645da62177d56f9a4bd89322a1dd
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 14:15:28 -0700

    chore: add regiosn

--- aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_eso.tf
+++ aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_eso.tf
@@ -1,3 +1,19 @@
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
+}
+
+provider "aws" {
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = local.region
+}
 data "aws_eks_cluster" "cluster" {
   depends_on = [module.eks_all_in_one]
   provider   = aws.target
