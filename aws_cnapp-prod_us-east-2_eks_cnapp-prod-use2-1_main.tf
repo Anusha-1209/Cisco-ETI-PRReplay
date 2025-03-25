8373593ea5573d4cc7ccec6d1ea29641451452e2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-24 22:06:45 -0700

    fix: state file name

--- aws_cnapp-prod_us-east-2_eks_cnapp-prod-use2-1_main.tf
+++ aws_cnapp-prod_us-east-2_eks_cnapp-prod-use2-1_main.tf
@@ -3,7 +3,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket  = "eticloud-tf-state-nonprod"                                                   # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key     = "terraform-state/aws/cnapp-policy-dev/eu-west-1/eks/cnapp-policy-dev-euw1-1.tfstate"       # UPDATE ME.
+    key     = "terraform-state/aws/cnapp-prod/eu-west-1/eks/cnapp-prod-use2-1.tfstate"       # UPDATE ME.
     # This is the region where the backend S3 bucket is located.
     region  = "us-east-2"                                                                   # DO NOT CHANGE.
   }
