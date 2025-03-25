bbc8bd8c51c996c4a4e32e1d9532b56a9e4a06b8
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 12:14:01 -0700

    fix: cluster vpc names

--- aws_cnapp-prod_eu-central-1_rds_unified-agent-prod-euc1-1_locals.tf
+++ aws_cnapp-prod_eu-central-1_rds_unified-agent-prod-euc1-1_locals.tf
@@ -1,7 +1,7 @@
 
 locals {
   vpc_name = "cnapp-prod-euc1-data"
-  cluster_vpc_name = "cnapp-prod-euc1"
+  cluster_vpc_name = "cnapp-prod-euc1-1"
   aws_account_name = "cnapp-prod"
   rds_name  = "unified-agent-prod-euc1-1"
 }
--- aws_cnapp-prod_us-east-2_rds_unified-agent-prod-use2-1_locals.tf
+++ aws_cnapp-prod_us-east-2_rds_unified-agent-prod-use2-1_locals.tf
@@ -1,7 +1,7 @@
 
 locals {
   vpc_name = "cnapp-prod-use2-data"
-  cluster_vpc_name = "cnapp-prod-use2"
+  cluster_vpc_name = "cnapp-prod-use2-1"
   aws_account_name = "cnapp-prod"
   rds_name  = "unified-agent-prod-use2-1"
 }
