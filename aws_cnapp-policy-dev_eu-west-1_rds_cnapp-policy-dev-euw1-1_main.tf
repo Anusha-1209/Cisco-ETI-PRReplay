dd71b678818bbf508bf0961a62d5452de36d874b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 12:08:45 -0700

    chore: update RDS secret path

--- aws_cnapp-policy-dev_eu-west-1_rds_cnapp-policy-dev-euw1-1_main.tf
+++ aws_cnapp-policy-dev_eu-west-1_rds_cnapp-policy-dev-euw1-1_main.tf
@@ -44,7 +44,7 @@ module "rds" {
   database_name     = "postgressql"
   db_instance_type  = "db.r5.xlarge"
   cluster_name      = local.rds_name
-  secret_path       = "secret/eticloud/infra/cnapp-policy-dev/policy-rds-dev-eu1"
+  secret_path       = "secret/eticcprod/infra/aurora-pg/eu-west-1/cnapp-policy-dev/policy-rds-dev-euw1-1"
   db_engine_version = "15"
   db_allowed_cidrs  = [
     data.aws_vpc.eks_vpc.cidr_block,
