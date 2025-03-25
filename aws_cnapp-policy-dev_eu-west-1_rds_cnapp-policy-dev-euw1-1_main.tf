bfe01bcae6bfabdd5fd3726d1c81ca6e8dc6165a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 11:43:38 -0700

    chore: deploy vpc-peering data-to-compute

---
+++ aws_cnapp-policy-dev_eu-west-1_rds_cnapp-policy-dev-euw1-1_main.tf
@@ -0,0 +1,52 @@
+terraform {
+  backend "s3" {
+    bucket  = "eticloud-tf-state-nonprod"                                                
+    key     = "terraform-state/aws/cnapp-policy-dev/eu-west-1/rds/cnapp-policy-dev-euw1-1.tfstate"     
+    region  = "us-east-2"                                                                  
+  }
+}
+
+locals {
+  name             = "cnapp-policy-dev-euw1-1"
+  region           = "eu-west-1"
+  aws_account_name = "cnapp-policy-dev"
+  vpc_data         = "policy-dev-data-euc1-1"
+}
+
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  provider    = vault.eticloud
+  path        = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
+}
+
+provider "aws" {
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = local.region
+}
+
+data "aws_vpc" "eks_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [local.name]
+  }
+}
+
+module "rds" {
+  source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-aurora-postgres?ref=1.1.0"
+  
+  vpc_name          = local.vpc_data
+  database_name     = "cnapp-policy"
+  db_instance_type  = "db.r5.xlarge"
+  cluster_name      = "policy-rds-dev-eu1"
+  secret_path       = "secret/eticloud/infra/cnapp-policy-dev/policy-rds-dev-eu1"
+  db_engine_version = "15"
+  db_allowed_cidrs  = [
+    data.aws_vpc.eks_vpc.cidr_block,
+  ]
+}
---
+++ aws_cnapp-policy-dev_eu-west-1_vpc-peering_compute-to-data_main.tf
@@ -0,0 +1,131 @@
+terraform {
+  backend "s3" {
+    bucket  = "eticloud-tf-state-nonprod"                                                
+    key     = "terraform-state/aws/cnapp-policy-dev/eu-west-1/vpc-peering/compute-to-data.tfstate"     
+    region  = "us-east-2"                                                                  
+  }
+}
+
+locals {
+  region           = "eu-west-1"
+  aws_account_name = "cnapp-policy-dev"
+  vpc_data         = "policy-dev-data-euc1-1"
+  vpc_compute      = "cnapp-policy-dev-euw1-1"
+}
+
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  provider    = vault.eticloud
+  path        = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
+}
+
+provider "aws" {
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = local.region
+}
+
+# Get the VPC IDs based on the names of the VPCs
+data "aws_vpc" "requestor_vpc" {
+  filter {
+    name   = "tag:ApplicationName"
+    values = [local.vpc_compute]
+  }
+}
+
+# VPC created from sre-tf-infra, not migrated to this repo
+data "aws_vpc" "acceptor_vpc" {
+  filter {
+    name   = "tag:ApplicationName"
+    values = [local.vpc_data]
+  }
+}
+
+data "aws_route_tables" "requestor_vpc_rt" {
+  vpc_id = data.aws_vpc.requestor_vpc.id
+}
+
+data "aws_route_tables" "acceptor_vpc_rt" {
+  vpc_id = data.aws_vpc.acceptor_vpc.id
+}
+
+# Use this to get the account ID
+data "aws_caller_identity" "current" {}
+
+# VPC peering resources
+resource "aws_vpc_peering_connection" "data_to_compute" {
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id   = data.aws_vpc.acceptor_vpc.id
+  vpc_id        = data.aws_vpc.requestor_vpc.id
+  auto_accept   = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  tags = {
+    Name                  = "VPC Peering between policy-dev-euw1-1 and policy-dev-data-euw1-1"
+    CSBApplicationName    = "dev-policy-compute-data-peering"
+    CSBCiscoMailAlias     = "eti-sre@cisco.com"
+    CSBDataClassification = "Cisco Confidential"
+    CSBDataTaxonomy       = "Cisco Operations Data"
+    CSBEnvironment        = "NonProd"
+    CSBResourceOwner      = "Outshift SRE"
+  }
+}
+
+# VPC routing resources
+resource "aws_route" "compute_to_data" {
+  count                     = length(data.aws_route_tables.requestor_vpc_rt.ids)
+  route_table_id            = data.aws_route_tables.requestor_vpc_rt.ids[count.index]
+  destination_cidr_block    = data.aws_vpc.acceptor_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.data_to_compute.id
+}
+
+resource "aws_route" "data_to_compute" {
+  count                     = length(data.aws_route_tables.acceptor_vpc_rt.ids)
+  route_table_id            = data.aws_route_tables.acceptor_vpc_rt.ids[count.index]
+  destination_cidr_block    = data.aws_vpc.requestor_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.data_to_compute.id
+}
+
+# Security groups
+resource "aws_security_group" "compute_to_data" {
+  name        = "compute-vpc-to-data-pvc"
+  description = "Allows all communication into data-pvc from the compute-pvc"
+  vpc_id      = data.aws_vpc.acceptor_vpc.id
+}
+
+resource "aws_security_group" "data_to_compute" {
+  name        = "data-vpc-to-compute-pvc"
+  description = "Allows all communication into compute-pvc from the data-pvc"
+  vpc_id      = data.aws_vpc.requestor_vpc.id
+}
+
+# security group rules because the rules are going to reference the groups
+resource "aws_security_group_rule" "compute_to_data" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.compute_to_data.id
+  source_security_group_id = aws_security_group.data_to_compute.id
+}
+
+resource "aws_security_group_rule" "data_to_compute" {
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 65535
+  protocol                 = "-1"
+  security_group_id        = aws_security_group.data_to_compute.id
+  source_security_group_id = aws_security_group.compute_to_data.id
+}
