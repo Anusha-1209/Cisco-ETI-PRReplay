66834e2413b787180914f5fe2af47137b6d5932f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 11:03:29 -0700

    fix(rds): alphanumeric chars only

--- aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_aurora-postgres.tf
+++ aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_aurora-postgres.tf
@@ -1,9 +1,9 @@
 module "rds" {
   source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-aurora-postgres?ref=1.1.0"
   vpc_name          = "liquibase-rds-test"
-  database_name     = "liquibase-rds-1"
+  database_name     = "liquibaserds1"
   db_instance_type  = "db.t4g.micro"
-  cluster_name      = "liquibase-rds-1"
+  cluster_name      = "liquibaserds1"
   db_engine_version = "15"
   db_allowed_cidrs  = ["10.19.0.0/16"] # EKS VPC CIDR
   secret_path       = "secret/eticcprod/infra/aurora-pg/eticloud-scratch-c/eu-central-1/liquibase-rds-1"
