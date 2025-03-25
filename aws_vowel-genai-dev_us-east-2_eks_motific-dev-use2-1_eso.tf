e766cbcdca552a3a74568d5f637731c66a1d8910
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 13:47:49 -0700

    chore: Adding vowel namespace

--- aws_vowel-genai-dev_us-east-2_eks_motific-dev-use2-1_eso.tf
+++ aws_vowel-genai-dev_us-east-2_eks_motific-dev-use2-1_eso.tf
@@ -29,7 +29,7 @@ data "vault_generic_secret" "cluster_certificate" {
 }
 
 module "eso_eticloud" {
-  source               = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=0.0.2"
+  source               = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
   cluster_name         = local.name
   vault_namespace      = "eticloud"
   kubernetes_host      = data.aws_eks_cluster.cluster.endpoint
@@ -38,7 +38,7 @@ module "eso_eticloud" {
 }
 
 module "eso_eticloud_apps_vowel" {
-  source               = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=0.0.2"
+  source               = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
   cluster_name         = local.name
   vault_namespace      = "eticloud/apps/vowel"
   kubernetes_host      = data.aws_eks_cluster.cluster.endpoint
