aad7c4429518832dc47d246b56aca691169e7ccd
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 11:18:35 -0700

    fix(rds+vpc): trying to bootstrap the RDS instance in a different VPC

--- aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_aurora-postgres.tf
+++ aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_aurora-postgres.tf
@@ -1,6 +1,6 @@
 module "rds" {
   source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-aurora-postgres?ref=1.1.0"
-  vpc_name          = "liquibase-rds-test"
+  vpc_name          = "daboucha-liquibase-2"
   database_name     = "liquibaserds1"
   db_instance_type  = "db.t4g.medium"
   cluster_name      = "liquibaserds1"
