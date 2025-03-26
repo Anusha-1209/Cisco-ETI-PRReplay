ccfdf17c9940512bfb443f77f72221f6e16e961b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-26 10:23:15 -0700

    Creating Base Branch for PR 219

---
+++ aws_cnapp-staging_eu-central-1_vpc-peering_compute-to-data_vpc-peering.tf
@@ -0,0 +1,100 @@
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  path     = "secret/infra/aws/cnapp-staging/terraform_admin"
+  provider = vault.eticloud
+}
+
+terraform {
+  backend "s3" {
+    bucket = "eticloud-tf-state-nonprod"
+    key    = "terraform-state/vpc-peering/eu-central-1/data-cnapp-staging-euc1-1.tfstate"
+    region = "us-east-2"
+  }
+}
+
+locals {
+  acceptor_vpc_name  = "cnapp-staging-euc1-1"
+  requestor_vpc_name = "cnapp-staging-euc1-data"
+}
+
+# Get the VPC IDs based on the names of the VPCs
+data "aws_vpc" "acceptor_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [local.acceptor_vpc_name]
+  }
+}
+
+data "aws_vpc" "requestor_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [local.requestor_vpc_name]
+  }
+}
+
+data "aws_route_tables" "acceptor_vpc_rt" {
+  vpc_id = data.aws_vpc.acceptor_vpc.id
+}
+
+data "aws_route_tables" "requestor_vpc_rt" {
+  vpc_id = data.aws_vpc.requestor_vpc.id
+}
+
+# Use this to get the account ID
+data "aws_caller_identity" "current" {}
+
+
+# By setting the AWS provider credentials via the data source above, we control in which account and region the resources get created.
+provider "aws" {
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "eu-central-1"
+  max_retries = 3
+  default_tags {
+    tags = {
+      Name               = "VPC Peering between ${local.acceptor_vpc_name} and ${local.requestor_vpc_name}"
+      ApplicationName    = "${local.acceptor_vpc_name}-vpc-peering"
+      CiscoMailAlias     = "eti-sre@cisco.com"
+      DataClassification = "Cisco Confidential"
+      DataTaxonomy       = "Cisco Operations Data"
+      Environment        = "NonProd"
+      ResourceOwner      = "Outshift SRE"
+    }
+  }
+}
+
+# VPC peering resources
+resource "aws_vpc_peering_connection" "peering_connection" {
+  peer_owner_id = data.aws_caller_identity.current.account_id
+  peer_vpc_id   = data.aws_vpc.acceptor_vpc.id
+  vpc_id        = data.aws_vpc.requestor_vpc.id
+  auto_accept   = true
+
+  accepter {
+    allow_remote_vpc_dns_resolution = true
+  }
+
+  requester {
+    allow_remote_vpc_dns_resolution = true
+  }
+}
+
+# VPC routing resources
+resource "aws_route" "db-to-eks" {
+  count                     = length(data.aws_route_tables.requestor_vpc_rt.ids)
+  route_table_id            = data.aws_route_tables.requestor_vpc_rt.ids[count.index]
+  destination_cidr_block    = data.aws_vpc.acceptor_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection.id
+}
+
+resource "aws_route" "eks-to-db" {
+  count                     = length(data.aws_route_tables.acceptor_vpc_rt.ids)
+  route_table_id            = data.aws_route_tables.acceptor_vpc_rt.ids[count.index]
+  destination_cidr_block    = data.aws_vpc.requestor_vpc.cidr_block
+  vpc_peering_connection_id = aws_vpc_peering_connection.peering_connection.id
+}
