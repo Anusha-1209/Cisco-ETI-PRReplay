74c892dd23be92f98906c64828968980edad3743
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 21:48:45 -0700

    fix(IAM): fmt

--- aws_dragonfly-prod_eu-central-1_iam_backend.tf
+++ aws_dragonfly-prod_eu-central-1_iam_backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "eticloud-tf-state-prod"
     # State file already exists
-    key = "terraform-state/aws/dragonfly-prod/iam/iam-roles.tfstate"
+    key    = "terraform-state/aws/dragonfly-prod/iam/iam-roles.tfstate"
     region = "us-east-2"
   }
 }
