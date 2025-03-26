cf3eae3fc5f8e2344de47686da5e41c1e0e45270
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 19:45:43 -0700

    chore: add s3 emr-motf-preview-use2-1

--- aws_motific-preview_us-east-2_s3_emr-motf-preview-use2-1_main.tf
+++ aws_motific-preview_us-east-2_s3_emr-motf-preview-use2-1_main.tf
@@ -1,19 +1,19 @@
 # This file was created by Outshift Platform Self-Service automation.
 terraform {
   backend "s3" {
-    bucket        = "eticloud-tf-state-nonprod" # We separate the different levels of development into different buckets. The buckets are eticloud-tf-state-sandbox, eticloud-tf-state-nonprod, eticloud-tf-state-prod. The environment should match the CSBEnvironment below.
-    key           = "terraform-state/motific-preview/us-east-2/s3/emr-motf-preview-use2-1.tfstate" # #note the path here. It should match the pattern terraform_state/<service>/<region>/<name>.tfstate
-    region        = "us-east-2" # Do not change without talking to the SRE team.
+    bucket = "eticloud-tf-state-nonprod"                                                    # We separate the different levels of development into different buckets. The buckets are eticloud-tf-state-sandbox, eticloud-tf-state-nonprod, eticloud-tf-state-prod. The environment should match the CSBEnvironment below.
+    key    = "terraform-state/motific-preview/us-east-2/s3/emr-motf-preview-use2-1.tfstate" # #note the path here. It should match the pattern terraform_state/<service>/<region>/<name>.tfstate
+    region = "us-east-2"                                                                    # Do not change without talking to the SRE team.
   }
 }
 provider "vault" {
   address   = "https://keeper.cisco.com"
   namespace = "eticloud"
-  alias = "eticloud"
+  alias     = "eticloud"
 }
 data "vault_generic_secret" "aws_infra_credential" {
   provider = vault.eticloud
-  path = "secret/infra/aws/motific-preview/terraform_admin"
+  path     = "secret/infra/aws/motific-preview/terraform_admin"
 }
 
 provider "aws" {
@@ -37,8 +37,8 @@ provider "aws" {
 }
 
 module "s3" {
-  source  = "git::https://wwwin-github.cisco.com/eti/sre-tf-module-aws-s3.git?ref=1.0.2"
-  bucket_name = "emr-motf-preview-use2-1"
+  source                = "git::https://wwwin-github.cisco.com/eti/sre-tf-module-aws-s3.git?ref=1.0.2"
+  bucket_name           = "emr-motf-preview-use2-1"
   CSBApplicationName    = "Motific EMR S3 Bucket"
   CSBCiscoMailAlias     = "eti-sre-admins@cisco.com"
   CSBDataClassification = "Cisco Restricted"
