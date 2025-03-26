ae9d7a37d8255a73934ff1dd01a5fe08e85e5296
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 18:30:43 -0700

    fix: naming corrections

---
+++ aws_motific-preview_sagemaker_motf-preview-sagemaker_locals.tf
@@ -0,0 +1,4 @@
+locals {
+  name     = "motf-preview-sagemaker"
+  vpc_name = "motf-preview-use2-1"
+}
--- aws_motific-preview_sagemaker_motific-preview-sagemaker_locals.tf
+++
@@ -1,4 +0,0 @@
-locals {
-  name     = "motific-preview-sagemaker"
-  vpc_name = "motific-preview-use2-1"
-}
