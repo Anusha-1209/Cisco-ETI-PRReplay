73a11c57772bfdbdf1356f753242385ed48ded59
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 23:52:25 -0700

    chore(karpneter): remove unused code

--- aws_dragonfly-prod_eu-central-1_iam_backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "eticloud-tf-state-prod"
-#     # State file already exists
-#     key    = "terraform-state/aws/dragonfly-prod/iam/iam-roles.tfstate"
-#     region = "us-east-2"
-#   }
-# }
