7d8f96f9eb0cfa365685a3d02e69657ca03238de
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 18:12:10 -0700

    chore: adding cisco-quantum-3 ec2 instance

---
+++ aws_cisco-research_us-west-2_ec2_cisco-quantum-3_ec2.tf
@@ -0,0 +1,28 @@
+terraform {
+  backend "s3" {
+    bucket = "eticloud-tf-state-nonprod"
+    key    = "terraform-state/vpc/us-east-2/cisco-quantum-3.tfstate"
+    region = "us-east-2"
+  }
+}
+
+locals {
+  name             = "cisco-quantum-3"
+  region           = "us-west-2"
+  aws_account_name = "cisco-research"
+}
+
+# Find instance types and cost here - https://instances.vantage.sh/?min_vcpus=4&region=us-east-2
+
+module "ec2" {
+  source             = "git::https://github.com/cisco-eti/sre-tf-module-ec2-allinone.git?ref=latest"
+  name               = local.name             # VPC name
+  region             = local.region           # AWS provider region
+  aws_account_name   = local.aws_account_name # AWS account name
+  instance_type      = "G5.2xlarge"           # EC2 instance type
+  availability_zone  = "us-west-2c"
+  is_public          = true                   # Is the instance public
+  ignore_ami_changes = true                   # Ignore AMI changes
+  ami_type           = "ubuntu"              # Windows AMI OS Flavor
+  vpc_name           = "cisco-quantum-vpc-1" # VPC Name
+}
