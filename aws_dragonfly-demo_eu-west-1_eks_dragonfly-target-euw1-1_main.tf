83eaf50edb9307843e46fd21c5be6b21facbe1f8
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-26 00:13:38 -0700

    fix(dragonfly-target-euw1-1): use latest EKS module

--- aws_dragonfly-demo_eu-west-1_eks_dragonfly-target-euw1-1_main.tf
+++ aws_dragonfly-demo_eu-west-1_eks_dragonfly-target-euw1-1_main.tf
@@ -26,7 +26,7 @@ locals {
 
 module "eks_all_in_one" {
   # EKS cluster partially created as of Jan 15 2024
-  source = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=extend_ami"
+  source = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
 
   name             = local.name             # EKS cluster name
   region           = local.region           # AWS provider region
@@ -35,9 +35,8 @@ module "eks_all_in_one" {
   cluster_version  = "1.28"                 # EKS cluster version
 
   # EKS Managed Private Node Group
-  hardened_image               = false           # Use or not cisco hardened images
-  ami_type                     = "AL2_x86_64"    # EKS AMI type, required in case non hardened images
-    enable_bootstrap_user_data = false           # Disable user data bootrap for non hardened images
+  ami_id                     = "ami-056e32f51503d3604" # EKS AMI ID, required in case non hardened images
+  ami_type                   = "AMAZON_LINUX_2"  # EKS AMI type, required in case non hardened images
   instance_types               = ["m5a.2xlarge"] # EKS instance types, prod US uses m5a.2xlarge
   min_size                     = 2               # EKS node group min size
   max_size                     = 5               # EKS node group max size
