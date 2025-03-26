72e4fe6e75e7d2a5d1820fef425e5471bd57fbc9
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-26 00:11:39 -0700

    merge

--- aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_main.tf
+++ aws_dragonfly-demo_eu-west-1_eks_dragonfly-demo-euw1-1_main.tf
@@ -38,8 +38,9 @@ module "eks_all_in_one" {
   cluster_version  = "1.28"                 # EKS cluster version
 
   # EKS Managed Private Node Group
+  ami_id                     = "ami-0d35ebabe93f2d3cf" # EKS AMI ID, required in case non hardened images
   ami_type                   = "AMAZON_LINUX_2"# EKS AMI type, required in case non hardened images
-  skip_cisco_hardened_ami    = true            # Skip cisco hardened images
+  skip_cisco_hardened_ami    = true            # Skip Cisco hardened AMI
   instance_types             = ["m5a.2xlarge"] # EKS instance types, prod US uses m5a.2xlarge
   min_size                   = 5               # EKS node group min size
   max_size                   = 10              # EKS node group max size
