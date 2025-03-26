8996cca3d2047da291d765465cf04ce4e22181ea
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-26 13:17:58 -0700

    chore: add motific staging resources

---
+++ aws_motific-staging_us-east-2_sagemaker_motf-staging-sagemaker_locals.tf
@@ -0,0 +1,4 @@
+locals {
+  name     = "motf-staging-sagemaker"
+  vpc_name = "motf-staging-use2-1"
+}
---
+++ aws_motific-staging_us-east-2_vpc_motf-staging-use2-data_vpc.tf
@@ -0,0 +1,47 @@
+terraform {
+  backend "s3" {
+    bucket = "eticloud-tf-state-nonprod"
+    key    = "terraform-state/vpc/us-east-2/motf-staging-use2-data-vpc.tfstate"
+    region = "us-east-2"
+  }
+}
+
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+# AWS credentails are the same of cwpp-dev vault secret (we deploy on same aws account)
+data "vault_generic_secret" "aws_infra_credential" {
+  path     = "secret/infra/aws/motific-staging/terraform_admin"
+  provider = vault.eticloud
+}
+
+provider "aws" {
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "us-east-2"
+  max_retries = 3
+  default_tags {
+    tags = {
+      ApplicationName    = "motf-staging-use2-data-vpc"
+      CiscoMailAlias     = "eti-sre-admins@cisco.com"
+      DataClassification = "Cisco Confidential"
+      DataTaxonomy       = "Cisco Operations Data"
+      Environment        = "NonProd"
+      ResourceOwner      = "Outshift SRE"
+    }
+  }
+}
+
+module "vpc" {
+  source                          = "git::https://github.com/cisco-eti/sre-tf-module-aws-vpc?ref=2.0.6"
+  region                          = "us-east-2"
+  vpc_cidr                        = "10.2.0.0/16"
+  vpc_name                        = "motf-staging-use2-data"
+  cluster_name                    = "motf-staging-use2-data"
+  create_database_subnet_group    = true
+  create_elasticache_subnet_group = false
+  create_secondary_subnets        = false
+}
