200d28a5a75301984cd888153ace5b13fa1f8fb2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 21:32:58 -0700

    fix: terraform fmt

--- aws_vowel-genai-dev_us-east-2_eks_wandb-k8s-agent-1_main.tf
+++ aws_vowel-genai-dev_us-east-2_eks_wandb-k8s-agent-1_main.tf
@@ -1,26 +1,26 @@
 terraform {
   backend "s3" {
-    bucket  = "eticloud-tf-state-nonprod"
-    key     = "terraform-state/aws/vowel-genai-dev/us-east-2/eks/wandb-k8s-agent-1.tfstate"
-    region  = "us-east-2"
+    bucket = "eticloud-tf-state-nonprod"
+    key    = "terraform-state/aws/vowel-genai-dev/us-east-2/eks/wandb-k8s-agent-1.tfstate"
+    region = "us-east-2"
   }
 }
 
 locals {
-  name              = "wandb-k8s-agent-1"
-  region            = "us-east-2"
-  aws_account_name  = "vowel-genai-dev"
-  vpc_cidr          = "10.0.0.0/16"
+  name             = "wandb-k8s-agent-1"
+  region           = "us-east-2"
+  aws_account_name = "vowel-genai-dev"
+  vpc_cidr         = "10.0.0.0/16"
 }
 
 module "eks_all_in_one" {
-  source                  = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
-  name                    = local.name              # EKS cluster name
-  region                  = local.region            # AWS provider region
-  aws_account_name        = local.aws_account_name  # AWS account name
-  cidr                    = local.vpc_cidr          # VPC CIDR
-  cluster_version         = "1.28"    # EKS cluster version
-  bastion_instance_count  = 0
+  source                 = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
+  name                   = local.name             # EKS cluster name
+  region                 = local.region           # AWS provider region
+  aws_account_name       = local.aws_account_name # AWS account name
+  cidr                   = local.vpc_cidr         # VPC CIDR
+  cluster_version        = "1.28"                 # EKS cluster version
+  bastion_instance_count = 0
 
 
   # EKS Managed Private Node Group
@@ -34,15 +34,15 @@ module "eks_all_in_one" {
   create_alb_irsa         = false
   create_otel_irsa        = false
   additional_aws_auth_configmap_roles = [
-      {
-        rolearn  = "arn:aws:iam::961088030672:role/cluster-access",
-        username = "cluster-access",
-        groups   = ["system:masters"]
-      },
-      {
-        rolearn  = "arn:aws:iam::961088030672:role/devops",
-        username = "devops",
-        groups   = ["system:masters"]
-      }
+    {
+      rolearn  = "arn:aws:iam::961088030672:role/cluster-access",
+      username = "cluster-access",
+      groups   = ["system:masters"]
+    },
+    {
+      rolearn  = "arn:aws:iam::961088030672:role/devops",
+      username = "devops",
+      groups   = ["system:masters"]
+    }
   ]
 }
