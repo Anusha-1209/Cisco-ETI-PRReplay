4d577192fc579260cc0f1739a7e18ca273d3131c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 23:41:03 -0700

    fix: update wandb-k8s-agent-1

--- aws_vowel-genai-dev_us-east-2_eks_wandb-k8s-agent-1_main.tf
+++ aws_vowel-genai-dev_us-east-2_eks_wandb-k8s-agent-1_main.tf
@@ -20,16 +20,16 @@ module "eks_all_in_one" {
   aws_account_name       = local.aws_account_name # AWS account name
   cidr                   = local.vpc_cidr         # VPC CIDR
   cluster_version        = "1.28"                 # EKS cluster version
-  bastion_instance_count = 0
+  bastion_instance_count = 1                      # Bastion instance count
 
 
   # EKS Managed Private Node Group
   instance_types          = ["m6a.xlarge"]
   ami_type                = "CISCO_HARDENED_AL2"
   skip_cisco_hardened_ami = false
-  min_size                = 3
+  min_size                = 2
   max_size                = 6
-  desired_size            = 3
+  desired_size            = 2
   create_karpenter_irsa   = true
   create_alb_irsa         = false
   create_otel_irsa        = false
