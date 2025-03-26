515c538036fd8c328f7d087154b8eb53a735b801
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 17:41:34 -0700

    fix: tf fmt errors

--- aws_vowel-genai-dev_us-east-2_vpc_motific-dev-use2-vms_vpc.tf
+++ aws_vowel-genai-dev_us-east-2_vpc_motific-dev-use2-vms_vpc.tf
@@ -7,15 +7,15 @@ terraform {
 }
 
 locals {
-  name              = "motific-dev-use2-vms"
-  region            = "us-east-2"
-  aws_account_name  = "vowel-genai-dev"
+  name             = "motific-dev-use2-vms"
+  region           = "us-east-2"
+  aws_account_name = "vowel-genai-dev"
 }
 
 module "vpc" {
-  source            = "git::https://github.com/cisco-eti/sre-tf-module-vpc-allinone.git?ref=latest"
-  name              = local.name              # VPC name
-  region            = local.region            # AWS provider region
-  aws_account_name  = local.aws_account_name  # AWS account name
-  cidr              = "10.2.0.0/16"           # VPC CIDR
+  source           = "git::https://github.com/cisco-eti/sre-tf-module-vpc-allinone.git?ref=latest"
+  name             = local.name             # VPC name
+  region           = local.region           # AWS provider region
+  aws_account_name = local.aws_account_name # AWS account name
+  cidr             = "10.2.0.0/16"          # VPC CIDR
 }
