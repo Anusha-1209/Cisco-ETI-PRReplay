75bf6f4dd1a006cab7dd4428f2ee3dee6d31af2d
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 20:04:34 -0700

    fix: use main.tf for providers

--- aws_motific-preview_us-east-2_iam_motf-unified-plugins-role_main.tf
+++ aws_motific-preview_us-east-2_iam_motf-unified-plugins-role_main.tf
@@ -1,3 +1,21 @@
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/motific-preview/terraform_admin"
+}
+
+provider "aws" {
+  alias      = "target"
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = "us-east-2"
+}
+
 resource "aws_iam_policy" "aws_motf_unified_plugins_policy" {
   name        = "motf-preview-unified-plugins-policy"
   description = "AWS motific unified plugins Role IAM Policy"
