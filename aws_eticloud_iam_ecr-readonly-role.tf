ddb946b9667ee280fc039be7316eb6dcae94ccd0
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 20:32:40 -0700

    chore: add iam roles for staging & prod

--- aws_eticloud_iam_ecr-readonly-role.tf
+++ aws_eticloud_iam_ecr-readonly-role.tf
@@ -7,40 +7,79 @@
 #################################
 # ECR Read Only Access 
 #################################
-
+data "aws_eks_cluster" "eks-dragonfly-dev-2" {
+  name = "eks-dragonfly-dev-2"
+}
+data "aws_eks_cluster" "eks-df-staging-1" {
+  name = "eks-df-staging-1"
+}
+data "aws_eks_cluster" "dragonfly-prod-euc1-1" {
+  name = "dragonfly-prod-euc1-1"
+}
+data "aws_eks_cluster" "dragonfly-prod-1" {
+  name = "dragonfly-prod-1"
+}
 locals {
-  eks_cluster_name = "eks-dragonfly-dev-2"
-  aws_account_id   = "474213319131"
-  eks_oidc         = "oidc.eks.eu-west-1.amazonaws.com/id/AEE5EF78D8DBE429EB35271E41F3AC72AWS"
-  service_account  = "dragonfly-backend:dragonfly-art-a-dev-app"
-  resources_names  = ["dragonfly/datamodel/*", "dragonfly/kg-plugin/*", "dragonfly/falco-rules/*"]
+  eks_irsa = {
+    dragonfly = {
+      eks-dragonfly-dev-2 = {
+        aws_account_id    = "474213319131"
+        eks_oidc          = "${replace(data.aws_eks_cluster.eks-dragonfly-dev-2.identity[0].oidc[0].issuer, "https://", "")}"
+        eks_cluster_name  = data.aws_eks_cluster.eks-dragonfly-dev-2.name
+        services_accounts = ["dragonfly-backend:dragonfly-art-a-dev-app"],
+        resources_names   = ["dragonfly/datamodel/*", "dragonfly/kg-plugin/*", "dragonfly/falco-rules/*"]
+      },
+      eks-df-staging-1 = {
+        aws_account_id    = "947352569831"
+        eks_oidc          = "${replace(data.aws_eks_cluster.eks-df-staging-1.identity[0].oidc[0].issuer, "https://", "")}"
+        eks_cluster_name  = data.aws_eks_cluster.eks-df-staging-1.name
+        services_accounts = ["dragonfly-backend:dragonfly-art-a-staging-app"],
+        resources_names   = ["dragonfly/datamodel/*", "dragonfly/kg-plugin/*", "dragonfly/falco-rules/*"]
+      },
+      dragonfly-prod-euc1-1 = {
+        aws_account_id    = "651416187950"
+        eks_oidc          = "${replace(data.aws_eks_cluster.dragonfly-prod-euc1-1.identity[0].oidc[0].issuer, "https://", "")}"
+        eks_cluster_name  = data.aws_eks_cluster.dragonfly-prod-euc1-1.name
+        services_accounts = ["dragonfly-backend:dragonfly-art-a-prod-eu-app"],
+        resources_names   = ["dragonfly/datamodel/*", "dragonfly/kg-plugin/*", "dragonfly/falco-rules/*"]
+      },
+      dragonfly-prod-1 = {
+        aws_account_id    = "651416187950"
+        eks_oidc          = "${replace(data.aws_eks_cluster.dragonfly-prod-euc1-1.identity[0].oidc[0].issuer, "https://", "")}"
+        eks_cluster_name  = data.aws_eks_cluster.dragonfly-prod-euc1-1.name
+        services_accounts = ["dragonfly-backend:dragonfly-art-a-prod-app"],
+        resources_names   = ["dragonfly/datamodel/*", "dragonfly/kg-plugin/*", "dragonfly/falco-rules/*"]
+      },
+    }
+  }
 }
 
 # IAM Role
 resource "aws_iam_role" "eks_irsa_ecr_ro" {
+  for_each = local.eks_irsa != null ? lookup(local.eks_irsa, "dragonfly", {}) : {}
 
-  name = "${local.eks_cluster_name}-eks-irsa-ecr-ro"
+  name = "${local.eks_irsa["dragonfly"][each.key].eks_cluster_name}-eks-irsa-ecr-ro"
   assume_role_policy = templatefile("${path.module}/policies/assume_role_irsa.tpl", {
-    aws_account_id  = local.aws_account_id
-    eks_oidc        = local.eks_oidc
-    service_account = local.service_account
+    aws_account_id   = local.eks_oidc["dragonfly"][each.key].aws_account_id
+    eks_oidc         = local.eks_oidc["dragonfly"][each.key].eks_oidc
+    service_accounts = join("\",\"", var.eks_irsa["dragonfly"][each.key].services_accounts)
   })
 }
 
 # IAM Policy
 resource "aws_iam_policy" "eks_irsa_ecr_ro_policy" {
 
-  name        = "${local.eks_cluster_name}-eks-irsa-ecr-ro-policy"
-  description = "EKS IRSA secret access ECR policy"
+  name        = "${local.eks_irsa["dragonfly"][each.key].eks_cluster_name}-eks-irsa-ecr-ro-policy"
+  description = "EKS IRSA secret access dragonfly ECR policy"
   policy = templatefile("${path.module}/policies/ecr_readonly_policy.tpl", {
-    resources = join("\",\"", (formatlist("arn:aws:ecr:us-east-2:626007623524:repository/%s", local.resources_names)))
+    resources = join("\",\"", (formatlist("arn:aws:ecr:us-east-2:626007623524:repository/%s", "${local.eks_irsa["dragonfly"][each.key].resources_names}")))
   })
 }
 
 # IAM Policy Attachment
 resource "aws_iam_policy_attachment" "eks_irsa_s3_ro_policy" {
 
-  name       = "${local.eks_cluster_name}-eks-irsa-ecr-ro-attach"
+  name       = "${local.eks_irsa["dragonfly"][each.key].eks_cluster_name}-eks-irsa-ecr-ro-attach"
   roles      = [aws_iam_role.eks_irsa_ecr_ro.name]
   policy_arn = aws_iam_policy.eks_irsa_ecr_ro_policy.arn
 }
