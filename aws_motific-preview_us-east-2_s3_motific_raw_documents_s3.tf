69e7ff31b075c8fc85d5de94c2d60e8ed52cfb87
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-25 17:12:32 -0700

    fix: change underscore to hyphen in bucekt name

--- aws_motific-preview_us-east-2_s3_motific_raw_documents_s3.tf
+++ aws_motific-preview_us-east-2_s3_motific_raw_documents_s3.tf
@@ -17,7 +17,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket = "eticloud-tf-state-nonprod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key = "terraform-state/aws/motific-preview/us-east-2/s3/motific_raw_documents.tfstate" # UPDATE ME.
+    key = "terraform-state/aws/motific-preview/us-east-2/s3/motific-raw-documents.tfstate" # UPDATE ME.
     # This is the region where the backend S3 bucket is located.
     region = "us-east-2" # DO NOT CHANGE.
 
@@ -38,7 +38,7 @@ provider "aws" {
 
 module "s3" {
   source      = "git::https://github.com/cisco-eti/sre-tf-module-aws-s3.git?ref=1.0.2"
-  bucket_name = "motific_raw_documents" # The name of the S3 bucket.  S3 bucket names are required to be globally unique across all of AWS.
+  bucket_name = "motific-raw-documents" # The name of the S3 bucket.  S3 bucket names are required to be globally unique across all of AWS.
   #Continuous Security Buddy Tags.
   #For more information, see the CSB tagging Sharepoint page here:
   #https://cisco.sharepoint.com/Sites/CSB/SitePages/Security%20Tagging%20and%20Audit%20in%20AWS.aspx
