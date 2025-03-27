d042a41b95d1b602e38c2dd8910fccfaa7db4b3d
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 16:18:32 -0700

    fix: remove duplicate service role

--- aws_motific-prod_account_serviceroles_autoscaling-amazonaws-com.tf
+++ aws_motific-prod_account_serviceroles_autoscaling-amazonaws-com.tf
@@ -17,32 +17,17 @@ data "vault_generic_secret" "aws_infra_credential" {
   path     = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
 }
 
-# Provider for us-east-2
 provider "aws" {
-  alias       = "east"
-  region      = "us-east-2"
-  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-}
-
-# Provider for us-west-2
-provider "aws" {
-  alias       = "west"
-  region      = "us-west-2"
-  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = local.region
 }
 
 locals {
+  region           = "us-east-2"
   aws_account_name = "motific-prod"
 }
 
-resource "aws_iam_service_linked_role" "AWSServiceRoleForAutoScalingEast" {
-  provider = aws.east
-  aws_service_name = "autoscaling.amazonaws.com"
-}
-
-resource "aws_iam_service_linked_role" "AWSServiceRoleForAutoScalingWest" {
-  provider = aws.west
+resource "aws_iam_service_linked_role" "AWSServiceRoleForAutoScaling" {
   aws_service_name = "autoscaling.amazonaws.com"
 }
