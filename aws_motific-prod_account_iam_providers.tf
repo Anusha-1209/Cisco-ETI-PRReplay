5995e96f9c79c1dc9477742414791b258940fb1f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 16:57:33 -0700

    fix: add region to motf prod iam provider

--- aws_motific-prod_account_iam_providers.tf
+++ aws_motific-prod_account_iam_providers.tf
@@ -7,4 +7,5 @@ provider "vault" {
 provider "aws" {
   access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = "us-east-2"
 }
