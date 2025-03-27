44d52bbd2fd6723ec495bcfc03b7f268567f2985
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 15:28:50 -0700

    Creating Base Branch for PR 229

---
+++ aws_dragonfly-prod_eu-central-1_msk_dragonfly-msk-prod-eu1_providers.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "eu-central-1"
+  max_retries = 3
+
+  default_tags {
+    tags = {
+      ApplicationName    = "dragonfly-msk-prod-eu1"
+      CiscoMailAlias     = "eti-sre-admins@cisco.com"
+      DataClassification = "Cisco Confidential"
+      DataTaxonomy       = "Cisco Operations Data"
+      Environment        = "Prod"
+      ResourceOwner      = "ETI SRE"
+    }
+  }
+}
+
+provider "vault" {
+  alias     = "eticcprod"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+provider "vault" {
+  alias     = "dragonfly"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud/apps/dragonfly"
+}
