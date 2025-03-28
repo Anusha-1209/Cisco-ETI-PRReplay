711076803ff0d53957bd3e45bf5fe1933d2c20c5
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 17:13:08 -0700

    feat: add topics to opensearch pipeline
    Ref: EXP-2541
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-prod_eu-central-1_os_dragonfly-prod-eu-1-osis_main.tf
+++ aws_dragonfly-prod_eu-central-1_os_dragonfly-prod-eu-1-osis_main.tf
@@ -29,7 +29,7 @@ resource "awscc_osis_pipeline" "ingestion_pipelines" {
   pipeline_configuration_body = templatefile("./pipeline.yaml", {
     region          = data.aws_region.current.name
     sts_role_arn    = module.pipeline_role.iam_role_arn
-    opensearch_host = data.aws_opensearch_domain.dragonfly_prod_1_os.endpoint
+    opensearch_host = data.aws_opensearch_domain.dragonfly_prod_eu_1_os.endpoint
 
     msk_cluster_arn = data.aws_msk_cluster.dragonfly_msk_1.arn
     kafka_topics    = each.value.topics
@@ -41,12 +41,12 @@ resource "awscc_osis_pipeline" "ingestion_pipelines" {
   log_publishing_options = {
     is_logging_enabled = true
     cloudwatch_log_destination = {
-      log_group = aws_cloudwatch_log_group.dragonfly_prod_1_osis_log[each.key].name
+      log_group = aws_cloudwatch_log_group.dragonfly_prod_eu_1_osis_log[each.key].name
     }
   }
 }
 
-resource "aws_cloudwatch_log_group" "dragonfly_prod_1_osis_log" {
+resource "aws_cloudwatch_log_group" "dragonfly_prod_eu_1_osis_log" {
   for_each = local.pipelines
 
   name = "/aws/vendedlogs/OpenSearchIngestion/${each.key}/audit-logs"
