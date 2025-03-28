72355f75d404e7001ba4f2a4a78552a154d00e7c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-28 12:24:27 -0700

    fix(region)

--- aws_eticloud-plg-prod_us-east-1_iam_locals.tf
+++ aws_eticloud-plg-prod_us-east-1_iam_locals.tf
@@ -3,7 +3,7 @@ locals {
 
   cnapp_clusters = {
     cnapp-prod-eu = {
-      eks_oidc = "oidc.eks.us-east-2.amazonaws.com/id/28A49D0DC19E0AE06F2E38C0AD473F7D"
+      eks_oidc = "oidc.eks.eu-central-1.amazonaws.com/id/28A49D0DC19E0AE06F2E38C0AD473F7D"
     }
     cnapp-prod-us = {
       eks_oidc = "oidc.eks.us-east-2.amazonaws.com/id/EFF9B51923E64F3067C820180603F855"
