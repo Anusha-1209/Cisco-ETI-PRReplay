dfca6a7d7ea99271fa56cfc64f724f5a5dd4d805
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-28 15:53:30 -0700

    Merge branch 'main' into motific-update-al2-ami

---
+++ aws_lightspin-prod-eu_account_iam_sto-security-audit.tf
@@ -0,0 +1,22 @@
+data "aws_iam_policy" "ReadOnlyAccess" {
+  arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
+}
+
+# IAM Roles
+resource "aws_iam_role" "sto-security-audit-role" {
+  name                 = "sto-security-audit"
+  description          = "STO Security Audit SSO IAM role access"
+  path                 = "/"
+  max_session_duration = "3600"
+
+  force_detach_policies = false
+  permissions_boundary  = ""
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role_with_saml.json
+}
+
+resource "aws_iam_role_policy_attachment" "sto-security-audit-policy-attachment" {
+    role = aws_iam_role.sto-security-audit-role.name
+    policy_arn = data.aws_iam_policy.ReadOnlyAccess.arn
+}
+
