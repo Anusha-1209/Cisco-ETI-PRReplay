d131cfcfaf2d4da1ed05ac9cdddc17bd99bdf73f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-28 15:35:09 -0700

    Merge branch 'main' into recreate-cluster

---
+++ aws_eticloud-scratch-c_us-east-2_eks_backstage1_locals.tf
@@ -0,0 +1,7 @@
+# This file was created by Outshift Platform Self-Service automation.
+locals {
+  name             = "backstage1"
+  region           = "us-east-2"
+  aws_account_name = "eticloud-scratch-c"
+  vpc_cidr         = "10.0.0.0/16"
+}
