227b29ac9efea62bf9945e09f899edfd85fe085a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-28 15:02:16 -0700

    Merge branch 'main' into chore/update-labelbox-external-id

---
+++ aws_apisec-dev_eu-west-1_eks_e2e_locals.tf
@@ -0,0 +1,7 @@
+# This file was created by Outshift Platform Self-Service automation.
+locals {
+  name             = "e2e"
+  region           = "eu-west-1"
+  aws_account_name = "apisec-dev"
+  vpc_cidr         = "10.0.0.0/16"
+}
