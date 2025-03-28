95a9227a3c5defe1199f91c061df1dc336ae4148
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 22:35:08 -0700

    Update aws/motific-prod/us-east-2/msk/motf-prod-use2-1/backend.tf
    Co-authored-by: Sri Aradhyula <sraradhy@cisco.com>

--- aws_motific-prod_us-east-2_msk_motf-prod-use2-1_backend.tf
+++ aws_motific-prod_us-east-2_msk_motf-prod-use2-1_backend.tf
@@ -2,7 +2,8 @@ terraform {
   backend "s3" {
      # We separate the different environments into different buckets. The buckets are eticloud-tf-state-sandbox, eticloud-tf-state-nonprod, eticloud-tf-state-prod. The environment should match the CSBEnvironment below.
     bucket = "eticloud-tf-state-prod"
-    key    = "terraform-state/msk/us-east-2/motf-prod-use2-1-msk.tfstate" # note the path here. It should match the patten terraform_state/<service>/<region>/<name>.tfstate
+    # note the path here. It should match the patten terraform_state/<service>/<region>/<name>.tfstate
+    key    = "terraform-state/msk/us-east-2/motf-prod-use2-1-msk.tfstate"
     region = "us-east-2"                                                     # do not change
   }
 }
