6fd2799b98f4a04b6e5430cb5ffc1b27f932b220
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 22:02:32 -0700

    feat(oidc): add provider IAM

---
+++ aws_eticloud-plg-prod_us-east-1_iam_oidc.tf
@@ -0,0 +1,16 @@
+# required for the thumbprint list of each EKS cluster
+
+data "tls_certificate" "eks_tls_certificate" {
+  url = "https://${each.value.eks_oidc}"
+}
+
+resource "aws_iam_openid_connect_provider" "eks_cluster" {
+  for_each = local.cnapp_clusters
+  url      = "https://${each.value.eks_oidc}"
+  client_id_list = [
+    "sts.amazonaws.com",
+  ]
+  thumbprint_list = [
+    data.tls_certificate.eks_tls_certificate.certificates[0].sha1_fingerprint,
+  ]
+}
