4fc767584132ba526dc04cbb539c632749d5147f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 23:46:27 -0700

    fix(motific-staging): update sagemaker role policy

--- aws_motific-staging_us-east-2_sagemaker_motf-staging-sagemaker_iam.tf
+++ aws_motific-staging_us-east-2_sagemaker_motf-staging-sagemaker_iam.tf
@@ -4,7 +4,7 @@ resource "aws_iam_role" "motific-staging" {
   assume_role_policy = data.aws_iam_policy_document.sagemaker_assume_role.json
 }
 
-resource "aws_iam_policy" "motific-staging" {
+resource "aws_iam_policy" "motific-staging-s3" {
   name        = local.name
   path        = "/"
   description = "Policy for ${local.name}"
@@ -39,8 +39,46 @@ resource "aws_iam_policy" "motific-staging" {
   })
 }
 
-resource "aws_iam_policy_attachment" "motific-staging" {
-  name       = local.name
+resource "aws_iam_policy" "s3-cross-account-access" {
+  name        = local.name
+  path        = "/"
+  description = "Policy for ${local.name}"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+              "s3:*"
+          ],
+          "Resource": "arn:aws:s3:::vowel-dev-sagemaker/*"
+      },
+      {
+          "Sid": "VisualEditor0",
+          "Effect": "Allow",
+          "Action": [
+              "s3:ListBucket"
+          ],
+          "Resource": "arn:aws:s3:::vowel-dev-sagemaker"
+      }
+    ]
+  })
+}
+resource "aws_iam_policy_attachment" "motific-staging-s3" {
+  name       = "${local.name}-s3"
   roles      = [aws_iam_role.motific-staging.name]
-  policy_arn = aws_iam_policy.motific-staging.arn
+  policy_arn = aws_iam_policy.motific-staging-s3.arn
+}
+
+resource "aws_iam_policy_attachment" "motific-staging-sagemaker" {
+  name       = "${local.name}-sagemaker"
+  roles      = [aws_iam_role.motific-staging.name]
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSageMakerFullAccess"
+}
+
+resource "aws_iam_policy_attachment" "motific-staging-s3-cross-account-access" {
+  name       = "${local.name}-s3-cross-account-access"
+  roles      = [aws_iam_role.motific-staging.name]
+  policy_arn = aws_iam_policy.motific-staging-s3-cross-account-access.arn
 }
