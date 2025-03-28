5cef0ac6059fc410079e1fbbb01651fe39730c9e
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 22:12:42 -0700

    fix: typo

--- aws_motific-prod_us-east-2_s3_motf-prod-processed-documents_s3.tf
+++ aws_motific-prod_us-east-2_s3_motf-prod-processed-documents_s3.tf
@@ -15,7 +15,7 @@ data "vault_generic_secret" "aws_infra_credential" {
 terraform {
   backend "s3" {
     # This is the name of the backend S3 bucket.
-    bucket = "eticloud-tf-state-sprod" # UPDATE ME.
+    bucket = "eticloud-tf-state-prod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
     key = "terraform-state/aws/motific-prod/us-east-2/s3/motf-prod-processed-documents.tfstate" # UPDATE ME.
     # This is the region where the backend S3 bucket is located.
