245ad0abc3d59470484be83acb3a91018a9b4718
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 21:42:06 -0700

    feat(aws-iam-s3): draft IAM policy for plg prod

---
+++ aws_eticloud-plg-prod_us-east-1_iam_main.tf
@@ -0,0 +1,27 @@
+# IAM policy that allows to list a specific bucket and write objects to it
+resource "aws_iam_policy" "plg_write_to_s3" {
+  name        = "WriteToPLGAnalyticsS3Bucket"
+  description = "IAM policy that allows to write to a specific S3 bucket"
+
+  policy = jsonencode({
+    "Version" = "2012-10-17",
+    "Statement" = [
+      {
+        "Sid" = "ListObjectsInBucket",
+        "Effect" = "Allow",
+        "Action" = [
+          "s3:ListBucket"
+        ],
+        "Resource" = "arn:aws:s3:::eti-plg-analytics-s3-bucket/Rosey/*"
+      },
+       {
+        "Sid" = "AllObjectActions",
+        "Effect" = "Allow",
+        "Action" = [
+          "s3:PutObject"
+        ],
+        "Resource" = "*"
+      }
+    ]
+  })
+}
