8d78bd47fd08f5bbfe980706c7b7bd65d4b4da52
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 17:53:17 -0700

    feat(dragonfly-prod): add s3 buckets in eu-central-1
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-prod_eu-central-1_s3_dragonfly-kafka-connector-logs_locals.tf
+++ aws_dragonfly-prod_eu-central-1_s3_dragonfly-kafka-connector-logs_locals.tf
@@ -2,5 +2,5 @@ locals {
   aws_account_name = "dragonfly-prod"
   aws_region = "eu-central-1"
 
-  bucket_name = "${local.aws_account_name}-euc1--kafka-connector-log-files"
+  bucket_name = "${local.aws_account_name}-euc1-kafka-connector-log-files"
 }
