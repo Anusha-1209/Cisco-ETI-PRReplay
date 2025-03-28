81e0a16c8c745fdb0c8a7492bd9f0c516b501d81
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 18:35:41 -0700

    -

--- aws_outshift-common-staging_us-east-2_s3_requests-failover-marvin-prod-use2-1_main.tf
+++ aws_outshift-common-staging_us-east-2_s3_requests-failover-marvin-prod-use2-1_main.tf
@@ -17,7 +17,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket = "eticloud-tf-state-nonprod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key = "terraform-state/aws/outshift-common-staging/us-east-2/s3/athena-results-marvin-staging-use2-1.tfstate"
+    key = "terraform-state/aws/outshift-common-staging/us-east-2/s3/requests-failover-marvin-staging-use2-1.tfstate"
     # This is the region where the backend S3 bucket is located.
     region = "us-east-2" # DO NOT CHANGE.
 
@@ -38,7 +38,7 @@ provider "aws" {
 
 module "s3" {
   source      = "git::https://github.com/cisco-eti/sre-tf-module-aws-s3.git?ref=1.0.2"
-  bucket_name = "athena-results-marvin-staging-use2-1" # The name of the S3 bucket.  S3 bucket names are required to be globally unique across all of AWS.
+  bucket_name = "requests-failover-marvin-staging-use2-1" # The name of the S3 bucket.  S3 bucket names are required to be globally unique across all of AWS.
   #Continuous Security Buddy Tags.
   #For more information, see the CSB tagging Sharepoint page here:
   #https://cisco.sharepoint.com/Sites/CSB/SitePages/Security%20Tagging%20and%20Audit%20in%20AWS.aspx
