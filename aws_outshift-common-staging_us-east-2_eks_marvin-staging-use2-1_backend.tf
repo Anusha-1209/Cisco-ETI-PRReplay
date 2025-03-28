2a648b2407c2abd9e9ff0b64e8bd8987bcf204b2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 18:16:42 -0700

    marvin eks

--- aws_outshift-common-staging_us-east-2_eks_marvin-staging-use2-1_backend.tf
+++ aws_outshift-common-staging_us-east-2_eks_marvin-staging-use2-1_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # This is the name of the backend S3 bucket.
-    bucket = "eticloud-tf-state-staging"
+    bucket = "eticloud-tf-state-nonprod"
     # This is the path to the Terraform state file in the backend S3 bucket.
     key = "terraform-state/aws/outshift-common-staging/us-east-2/eks/marvin-staging-use2-1.tfstate"
     # This is the region where the backend S3 bucket is located.
