a1ba03fb1c3259ec0307a339bda19773f03cbe51
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-28 15:33:48 -0700

    fix: recreate hamzah's cluster

--- aws_eticloud-scratch-c_us-east-2_eks_backstage-poc_main.tf
+++ aws_eticloud-scratch-c_us-east-2_eks_backstage-poc_main.tf
@@ -2,7 +2,7 @@
 module "eks_all_in_one" {
   source                 = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
   name                   = local.name             # EKS cluster name
-  region                 = local.region           # AWS provider region
+  region                 = local.region            # AWS provider region
   aws_account_name       = local.aws_account_name # AWS account name
   cidr                   = local.vpc_cidr         # VPC CIDR
   cluster_version        = "1.29"   # EKS cluster version
