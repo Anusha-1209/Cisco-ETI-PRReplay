42baa6482894cda06c50c294b1e2902fe109a9ac
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 17:03:20 -0700

    Creating Base Branch for PR 222

---
+++ aws_dragonfly-prod_eu-central-1_os_dragonfly-prod-eu-1-osis_variables.tf
@@ -0,0 +1,30 @@
+variable "pipeline_name" {
+  description = "The name of the pipeline"
+  type        = string
+  default     = "osis-dragonfly-prd-eu1-kafka"
+}
+
+variable "domain_name" {
+  description = "The name of the domain"
+  type        = string
+  default     = "os-dragonfly-prod-eu1"
+}
+
+variable "msk_cluster_name" {
+  description = "The name of the MSK cluster"
+  type        = string
+  default     = "dragonfly-msk-prod-eu1"
+}
+
+variable "tags" {
+  description = "Tags to apply to all resources"
+  type        = map(string)
+  default = {
+    ApplicationName    = "osis-dragonfly-prod-eu1"
+    CiscoMailAlias     = "eti-sre-admins@cisco.com"
+    DataClassification = "Cisco Confidential"
+    DataTaxonomy       = "Cisco Operations Data"
+    Environment        = "Prod"
+    ResourceOwner      = "ETI SRE"
+  }
+}
