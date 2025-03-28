2dfae7c73bacc15964a1e6d086dbb77fbecf7610
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-28 13:54:16 -0700

    Merge branch 'main' into SRE-6866

--- aws_outshift-common-prod_us-east-2_eks_marvin-prod-use2-1_marvin-prod-use2-1-eks-iam_eks-marvin-custom-roles.tf
+++ aws_outshift-common-prod_us-east-2_eks_marvin-prod-use2-1_marvin-prod-use2-1-eks-iam_eks-marvin-custom-roles.tf
@@ -3,7 +3,7 @@ data "aws_eks_cluster" "cluster_marvin" {
   name = local.cluster_name
 }
 locals {
-  cluster_name = "marvin-prod-euc1-1" # The name of the associated EKS cluster. Must be updated
+  cluster_name = "marvin-prod-use2-1" # The name of the associated EKS cluster. Must be updated
   account_id = data.aws_caller_identity.current_outshift_common.account_id
   oidc_id    = trimprefix(data.aws_eks_cluster.cluster_marvin.identity[0].oidc[0].issuer, "https://")
 }
@@ -22,7 +22,7 @@ resource "aws_iam_policy" "aws_s3_read_write_fail_over_requests_policy" {
           "s3:List*",
           "s3:Describe*"
         ],
-        "Resource": "arn:aws:s3:::requests-failover-marvin-prod-euc1-1*"
+        "Resource": "arn:aws:s3:::requests-failover-marvin-prod-use2-1*"
       }
     ]
   })
@@ -80,7 +80,7 @@ resource "aws_iam_policy" "aws_s3-msk-connect-marvin-staging-1_policy" {
           "s3:List*",
           "s3:Describe*"
         ],
-        "Resource": "arn:aws:s3:::msk-connect-marvin-prod-euc1-1*"
+        "Resource": "arn:aws:s3:::msk-connect-marvin-prod-use2-1*"
       },
       {
         "Effect": "Allow",
@@ -90,7 +90,7 @@ resource "aws_iam_policy" "aws_s3-msk-connect-marvin-staging-1_policy" {
           "s3:List*",
           "s3:Describe*"
         ],
-        "Resource": "arn:aws:s3:::athena-results-marvin-prod-euc1-1*"
+        "Resource": "arn:aws:s3:::athena-results-marvin-prod-use2-1*"
       }
     ]
   })
