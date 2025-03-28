2e04e2b7d1cef6cce39493b9eda345b3513a4c8f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 22:21:32 -0700

    change-cluster-name-for-rds

--- aws_vowel-genai-dev_us-east-2_rds_motf-e2e-use2-1_backend.tf
+++ aws_vowel-genai-dev_us-east-2_rds_motf-e2e-use2-1_backend.tf
@@ -2,7 +2,7 @@
 terraform {
   backend "s3" {
     bucket = "eticloud-tf-state-nonprod" 
-    key    = "terraform-state/aurora-postgres/us-east-2/motific-dev-use2-1.tfstate"
+    key    = "terraform-state/aurora-postgres/us-east-2/motf-e2e-use2-1.tfstate"
     region = "us-east-2"
   }
 }
