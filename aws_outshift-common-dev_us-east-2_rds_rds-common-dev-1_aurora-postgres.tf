ea5dbd8d3a302184184263567e56e0914fc1bf8a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-28 15:13:28 -0700

    fix: update to latest rds module

--- aws_outshift-common-dev_us-east-2_rds_rds-common-dev-1_aurora-postgres.tf
+++ aws_outshift-common-dev_us-east-2_rds_rds-common-dev-1_aurora-postgres.tf
@@ -1,46 +1,52 @@
-# Atlantis has credentials to the eticloud AWS account. It uses those credentials to store and retrieve state information.
-# `Path=` specifies the path to credentials in Keeper. The assumed namespace is eticloud/eticcprod.
-# This data call is required for all accounts. The two current options are "scratch" (as below) and "prod".
-data "vault_generic_secret" "aws_infra_credential" {
-  path="secret/eticcprod/infra/prod/aws"
-}
-
-# Describes the statefile and table in the eticloud aws account. Each Atlantis project should have it's own statefile (key)
 terraform {
   backend "s3" {
-    bucket        = "eticloud-tf-state-nonprod" # We separate the different environments into different buckets. The buckets are eticloud-tf-state-sandbox, eticloud-tf-state-nonprod, eticloud-tf-state-prod. The environment should match the CSBEnvironment below.
-    key           = "terraform-state/aurora-postgres/us-east-2/rds-common-dev-1-rds.tfstate"  #note the path here. It should match the patten terraform_state/<service>/<region>/<name>.tfstate
-    region        = "us-east-2" #do not change
+    bucket  = "eticloud-tf-state-nonprod"                                                
+    key     = "terraform-state/aws/eticloud/us-east-2/rds/rds-common-dev-1.tfstate"     
+    region  = "us-east-2"                                                                  
   }
 }
 
-# Change this default to move the VPC into a a different region.
-variable "AWS_INFRA_REGION" {
-  description     = "AWS Region"
-  default         = "us-east-2"
+locals {
+  region           = "us-east-2"
+  aws_account_name = "outshift-common-dev"
+  data_vpc         = "eks-common-dev-1-data"
+  eks_vpc          = "eks-common-dev-1"
+  rds_name         = "rds-common-dev-1"
 }
 
-# By setting the AWS provider credentials via the data source above, we control in which account and region the resources get created.
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  provider    = vault.eticloud
+  path        = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
+}
+
 provider "aws" {
-  access_key      = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key      = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region          = var.AWS_INFRA_REGION
-  max_retries     = 3
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = local.region
+}
+
+data "aws_vpc" "eks_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [local.eks_vpc]
+  }
 }
 
 module "rds" {
-    source                = "git::https://wwwin-github.cisco.com/eti/sre-tf-module-aws-aurora-postgres?ref=1.0.2" # The reference specifies the version of the module.
-    vpc_name              = "eks-common-dev-1-data"           # the name of the VPC where the database instances and clusters will be created.
-    cluster_name          = "rds-common-dev-1"              # the name of the database instance
-    database_name         = "dev01"
-    # pg_engine_version  = "13.4"        # Options are 13.4, 12.8, 11.13. This module is specifically Aurora PostgreSQL.
-    db_instance_type      = "db.r5.xlarge" # Instance types list available here: https://aws.amazon.com/rds/instance-types/
-    CSBApplicationName    = "rds-common-dev-1-db"
-    CSBCiscoMailAlias     = "eti-sre@cisco.com"
-    CSBDataClassification = "Cisco Confidential"
-    CSBDataTaxonomy       = "Cisco Operations Data"
-    CSBEnvironment        = "NonProd"
-    CSBResourceOwner      = "eti"
+  source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-aurora-postgres?ref=1.1.0"
+  vpc_name          = local.data_vpc
+  database_name     = "postgressql"
+  db_instance_type  = "db.r5.xlarge"
+  cluster_name      = local.rds_name
+  secret_path       = "secret/eticcprod/infra/aurora-pg/us-east-2/outshft-common-dev/rds-common-dev-1"
+  db_engine_version = "15"
+  db_allowed_cidrs  = [
+    data.aws_vpc.eks_vpc.cidr_block,
+  ]
 }
-
-
