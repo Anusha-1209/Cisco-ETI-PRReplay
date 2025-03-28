47cb1ac39bce24009b126a774a4ffaf4ffb91c2a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-28 15:44:29 -0700

    fix: trying pattern from other cluster

--- aws_eticloud-scratch-c_us-east-2_eks_backstage01_main.tf
+++ aws_eticloud-scratch-c_us-east-2_eks_backstage01_main.tf
@@ -1,22 +1,19 @@
 # This file was created by Outshift Platform Self-Service automation.
 module "eks_all_in_one" {
-  source                 = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
-  name                   = local.name             # EKS cluster name
-  region                 = local.region           # AWS provider region
-  aws_account_name       = local.aws_account_name # AWS account name
-  cidr                   = local.vpc_cidr         # VPC CIDR
-  cluster_version        = "1.29"   # EKS cluster version
-  bastion_instance_count = 1                      # Bastion instance count
+  source = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
 
+  name             = local.name             # EKS cluster name
+  region           = local.region           # AWS provider region
+  aws_account_name = local.aws_account_name # AWS account name
+  cidr             = "10.0.0.0/16"          # VPC CIDR
+  cluster_version  = "1.29"                 # EKS cluster version
 
   # EKS Managed Private Node Group
-  instance_types          = ["t3.xlarge"]
-  ami_type                = "AMAZON_LINUX_2"
-  # skip_cisco_hardened_ami = "false"
-  min_size                = "1"
-  max_size                = "4"
-  desired_size            = "3"
-  create_karpenter_irsa   = "true"
-  create_alb_irsa         = "true"
-  create_otel_irsa        = "true"
-}
+  instance_types = ["m6a.xlarge"] # EKS instance types
+  min_size       = 1               # EKS node group min size
+  max_size       = 5               # EKS node group max size
+  desired_size   = 3               # EKS node group desired size
+
+  # Karpenter
+  create_karpenter_irsa = true # Create Karpenter IRSA
+} 
