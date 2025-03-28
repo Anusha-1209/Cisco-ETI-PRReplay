dc4c34462017693ebdd958aa88d7e5224b804c7c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 17:10:36 -0700

    feat: add topics to opensearch pipeline
    Ref: EXP-2541
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-prod_eu-central-1_os_dragonfly-prod-eu-1-osis_main.tf
+++ aws_dragonfly-prod_eu-central-1_os_dragonfly-prod-eu-1-osis_main.tf
@@ -1,24 +1,53 @@
-resource "awscc_osis_pipeline" "ingestion_pipeline" {
-  pipeline_name = var.pipeline_name
+locals {
+  pipelines = {
+    "dragonfly-osis-pipeline-1" = {
+
+      topics = [
+        "falco",
+        "monitoring",
+        "kg-edges",
+        "kg-nodes",
+      ],
+      min_units = 1,
+      max_units = 4,
+    },
+    "dragonfly-osis-pipeline-2" = {
+      topics = [
+        "threat",
+        "attack",
+      ]
+      min_units = 1,
+      max_units = 4,
+    },
+  }
+}
+
+resource "awscc_osis_pipeline" "ingestion_pipelines" {
+  for_each = local.pipelines
+
+  pipeline_name = each.key
   pipeline_configuration_body = templatefile("./pipeline.yaml", {
     region          = data.aws_region.current.name
     sts_role_arn    = module.pipeline_role.iam_role_arn
-    opensearch_host = data.aws_opensearch_domain.dragonfly_prod_eu_1_os.endpoint
+    opensearch_host = data.aws_opensearch_domain.dragonfly_prod_1_os.endpoint
 
-    msk_cluster_arn = data.aws_msk_cluster.dragonfly_msk_eu1.arn
-    kafka_topics = [
-      "falco",
-      "monitoring",
-    ]
+    msk_cluster_arn = data.aws_msk_cluster.dragonfly_msk_1.arn
+    kafka_topics    = each.value.topics
   })
 
-  min_units = 1
-  max_units = 4
+  min_units = each.value.min_units
+  max_units = each.value.max_units
 
   log_publishing_options = {
     is_logging_enabled = true
     cloudwatch_log_destination = {
-      log_group = aws_cloudwatch_log_group.dragonfly_prod_eu_1_osis_logs.name
+      log_group = aws_cloudwatch_log_group.dragonfly_prod_1_osis_log[each.key].name
     }
   }
+}
+
+resource "aws_cloudwatch_log_group" "dragonfly_prod_1_osis_log" {
+  for_each = local.pipelines
+
+  name = "/aws/vendedlogs/OpenSearchIngestion/${each.key}/audit-logs"
 }
