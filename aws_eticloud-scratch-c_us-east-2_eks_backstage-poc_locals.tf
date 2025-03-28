00a5d9fa54268fbb857a4c8be4d737f0ee17fb3a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-28 15:37:42 -0700

    chore: remove destroyed terraform

--- aws_eticloud-scratch-c_us-east-2_eks_backstage-poc_locals.tf
+++
@@ -1,7 +0,0 @@
-# This file was created by Outshift Platform Self-Service automation.
-locals {
-  name             = "backstage-poc"
-  region           = "us-east-2"
-  aws_account_name = "eticloud-scratch-c"
-  vpc_cidr         = "10.0.0.0/16"
-}
