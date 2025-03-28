c42fa4f83e02cdbb03b4a961a89ff9d7923d4c4c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-28 12:31:02 -0700

    Merge branch 'main' into SRE-6866

---
+++ aws_vowel-genai-dev_us-east-2_eks_motf-e2e-use2-1_locals.tf
@@ -0,0 +1,7 @@
+# This file was created by Outshift Platform Self-Service automation.
+locals {
+  name             = "motf-e2e-use2-1"
+  region           = "us-east-2"
+  aws_account_name = "vowel-genai-dev"
+  vpc_cidr         = "10.0.0.0/16"
+}
