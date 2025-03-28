e12589e8a0146d764c4ea20199197ee6453ff520
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-27 17:59:35 -0700

    first commit

--- aws_outshift-common-staging_serviceroles_autoscaling-amazonaws-com.tf
+++ aws_outshift-common-staging_serviceroles_autoscaling-amazonaws-com.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     # This is the name of the backend S3 bucket.
-    bucket = "eticloud-tf-state-prod"
+    bucket = "eticloud-tf-state-staging"
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key = "terraform-state/aws/outshift-common-prod/servicerole/autoscaling-amazonaws-com.tfstate"
+    key = "terraform-state/aws/outshift-common-staging/servicerole/autoscaling-amazonaws-com.tfstate"
     # This is the region where the backend S3 bucket is located.
     region = "us-east-2"
   }
