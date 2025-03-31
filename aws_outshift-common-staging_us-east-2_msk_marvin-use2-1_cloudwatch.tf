0c8d580f638c16ca325e8be6c068aab6e01abeca
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 10:07:29 -0700

    Merge remote-tracking branch 'origin' into marvin_dev
    # Conflicts:
    #	atlantis.yaml

---
+++ aws_outshift-common-staging_us-east-2_msk_marvin-use2-1_cloudwatch.tf
@@ -0,0 +1,3 @@
+resource "aws_cloudwatch_log_group" "broker_logs" {
+  name = "marvin-staging-use2-1-msk-broker-logs"
+}
---
+++ aws_outshift-common-staging_us-east-2_msk_marvin-use2-1_kms.tf
@@ -0,0 +1,3 @@
+resource "aws_kms_key" "encryption_key" {
+  description = "marvin-staging-use2-1-msk MSK cluster encryption key"
+}
