383b4f72097799038a5c5e0fb02eba1e0a8ba591
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 09:22:50 -0700

    chore: update atlantis projects

--- aws_outshift-common-prod_us-east-2_rds_rds-common-prod-1_main.tf
+++ aws_outshift-common-prod_us-east-2_rds_rds-common-prod-1_main.tf
@@ -1,14 +1,14 @@
 terraform {
   backend "s3" {
     bucket  = "eticloud-tf-state-prod"                                                
-    key     = "terraform-state/aws/eticloud/us-east-2/rds/rds-common-prod-1.tfstate"     
+    key     = "terraform-state/aws/outshift-common-prod/us-east-2/rds/rds-common-prod-1.tfstate"     
     region  = "us-east-2"                                                                  
   }
 }
 
 locals {
   region           = "us-east-2"
-  aws_account_name = "eticloud"
+  aws_account_name = "outshift-common-prod"
   data_vpc         = "eks-common-prod-1-data"
   eks_vpc          = "eks-common-prod-1"
   rds_name         = "rds-common-prod-1"
@@ -44,7 +44,7 @@ module "rds" {
   database_name     = "postgressql"
   db_instance_type  = "db.r5.xlarge"
   cluster_name      = local.rds_name
-  secret_path       = "secret/eticcprod/infra/aurora-pg/us-east-2/eticloud/rds-common-prod-1"
+  secret_path       = "secret/eticcprod/infra/aurora-pg/us-east-2/outshift-common-prod/rds-common-prod-1"
   db_engine_version = "15"
   db_allowed_cidrs  = [
     data.aws_vpc.eks_vpc.cidr_block,
