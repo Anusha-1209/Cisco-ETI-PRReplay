d513e858ac620b1b574ff58ce3cfc29c145f515d
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 13:16:00 -0700

    fix: updates

--- aws_motific-prod_us-west-2_eks_motf-prod-usw2-1_main.tf
+++ aws_motific-prod_us-west-2_eks_motf-prod-usw2-1_main.tf
@@ -25,8 +25,4 @@ module "eks_all_in_one" {
   min_size       = 2                        # EKS node group min size
   max_size       = 15                       # EKS node group max size
   desired_size   = 2                        # EKS node group desired size
-  allow_eks_api_internet_access = false     # Allow EKS API internet access
-
-  # Karpenter is setup by default
-  # create_karpenter_irsa = true              # Create Karpenter IRSA
 }
