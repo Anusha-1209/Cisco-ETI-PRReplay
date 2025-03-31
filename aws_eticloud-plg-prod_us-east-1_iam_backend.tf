ede5e919c4b8b6dbc16a4d4561edd386cdd7e17c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 10:16:46 -0700

    Merge branch 'main' into marvin_dev

---
+++ aws_eticloud-plg-prod_us-east-1_iam_backend.tf
@@ -0,0 +1,8 @@
+# AWS account: eticloud
+terraform {
+  backend "s3" {
+    bucket = "eticloud-tf-state-prod"
+    key    = "terraform-state/eticloud-plg-prod/iam/iam-roles.tfstate"
+    region = "us-east-2"
+  }
+}
---
+++ aws_eticloud-plg-prod_us-east-1_iam_providers.tf
@@ -0,0 +1,25 @@
+provider "vault" {
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud/eticcprod"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  path = "secret/eticcprod/infra/eticloud-plg-prod/aws"
+}
+
+provider "aws" {
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = "us-east-1"
+
+  default_tags {
+    tags = {
+      ApplicationName    = "ETI Plg prod"
+      CiscoMailAlias     = "eti-sre-admins@cisco.com"
+      DataClassification = "Cisco Confidential"
+      DataTaxonomy       = "Cisco Operations Data"
+      EnvironmentName    = "Prod"
+      ResourceOwner      = "ETI SRE"
+    }
+  }
+}
