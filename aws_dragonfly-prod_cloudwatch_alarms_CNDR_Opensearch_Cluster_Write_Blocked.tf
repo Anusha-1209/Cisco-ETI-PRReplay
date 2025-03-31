0c4f0c303f24cdd5829396db93a584b9155037c7
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 13:31:23 -0700

    fix: update tf

--- aws_dragonfly-prod_cloudwatch_alarms_CNDR_Opensearch_Cluster_Write_Blocked.tf
+++ aws_dragonfly-prod_cloudwatch_alarms_CNDR_Opensearch_Cluster_Write_Blocked.tf
@@ -3,7 +3,8 @@ resource "aws_cloudwatch_metric_alarm" "CNDR_Opensearch_Cluster_Index_Writes_Blo
   comparison_operator       = "GreaterThanOrEqualToThreshold"
   evaluation_periods        = 2
   metric_name               = "ClusterIndexWritesBlocked"
-  namespace                 = "AWS/OPENSEARCH"
+
+  namespace                 = "AWS/ES"
   period                    = 120
   statistic                 = "Average"
   threshold                 = 1
@@ -12,4 +13,21 @@ resource "aws_cloudwatch_metric_alarm" "CNDR_Opensearch_Cluster_Index_Writes_Blo
   ok_actions                = [aws_sns_topic.dragonfly-urgent-pagerduty.arn]
   alarm_description         = "This metric monitors the of blocked index writes in the OpenSearch cluster."
   insufficient_data_actions = []
+
+  metric_query {
+    id = "m1"
+
+    metric {
+      metric_name = "ClusterIndexWritesBlocked"
+      namespace   = "AWS/ES"
+      period      = 300
+      stat        = "Maximum"
+      unit        = "Count"
+
+      dimensions = {
+        DomainName = "os-dragonfly-prod-1"
+        ClientId = "651416187950"
+      }
+    }
+  }
 }
