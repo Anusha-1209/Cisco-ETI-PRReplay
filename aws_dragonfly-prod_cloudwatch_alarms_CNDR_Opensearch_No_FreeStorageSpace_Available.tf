aed07fea356658acc3fc32561aff11a7be76984f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 13:34:08 -0700

    fix: update tf

--- aws_dragonfly-prod_cloudwatch_alarms_CNDR_Opensearch_No_FreeStorageSpace_Available.tf
+++ aws_dragonfly-prod_cloudwatch_alarms_CNDR_Opensearch_No_FreeStorageSpace_Available.tf
@@ -2,10 +2,6 @@ resource "aws_cloudwatch_metric_alarm" "CNDR_Opensearch_No_FreeStorageSpace_Avai
   alarm_name                = "CNDR_Opensearch_No_FreeStorageSpace_Available"
   comparison_operator       = "LessThanOrEqualToThreshold"
   evaluation_periods        = 2
-  metric_name               = "FreeStorageSpace"
-  namespace                 = "AWS/OPENSEARCH"
-  period                    = 120
-  statistic                 = "Average"
   threshold                 = 50000
   alarm_actions             = [aws_sns_topic.dragonfly-urgent-pagerduty.arn]
   ok_actions                = [aws_sns_topic.dragonfly-urgent-pagerduty.arn]
