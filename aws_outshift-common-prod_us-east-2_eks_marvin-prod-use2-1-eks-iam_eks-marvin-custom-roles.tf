0e4cbef3d283bb2755b4df542c8c326182af52e6
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 12:39:37 -0700

    -

--- aws_outshift-common-prod_us-east-2_eks_marvin-prod-use2-1-eks-iam_eks-marvin-custom-roles.tf
+++ aws_outshift-common-prod_us-east-2_eks_marvin-prod-use2-1-eks-iam_eks-marvin-custom-roles.tf
@@ -20,7 +20,7 @@ resource "aws_iam_policy" "aws_rds_iam_connect_policy" {
           "rds-db:connect"
         ],
         "Resource": [
-          "arn:aws:rds-db:us-east-2:${local.account_id}:dbuser:db-7VAH3YY73T2GWGMAFOMZYBO65M/*"
+          "arn:aws:rds-db:us-east-2:${local.account_id}:dbuser:cluster-PWCSZNHVYBDK4C3TACFRRKDY3M/*"
         ]
       }
     ]
