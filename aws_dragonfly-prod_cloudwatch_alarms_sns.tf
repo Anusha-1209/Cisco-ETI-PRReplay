efe1f4b7078152d5719abf91e9c567220e8ec8c3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 13:30:04 -0700

    fix: update tf

--- aws_dragonfly-prod_cloudwatch_alarms_sns.tf
+++ aws_dragonfly-prod_cloudwatch_alarms_sns.tf
@@ -2,7 +2,8 @@ resource "aws_sns_topic" "dragonfly-urgent-pagerduty" {
   name = "dragonfly-urgent-pagerduty"
 }
 resource "aws_sns_topic_subscription" "dragonfly-urgent-pagerduty-email-target" {
-  topic_arn = aws_sns_topic.dragonfly-urgent-pagerduty.arn
-  protocol  = "email"
-  endpoint  = "panoptica-cndr-urgent@cisco-eti.pagerduty.com"
+  topic_arn              = aws_sns_topic.dragonfly-urgent-pagerduty.arn
+  protocol               = "email"
+  endpoint               = "panoptica-cndr-urgent@cisco-eti.pagerduty.com"
+  endpoint_auto_confirms = true
 }
