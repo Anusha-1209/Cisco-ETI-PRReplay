49de0247a50f32ff546dea3d9c16cd14bce3903f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 13:14:05 -0700

    fix: update cluster

--- aws_motific-prod_us-west-2_eks_motf-prod-usw2-1_main.tf
+++ aws_motific-prod_us-west-2_eks_motf-prod-usw2-1_main.tf
@@ -21,11 +21,12 @@ module "eks_all_in_one" {
   cluster_version  = "1.28"                 # EKS cluster version
 
   # EKS Managed Private Node Group
-  instance_types = ["m6a.2xlarge"] # EKS instance types
-  min_size       = 5               # EKS node group min size
-  max_size       = 15              # EKS node group max size
-  desired_size   = 8               # EKS node group desired size
+  instance_types = ["m6a.2xlarge"]          # EKS instance types
+  min_size       = 2                        # EKS node group min size
+  max_size       = 15                       # EKS node group max size
+  desired_size   = 2                        # EKS node group desired size
+  allow_eks_api_internet_access = false     # Allow EKS API internet access
 
-  # Karpenter
-  create_karpenter_irsa = true # Create Karpenter IRSA
+  # Karpenter is setup by default
+  # create_karpenter_irsa = true              # Create Karpenter IRSA
 }
