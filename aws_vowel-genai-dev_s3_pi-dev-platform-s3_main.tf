16a0d83e90f2ca9a385bc6cec87bb6c3b528135a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 09:50:37 -0700

    chore: adding pi-dev s3-bucket and iam role

---
+++ aws_vowel-genai-dev_s3_pi-dev-platform-s3_main.tf
@@ -0,0 +1,48 @@
+# This file was created by Outshift Platform Self-Service automation.
+terraform {
+  backend "s3" {
+    bucket = "eticloud-tf-state-nonprod"                                                    # We separate the different levels of development into different buckets. The buckets are eticloud-tf-state-sandbox, eticloud-tf-state-nonprod, eticloud-tf-state-prod. The environment should match the CSBEnvironment below.
+    key    = "terraform-state/vowel-genai-dev/us-east-2/s3/pi-dev-platform-s3.tfstate" # #note the path here. It should match the pattern terraform_state/<service>/<region>/<name>.tfstate
+    region = "us-east-2"                                                                    # Do not change without talking to the SRE team.
+  }
+}
+provider "vault" {
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+  alias     = "eticloud"
+}
+data "vault_generic_secret" "aws_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/vowel-genai-dev/terraform_admin"
+}
+
+provider "aws" {
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "us-east-2"
+  max_retries = 3
+  default_tags {
+    tags = {
+      ApplicationName    = "Prompt-Intel S3 Bucket"
+      CiscoMailAlias     = "eti-sre-admins@cisco.com"
+      DataClassification = "Cisco Restricted"
+      DataTaxonomy       = "Cisco Operations Data"
+      Environment        = "NonProd"
+      ResourceOwner      = "Motific"
+
+      IntendedPublic = "False"
+
+    }
+  }
+}
+
+module "s3" {
+  source                = "git::https://github.com/cisco-eti/sre-tf-module-aws-s3.git?ref=1.0.2"
+  bucket_name           = "pi-dev-platform-s3"
+  CSBApplicationName    = "Prompt-Intel S3 Bucket"
+  CSBCiscoMailAlias     = "eti-sre-admins@cisco.com"
+  CSBDataClassification = "Cisco Restricted"
+  CSBDataTaxonomy       = "Cisco Operations Data"
+  CSBEnvironment        = "NonProd"
+  CSBResourceOwner      = "Motific"
+}
