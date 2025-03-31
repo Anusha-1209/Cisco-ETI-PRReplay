77c71ac708ef7213bf1e96f120179ffde8351607
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 13:44:08 -0700

    chore: adding content safety motific-staging

---
+++ azure_motific-staging_providers.tf
@@ -0,0 +1,24 @@
+data "vault_generic_secret" "azure_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/azure/terraform_admin"
+}
+
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+provider "vault" {
+  alias     = "motific"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud/apps/vowel"
+}
+
+provider "azurerm" {
+  subscription_id = "c27de96e-9793-41d8-8ec7-7499674801bf"
+  client_id       = data.vault_generic_secret.azure_infra_credential.data["appId"]
+  client_secret   = data.vault_generic_secret.azure_infra_credential.data["password"]
+  tenant_id       = data.vault_generic_secret.azure_infra_credential.data["tenant"]
+  features {}
+}
