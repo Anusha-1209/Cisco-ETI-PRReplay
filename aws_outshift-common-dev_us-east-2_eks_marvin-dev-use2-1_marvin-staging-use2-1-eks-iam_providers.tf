f1ff61598da6fb267939f372cba1b36464953786
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 10:03:41 -0700

    first commit

--- aws_outshift-common-dev_us-east-2_eks_marvin-dev-use2-1_marvin-staging-use2-1-eks-iam_providers.tf
+++ aws_outshift-common-dev_us-east-2_eks_marvin-dev-use2-1_marvin-staging-use2-1-eks-iam_providers.tf
@@ -1,22 +1,28 @@
 provider "vault" {
-  alias     = "eticloud"
   address   = "https://keeper.cisco.com"
   namespace = "eticloud"
+}
+
+
+data "vault_generic_secret" "aws_infra_credential" {
+  path     = "secret/infra/aws/outshift-common-dev/terraform_admin"
 }
 
 provider "aws" {
   access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region      = "us-east-2"
+  region      = "us-east-2" # Must match the region where the EKS cluster and VPC are created.
   max_retries = 3
   default_tags {
+    # These tags are required for security compliance.
+    # For more information on Data Classification and Data Taxonomy, please talk to the SRE team.
     tags = {
-      ApplicationName    = "marvin-dev-use2-1"
+      ApplicationName    = "${local.cluster_name} IAM Roles"
       CiscoMailAlias     = "eti-sre-admins@cisco.com"
       DataClassification = "Cisco Confidential"
       DataTaxonomy       = "Cisco Operations Data"
       Environment        = "NonProd"
-      ResourceOwner      = "Outshift SRE"
+      ResourceOwner      = "ETI SRE"
     }
   }
 }
