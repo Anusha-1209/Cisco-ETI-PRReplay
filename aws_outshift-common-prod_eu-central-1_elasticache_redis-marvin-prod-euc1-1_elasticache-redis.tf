6fecc65cf7db8ce9e23071cd1701728006ebe8d0
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 11:52:30 -0700

    -

--- aws_outshift-common-prod_eu-central-1_elasticache_redis-marvin-prod-euc1-1_elasticache-redis.tf
+++ aws_outshift-common-prod_eu-central-1_elasticache_redis-marvin-prod-euc1-1_elasticache-redis.tf
@@ -18,7 +18,7 @@ locals {
 provider "aws" {
   access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region     = "eu-central-1"
+  region     = "us-east-2"
   default_tags {
     tags = {
       ApplicationName    = local.elasticache_name
