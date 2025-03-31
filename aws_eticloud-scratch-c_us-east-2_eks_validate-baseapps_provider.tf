a2edb05e90ec875067082196718b501ad067a1c6
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 15:02:33 -0700

    fix: update tf

--- aws_eticloud-scratch-c_us-east-2_eks_validate-baseapps_provider.tf
+++ aws_eticloud-scratch-c_us-east-2_eks_validate-baseapps_provider.tf
@@ -27,39 +27,3 @@ provider "aws" {
     }
   }
 }
-
-data "vault_generic_secret" "argocd_aws_infra_credential" {
-  provider = vault.eticloud
-  path     = "secret/infra/aws/eticloud-preprod/terraform_admin"
-}
-provider "aws" {
-  alias       = "argocd"
-  access_key  = data.vault_generic_secret.argocd_aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key  = data.vault_generic_secret.argocd_aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region      = "us-east-2"
-  max_retries = 3
-  default_tags {
-    tags = {
-      ApplicationName    = "Self-Service EKS Cluster"
-      CiscoMailAlias     = "sraradhy@cisco.com"
-      DataClassification = "Cisco Restricted"
-      DataTaxonomy       = "Cisco Operations Data"
-      Environment        = "NonProd"
-      ResourceOwner      = "Sri_Aradhyula"
-    }
-  }
-}
-
-provider "kubernetes" {
-  alias                  = "target"
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
-  token                  = data.aws_eks_cluster_auth.cluster.token
-}
-
-provider "kubernetes" {
-  alias                  = "argocd"
-  host                   = data.aws_eks_cluster.argocd.endpoint
-  cluster_ca_certificate = base64decode(data.vault_generic_secret.argocd_cluster_certificate.data["b64certificate"])
-  token                  = data.aws_eks_cluster_auth.argocd.token
-}
