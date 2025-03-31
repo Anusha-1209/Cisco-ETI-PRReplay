2e3020f867300b2dc497ec09aa1aca576aa71e50
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 15:55:11 -0700

    adding rosey postgress dev

--- aws_rosey-test_eu-west-1_rds_rosey-dev-euw1-1_backend.tf
+++
@@ -1,8 +0,0 @@
-
-terraform {
-  backend "s3" {
-    bucket = "eticloud-tf-state-nonprod" 
-    key    = "terraform-state/aurora-postgres/dev/eu-west-1/rosey.tfstate"
-    region = "us-east-2"
-  }
-}
