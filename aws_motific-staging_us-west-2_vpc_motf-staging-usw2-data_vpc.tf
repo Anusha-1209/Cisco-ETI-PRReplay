5282c14204262f851d27104ecb82c561f081db58
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 15:27:13 -0700

    fix(vpc): region

--- aws_motific-staging_us-west-2_vpc_motf-staging-usw2-data_vpc.tf
+++ aws_motific-staging_us-west-2_vpc_motf-staging-usw2-data_vpc.tf
@@ -37,7 +37,7 @@ provider "aws" {
 
 module "vpc" {
   source                          = "git::https://github.com/cisco-eti/sre-tf-module-aws-vpc?ref=2.0.6"
-  region                          = "us-east-2"
+  region                          = "us-west-2"
   vpc_cidr                        = "10.3.0.0/16"
   vpc_name                        = "motf-staging-usw2-data"
   cluster_name                    = "motf-staging-usw2-data"
