a237a1cfd43db543ceb1f99f2a6ad8d53804b8f7
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 15:06:58 -0700

    chore: eso created for interns namespace

--- aws_eticloud-scratch-c_us-east-2_eks_backstage01_eso.tf
+++ aws_eticloud-scratch-c_us-east-2_eks_backstage01_eso.tf
@@ -16,5 +16,15 @@ module "eso_eticloud" {
   vault_namespace = "eticloud"
   kubernetes_host = data.aws_eks_cluster.cluster.endpoint
   kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
-  policies        = ["external-secrets-dev"]
+  policies        = ["external-secrets-${local.name}"]
+}
+
+module "eso_eticloud_apps_interns" {
+  source = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
+
+  cluster_name    = local.name
+  vault_namespace = "eticloud/apps/interns"
+  kubernetes_host = data.aws_eks_cluster.cluster.endpoint
+  kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
+  policies        = ["external-secrets-${local.name}"]
 }
