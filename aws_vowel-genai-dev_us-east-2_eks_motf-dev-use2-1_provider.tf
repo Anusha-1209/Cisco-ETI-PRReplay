b8cf2202b632ace35f2074e4d4e451c97450656b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 11:16:57 -0700

    chore: fix motific provider issue

--- aws_vowel-genai-dev_us-east-2_eks_motf-dev-use2-1_provider.tf
+++ aws_vowel-genai-dev_us-east-2_eks_motf-dev-use2-1_provider.tf
@@ -4,6 +4,13 @@ provider "aws" {
   secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
   region      = "us-east-2"
   max_retries = 3
+
+provider "aws" {
+  alias      = "target"
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = local.region
+}
   default_tags {
     tags = {
       ApplicationName    = "Self-Service EKS Cluster"
