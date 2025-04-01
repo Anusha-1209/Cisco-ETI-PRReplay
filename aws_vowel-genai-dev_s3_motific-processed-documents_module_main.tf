d9e7199d574cc95a530f066e4f146bd2eb55d879
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 19:37:12 -0700

    fix: atlantis sub-dir scan

---
+++ aws_vowel-genai-dev_s3_motific-processed-documents_module_main.tf
@@ -0,0 +1,34 @@
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/vowel-genai-dev/terraform_admin"
+}
+terraform {
+  backend "s3" {
+    # This is the name of the backend S3 bucket.
+    bucket = "eticloud-tf-state-nonprod" # UPDATE ME.
+    # This is the path to the Terraform state file in the backend S3 bucket.
+    key = "terraform-state/aws/vowel-genai-dev/us-east-2/s3/motific-processed-documents.tfstate" # UPDATE ME.
+    # This is the region where the backend S3 bucket is located.
+    region = "us-east-2" # DO NOT CHANGE.
+
+  }
+}
+variable "AWS_INFRA_REGION" {
+  description = "AWS Region"
+  default     = "us-east-2" #Set the region for the resources to be created.
+}
+provider "aws" {
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = "us-east-2"
+}
+module "motific-processed-documents"{
+  source = "../"
+
+}
