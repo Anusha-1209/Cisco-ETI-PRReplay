53051beae8183c4cf0423e1cee0311f3f0a532fb
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 17:48:08 -0700

    fix: reconcile tf

--- aws_outshift-common-staging_iam_sso-roles_data.tf
+++ aws_outshift-common-staging_iam_sso-roles_data.tf
@@ -20,14 +20,5 @@ data "aws_iam_policy_document" "assume_role_with_saml" {
     }
   }
 
-  statement {
-    effect = "Allow"
 
-    actions = ["sts:AssumeRole"]
-    principals {
-      type = "AWS"
-
-      identifiers = ["arn:aws:iam::${local.account_id}:root"]
-    }
-  }
 }
