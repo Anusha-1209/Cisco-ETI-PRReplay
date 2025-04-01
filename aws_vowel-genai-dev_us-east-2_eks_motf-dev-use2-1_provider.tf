6ffc5adc963ff7639f533601b13353fdb8c07879
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 11:15:18 -0700

    chore: correct motf-dev-use2-1 provider

--- aws_vowel-genai-dev_us-east-2_eks_motf-dev-use2-1_provider.tf
+++ aws_vowel-genai-dev_us-east-2_eks_motf-dev-use2-1_provider.tf
@@ -1,8 +1,19 @@
 provider "aws" {
-  alias      = "target"
-  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region     = local.region
+  alias       = "eks"
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "us-east-2"
+  max_retries = 3
+  default_tags {
+    tags = {
+      ApplicationName    = "Self-Service EKS Cluster"
+      CiscoMailAlias     = "sraradhy@cisco.com"
+      DataClassification = "Cisco Restricted"
+      DataTaxonomy       = "Cisco Operations Data"
+      Environment        = "NonProd"
+      ResourceOwner      = "Sri_Aradhyula"
+    }
+  }
 }
 
 provider "vault" {
