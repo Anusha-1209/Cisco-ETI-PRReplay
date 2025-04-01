f7899c471d51e934e8d232d937181d440ab256d3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 00:01:28 -0700

    feat!: create new global rds setup for common dev clusters

---
+++ aws_outshift-common-dev_global_rds_global-rds-common-dev-1_aurora-postgres.tf
@@ -0,0 +1,83 @@
+terraform {
+  backend "s3" {
+    bucket  = "eticloud-tf-state-nonprod"                                                
+    key     = "terraform-state/aws/eticloud/global/rds/global-rds-common-dev-1.tfstate"     
+    region  = "us-east-2"                                                                  
+  }
+}
+
+locals {
+  region             = "us-east-2"
+  aws_account_name   = "outshift-common-dev"
+  data_primary_vpc   = "common-dev-vpc-data"
+  data_secondary_vpc = "common-dev-vpc-data"
+  eks_vpc            = "comn-dev-use2-1"
+  rds_name           = "rds-common-dev-1"
+}
+
+#
+resource "aws_rds_global_cluster" "rds_global" {
+  # global RDS cluster aren't tied to a region so the provider should not matter here
+  provider                     = aws.primary
+  global_cluster_identifier    = "global-rds-common-dev-1"
+  database_name                = "postgressql"
+  force_destroy                = true
+  source_db_cluster_identifier = module.rds_primary.cluster_arn
+  depends_on                   = [module.rds_primary]
+}
+
+resource "aws_kms_key" "primary" {
+  provider     = aws.primary
+  description  = "Multi-Region primary key for RDS global"
+  multi_region = true
+}
+
+resource "aws_kms_replica_key" "secondary" {
+  provider        = aws.secondary
+  description     = "Multi-Region replica key"
+  primary_key_arn = aws_kms_key.primary.arn
+}
+
+# Primary region us-east-2
+module "rds_primary" {
+  providers = {
+    aws = aws.primary
+  }
+  source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-aurora-postgres?ref=2.0.0-rc1"
+  vpc_name          = local.data_primary_vpc
+  database_name     = "postgressql"
+  db_instance_type  = "db.r5.xlarge"
+  cluster_name      = "global-rds-common-dev-use2-1"
+  kms_key_id        = aws_kms_key.primary.arn
+  secret_path       = "secret/dev/infra/aurora-pg/us-east-2/outshift-common-dev/global-rds-common-dev-use2-1"
+  db_engine_version = "15.4"
+  db_allowed_cidrs  = [
+    data.aws_eks_primary_vpc.eks_vpc.cidr_block,
+    data.aws_eks_secondary_vpc.eks_vpc.cidr_block,
+  ]
+}
+
+# Secondary region us-east-2
+module "rds_secondary" {
+  providers = {
+    aws = aws.secondary
+  }
+  source                    = "git::https://github.com/cisco-eti/sre-tf-module-aws-aurora-postgres?ref=2.0.0-rc1"
+  vpc_name                  = local.data_secondary_vpc
+  database_name             = null
+  global_cluster_identifier = aws_rds_global_cluster.rds_global.id
+  is_primary_cluster        = false
+  db_instance_type          = "db.r5.xlarge"
+  # headless, no instances
+  instances                 = {}
+  cluster_name              = "global-rds-common-dev-usw2-1"
+  kms_key_id                = aws_kms_replica_key.secondary.arn
+  secret_path               = "secret/dev/infra/aurora-pg/us-west-2/outshift-common-dev/global-rds-common-dev-usw2-1"
+  db_engine_version         = "15.4"
+  db_allowed_cidrs          = [
+    data.aws_eks_primary_vpc.eks_vpc.cidr_block,
+    data.aws_eks_secondary_vpc.eks_vpc.cidr_block,
+  ]
+
+  depends_on = [module.rds_primary]
+}
---
+++ aws_outshift-common-dev_global_rds_global-rds-common-dev-1_providers.tf
@@ -0,0 +1,40 @@
+provider "vault" {
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+provider "aws" {
+  alias       = "primary"
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "us-east-2"
+  max_retries = 3
+  default_tags {
+    tags = {
+      ApplicationName    = "global-rds-common-dev-1_primary"
+      CiscoMailAlias     = "eti-sre-admins@cisco.com"
+      DataClassification = "Cisco Confidential"
+      DataTaxonomy       = "Cisco Operations Data"
+      EnvironmentName    = "NonProd"
+      ResourceOwner      = "ETI SRE"
+    }
+  }
+}
+
+provider "aws" {
+  alias       = "secondary"
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "us-west-2"
+  max_retries = 3
+  default_tags {
+    tags = {
+      ApplicationName    = "global-rds-common-dev-1_secondary"
+      CiscoMailAlias     = "eti-sre-admins@cisco.com"
+      DataClassification = "Cisco Confidential"
+      DataTaxonomy       = "Cisco Operations Data"
+      EnvironmentName    = "NonProd"
+      ResourceOwner      = "ETI SRE"
+    }
+  }
+}
