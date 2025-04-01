3a92aa78ba2ab18a80b5dbed36754b64f8ffcc90
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 12:03:23 -0700

    fix(rosey-logs): specify exact service account

--- aws_cnapp-prod_rosey-logs_locals.tf
+++ aws_cnapp-prod_rosey-logs_locals.tf
@@ -4,11 +4,13 @@ locals {
   # EKS clsuter region and OIDC provider id
   clusters = {
     "cnapp-prod-us" = {
+      name             = "cnapp-prod-use2-1"
       region           = "us-east-2"
       region_prefix    = "us"
       oidc_provider_id = "EFF9B51923E64F3067C820180603F855"
     }
     "cnapp-prod-eu" = {
+      name             = "cnapp-prod-euc1-1"
       region           = "eu-central-1"
       region_prefix    = "eu"
       oidc_provider_id = "28A49D0DC19E0AE06F2E38C0AD473F7D"
