08ba6f8abde5e76c91e2de18dbec417f8d4c2e7b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 21:23:54 -0700

    fix: update to latest tag

--- aws_outshift-common-staging_us-west-2_vpc-peering_compute-to-data_vpc-peering.tf
+++ aws_outshift-common-staging_us-west-2_vpc-peering_compute-to-data_vpc-peering.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 module "sre_tf_module_multi_region_vpc_peering" {
-  source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering"
+  source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=latest"
   aws_account_name   = "outshift-common-staging"
   accepter_vpc_name  = "comn-staging-usw2-1"
   requester_vpc_name = "common-staging-vpc-data"
