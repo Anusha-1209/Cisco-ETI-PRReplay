480b08e1b36548b90a4865ed280e386ca8b223e1
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 13:46:55 -0700

    chore(dragonfly): move EKS clusters from sre-tf-infra
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-prod_us-east-2_eks_dragonfly-prod-1-cm_dependencies.tf
+++ aws_dragonfly-prod_us-east-2_eks_dragonfly-prod-1-cm_dependencies.tf
@@ -1,13 +1,16 @@
 data "vault_generic_secret" "aws_infra_credential" {
-  path = "secret/infra/aws/dragonfly-prod/terraform_admin"
+  path     = "secret/infra/aws/dragonfly-prod/terraform_admin"
+  provider = vault.eticloud
 }
 
 data "vault_generic_secret" "cluster_certificate" {
   path = "secret/infra/eks/dragonfly-prod-1/certificate"
+  provider = vault.eticloud
 }
 
 data "vault_generic_secret" "aws_auth_configmap" {
   path = "secret/infra/eks/dragonfly-prod-1/aws-auth"
+  provider = vault.eticloud
 }
 
 data "aws_eks_cluster" "cluster" {
