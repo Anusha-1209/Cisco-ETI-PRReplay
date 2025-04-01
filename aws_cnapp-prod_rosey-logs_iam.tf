f934c75093e384638c69f827656f12d525d24e8e
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 10:00:03 -0700

    feat(factorize)

--- aws_cnapp-prod_rosey-logs_iam.tf
+++ aws_cnapp-prod_rosey-logs_iam.tf
@@ -1,17 +1,11 @@
 # IAM role and policy required to write in the Rosey related bucket
 
-data "aws_caller_identity" "current" {
-  # region should not matter here, we use it to fetch the account ID
-  provider = aws.us
-}
+data "aws_caller_identity" "current" {}
 
-# https://github.com/hashicorp/terraform/issues/24476
-# Cannot use for_each to trim the below configuration, each resource is region bound
-
-resource "aws_iam_policy" "rosey_logs_us" {
-  provider    = aws.us
-  name        = "RoseyLogs-us"
-  description = "IAM Policy required in order to write to the Rosey-logs-us bucket"
+resource "aws_iam_policy" "rosey_logs" {
+  for_each    = local.clusters
+  name        = "RoseyLogs-${substr(each.key, 0, 2)}"
+  description = "IAM Policy required in order to write to the rosey-logs-${substr(each.key, 0, 2)} bucket"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -22,7 +16,7 @@ resource "aws_iam_policy" "rosey_logs_us" {
           "sts:AssumeRoleWithWebIdentity"
         ],
         Resource = [
-          "arn:aws:iam::${local.account_id}:role/${aws_iam_role.rosey_logs_us.name}"
+          "arn:aws:iam::${local.account_id}:role/${aws_iam_role.rosey_logs[each.key].name}"
         ]
       },
       {
@@ -32,62 +26,30 @@ resource "aws_iam_policy" "rosey_logs_us" {
           "s3:*Object"
         ],
         Resource = [
-          "arn:aws:s3:::rosey-logs-us/*",
-          "arn:aws:s3:::rosey-logs-us",
+          "arn:aws:s3:::rosey-logs-${substr(each.key, 0, 2)}/*",
+          "arn:aws:s3:::rosey-logs-${substr(each.key, 0, 2)}",
         ]
       }
     ]
   })
 }
 
-resource "aws_iam_policy" "rosey_logs_eu" {
-  provider    = aws.eu
-  name        = "RoseyLogs-us"
-  description = "IAM Policy required in order to write to the Rosey-logs-eu bucket"
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid    = "stsAssumeRoleWithWebIdentityLogs",
-        Effect = "Allow",
-        Action = [
-          "sts:AssumeRoleWithWebIdentity"
-        ],
-        Resource = [
-          "arn:aws:iam::${local.account_id}:role/${aws_iam_role.rosey_logs_eu.name}"
-        ]
-      },
-      {
-        Sid    = "S3AccessToSpecificBucket",
-        Effect = "Allow",
-        Action = [
-          "s3:*Object"
-        ],
-        Resource = [
-          "arn:aws:s3:::rosey-logs-eu/*",
-          "arn:aws:s3:::rosey-logs-eu",
-        ]
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role" "rosey_logs_us" {
-  provider = aws.us
-  name     = "RoseyLogs-us"
+resource "aws_iam_role" "rosey_logs" {
+  for_each = local.clusters
+  name     = "RoseyLogs-${substr(each.key, 0, 2)}"
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
         Principal = {
-          Federated = "arn:aws:iam::${local.account_id}:oidc-provider/oidc.eks.us-east-2.amazonaws.com/id/EFF9B51923E64F3067C820180603F855"
+          Federated = "arn:aws:iam::${local.account_id}:oidc-provider/oidc.eks.${each.key}.amazonaws.com/id/${each.value}"
         }
         Action = "sts:AssumeRoleWithWebIdentity"
         Condition = {
           StringEquals = {
-            "oidc.eks.us-east-2.amazonaws.com/id/EFF9B51923E64F3067C820180603F855:aud" = "sts.amazonaws.com"
-            "oidc.eks.us-east-2.amazonaws.com/id/EFF9B51923E64F3067C820180603F855:sub" = "system:serviceaccount:opentelemetry-exporter:*opentelemetry*"
+            "oidc.eks.${each.key}.amazonaws.com/id/${each.value}:aud" = "sts.amazonaws.com"
+            "oidc.eks.${each.key}.amazonaws.com/id/${each.value}:sub" = "system:serviceaccount:opentelemetry-exporter:*opentelemetry*"
           }
         }
       }
@@ -97,39 +59,8 @@ resource "aws_iam_role" "rosey_logs_us" {
   force_detach_policies = false
 }
 
-resource "aws_iam_role" "rosey_logs_eu" {
-  provider = aws.eu
-  name     = "RoseyLogs-eu"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Federated = "arn:aws:iam::${local.account_id}:oidc-provider/oidc.eks.eu-central-1.amazonaws.com/id/..."
-        }
-        Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringEquals = {
-            "oidc.eks.eu-central-1.amazonaws.com/id/EFF9B51923E64F3067C820180603F855:aud" = "sts.amazonaws.com"
-            "oidc.eks.eu-central-1.amazonaws.com/id/EFF9B51923E64F3067C820180603F855:sub" = "system:serviceaccount:opentelemetry-exporter:*opentelemetry*"
-          }
-        }
-      }
-    ]
-  })
-
-  force_detach_policies = false
-}
-
-resource "aws_iam_role_policy_attachment" "rosey_logs_us" {
-  provider   = aws.us
-  role       = aws_iam_role.rosey_logs_us.name
-  policy_arn = aws_iam_policy.rosey_logs_us.arn
-}
-
-resource "aws_iam_role_policy_attachment" "rosey_logs_eu" {
-  provider   = aws.eu
-  role       = aws_iam_role.rosey_logs_eu.name
-  policy_arn = aws_iam_policy.rosey_logs_eu.arn
+resource "aws_iam_role_policy_attachment" "rosey_logs" {
+  for_each   = local.clusters
+  role       = aws_iam_role.rosey_logs[each.key].name
+  policy_arn = aws_iam_policy.rosey_logs[each.key].arn
 }
--- aws_cnapp-prod_rosey-logs_locals.tf
+++ aws_cnapp-prod_rosey-logs_locals.tf
@@ -1,4 +1,8 @@
 locals {
   account_id   = data.aws_caller_identity.current.account_id
   account_name = "cnapp-prod"
+  clusters = {
+    "us-east-2"    = "EFF9B51923E64F3067C820180603F855"
+    "eu-central-1" = "28A49D0DC19E0AE06F2E38C0AD473F7D"
+  }
 }
