f2f5830246a6b99b7f5803e4da13422e7778eb06
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 11:13:37 -0700

    chore: correct motf-dev-use2-1 provider

--- aws_vowel-genai-dev_us-east-2_eks_motf-dev-use2-1_provider.tf
+++ aws_vowel-genai-dev_us-east-2_eks_motf-dev-use2-1_provider.tf
@@ -1,3 +1,10 @@
+provider "aws" {
+  alias      = "target"
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = local.region
+}
+
 provider "vault" {
   alias     = "eticloud"
   address   = "https://keeper.cisco.com"
@@ -7,22 +14,4 @@ provider "vault" {
 data "vault_generic_secret" "aws_infra_credential" {
   provider = vault.eticloud
   path     = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
-}
-
-provider "aws" {
-  alias      = "eks"
-  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region     = local.region
-  max_retries = 3
-  default_tags {
-    tags = {
-      ApplicationName    = "motf-dev-use2-1"
-      CiscoMailAlias     = "gbouline@cisco.com"
-      DataClassification = "Cisco Restricted"
-      DataTaxonomy       = "Cisco Operations Data"
-      Environment        = "NonProd"
-      ResourceOwner      = "Garett_Bouline"
-    }
-  }
 }
