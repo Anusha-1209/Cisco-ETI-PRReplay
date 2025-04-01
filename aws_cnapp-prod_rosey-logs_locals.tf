b0f4a3cd93fb6b4c1871962a02f222fdf8e710a6
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 12:03:08 -0700

    Creating Base Branch for PR 390

---
+++ aws_cnapp-prod_rosey-logs_locals.tf
@@ -0,0 +1,17 @@
+locals {
+  account_id   = data.aws_caller_identity.current.account_id
+  account_name = "cnapp-prod"
+  # EKS clsuter region and OIDC provider id
+  clusters = {
+    "cnapp-prod-us" = {
+      region           = "us-east-2"
+      region_prefix    = "us"
+      oidc_provider_id = "EFF9B51923E64F3067C820180603F855"
+    }
+    "cnapp-prod-eu" = {
+      region           = "eu-central-1"
+      region_prefix    = "eu"
+      oidc_provider_id = "28A49D0DC19E0AE06F2E38C0AD473F7D"
+    }
+  }
+}
