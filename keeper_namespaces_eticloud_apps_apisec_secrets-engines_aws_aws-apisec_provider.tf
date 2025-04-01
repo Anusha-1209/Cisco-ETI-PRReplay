79c2623e2de1331b8e054a9901d2a6692061ccb2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 15:07:38 -0700

    -

--- keeper_namespaces_eticloud_apps_apisec_secrets-engines_aws_aws-apisec_provider.tf
+++ keeper_namespaces_eticloud_apps_apisec_secrets-engines_aws_aws-apisec_provider.tf
@@ -4,21 +4,17 @@ provider "vault" {
 }
 
 provider "vault" {
-  alias     = "eticcprod"
-  namespace = "eticloud/eticcprod"
+  alias     = "eticloud"
+  namespace = "eticloud"
 }
 
 # Pulling AWS credentials from Keeper
 
 data "vault_generic_secret" "aws-apisec-infra-credentials" {
-    provider = vault.eticcprod
-    path     = "secret/eticcprod/infra/apisec-dev/aws"    
+    provider = vault.eticloud
+    path     = "secret/infra/aws/apisec-dev/terraform_admin"   
 }
 
-data "vault_generic_secret" "aws-eticloud-infra-credentials" {
-    provider = vault.eticcprod
-    path     = "secret/eticcprod/infra/prod/aws"
-}
 
 # By setting the AWS provider credentials via the data source above, we control in which account and region the resources get created.
 provider "aws" {
@@ -30,10 +26,3 @@ provider "aws" {
 }
 
 # Providers without an alias becomes the _default_ for resources that do not specify a provider
-provider "aws" {
-  alias       = "eticloud"
-  access_key  = data.vault_generic_secret.aws-eticloud-infra-credentials.data["AWS_ACCESS_KEY_ID"]
-  secret_key  = data.vault_generic_secret.aws-eticloud-infra-credentials.data["AWS_SECRET_ACCESS_KEY"]
-  region      = "us-east-2"
-  max_retries = 3
-}
