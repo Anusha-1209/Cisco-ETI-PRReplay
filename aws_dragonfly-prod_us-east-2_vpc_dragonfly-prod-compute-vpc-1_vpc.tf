78c2c7679254171dfb6b1a1ebf7a1df987d741f8
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 14:16:51 -0700

    chore(dragonfly): move VPCs from sre-tf-infra
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-prod_us-east-2_vpc_dragonfly-prod-compute-vpc-1_vpc.tf
+++ aws_dragonfly-prod_us-east-2_vpc_dragonfly-prod-compute-vpc-1_vpc.tf
@@ -2,7 +2,7 @@ module "vpc" {
   source                          = "git::https://github.com/cisco-eti/sre-tf-module-aws-vpc?ref=2.0.6"
   region                          = "us-east-2"
   vpc_cidr                        = "10.10.0.0/16"
-  vpc_name                        = "dragonfly-compute-prod-1"
+  vpc_name                        = "dragonfly-compute-prod-1-vpc"
   cluster_name                    = "dragonfly-prod-1"
   create_database_subnet_group    = false
   create_elasticache_subnet_group = false
