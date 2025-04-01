ba2c483e9e78a9fa705fd9dd7ba473a3a74087a1
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 21:29:36 -0700

    feat(dragonfly-dev): create kserve inference role
    Ref: EXP-2795
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

---
+++ aws_dragonfly-dev_iam_kserve-inference-role_role.tf
@@ -0,0 +1,61 @@
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    effect = "Allow"
+    principals {
+      type = "Federated"
+      identifiers = [
+        "arn:aws:iam::${data.aws_caller_identity.current.account_id}:oidc-provider/${local.oidc_id}",
+      ]
+    }
+    actions = ["sts:AssumeRoleWithWebIdentity"]
+    condition {
+      test     = "StringEquals"
+      values = ["sts.amazonaws.com"]
+      variable = "${local.oidc_id}:aud"
+    }
+    condition {
+      test     = "StringEquals"
+      values = ["system:serviceaccount:${local.service_account}"]
+      variable = "${local.oidc_id}:sub"
+    }
+  }
+}
+
+data "aws_iam_policy_document" "policy" {
+  statement {
+    actions = [
+      "s3:ListBucket",
+    ]
+
+    resources = [
+      "arn:aws:s3:::dragonfly-ml-models"
+    ]
+
+    effect = "Allow"
+  }
+
+  statement {
+    actions = [
+      "s3:*Object",
+    ]
+
+    resources = [
+      "arn:aws:s3:::dragonfly-ml-models/*"
+    ]
+
+    effect = "Allow"
+  }
+}
+
+resource "aws_iam_role" "role" {
+  name        = local.role_name
+  description = local.role_description
+
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  inline_policy {
+    name   = "kserve-inference-policy"
+    policy = data.aws_iam_policy_document.policy.json
+  }
+
+  managed_policy_arns = []
+}
