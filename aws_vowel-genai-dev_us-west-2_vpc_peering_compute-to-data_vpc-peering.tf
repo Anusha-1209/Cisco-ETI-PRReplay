2d02ea2ddb509f57493b2fea2a7dabe54823e2f1
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 14:52:03 -0700

    feat(vpc-peering): fmt

--- aws_vowel-genai-dev_us-west-2_vpc_peering_compute-to-data_vpc-peering.tf
+++ aws_vowel-genai-dev_us-west-2_vpc_peering_compute-to-data_vpc-peering.tf
@@ -18,7 +18,7 @@ module "vpc_peering_us-east_2_eks_us_west_2_data" {
 module "vpc_peering_vowel_dev_1_eks_us_west_2_data" {
   source             = "git::https://github.com/cisco-eti/sre-tf-module-vpc-peering.git?ref=latest"
   aws_account_name   = "vowel-genai-dev"
-  accepter_vpc_name  = "vowel-dev-1-vpc"  # where the vowel-dev-1 EKS cluster lives
+  accepter_vpc_name  = "vowel-dev-1-vpc" # where the vowel-dev-1 EKS cluster lives
   requester_vpc_name = "motf-dev-usw2-data"
   accepter_region    = "us-east-2"
   requester_region   = "us-west-2"
