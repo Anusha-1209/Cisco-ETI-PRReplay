e3d6022c9cff6b16206d3fd83ad6fa84fe3f7f97
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 08:43:23 -0700

    .

--- aws_outshift-common-dev_global_rds_global-rds-common-dev-1_aurora-postgres.tf
+++ aws_outshift-common-dev_global_rds_global-rds-common-dev-1_aurora-postgres.tf
@@ -42,7 +42,7 @@ module "rds_primary" {
   kms_key_id        = aws_kms_key.primary.arn
   secret_path       = "secret/dev/infra/aurora-pg/us-east-2/outshift-common-dev/global-rds-common-dev-use2-1"
   db_engine_version = "15.4"
-  db_allowed_cidrs  = [ data.aws_vpc.eks_primary_vpc.cidr_block, data.aws_vpc.eks_secondary_vpc.cidr_block ]
+  db_allowed_cidrs  = [ "10.0.0.0/16", "10.1.0.0/16" ]
 }
 
 # Secondary region us-east-2
