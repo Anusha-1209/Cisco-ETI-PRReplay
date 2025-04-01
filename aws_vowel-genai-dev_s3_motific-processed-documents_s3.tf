5ff8b34a298825ecfee6101888f06d396ed2c66f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 19:39:25 -0700

    fix: rename tf files

--- aws_vowel-genai-dev_s3_motific-processed-documents_s3.tf
+++ aws_vowel-genai-dev_s3_motific-processed-documents_s3.tf
@@ -1,22 +1,34 @@
-resource "aws_s3_bucket" "motific-processed-documents" {
-#  source      = "git::https://github.com/cisco-eti/sre-tf-module-aws-s3.git?ref=1.0.2"
-  bucket = "motific-processed-documents" 
-  tags = {
-    ApplicationName    = "sre-eks"
-    CiscoMailAlias     = "eti-sre@cisco.com"
-    DataClassification = "Cisco Restricted"
-    DataTaxonomy       = "Cisco Operations Data"
-    Environment        = "NonProd"
-    ResourceOwner      = "eti"
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
 }
+
+data "vault_generic_secret" "aws_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/vowel-genai-dev/terraform_admin"
 }
-resource "aws_s3_bucket_lifecycle_configuration" "motific-processed-documents" {
-  bucket = "motific-processed-documents"
-  rule {
-    id     = "TTL-policy"
-    status = "Enabled"
-    noncurrent_version_expiration {
-      noncurrent_days = 7
-    }
+terraform {
+  backend "s3" {
+    # This is the name of the backend S3 bucket.
+    bucket = "eticloud-tf-state-nonprod" # UPDATE ME.
+    # This is the path to the Terraform state file in the backend S3 bucket.
+    key = "terraform-state/aws/vowel-genai-dev/us-east-2/s3/motific-processed-documents.tfstate" # UPDATE ME.
+    # This is the region where the backend S3 bucket is located.
+    region = "us-east-2" # DO NOT CHANGE.
+
   }
+}
+variable "AWS_INFRA_REGION" {
+  description = "AWS Region"
+  default     = "us-east-2" #Set the region for the resources to be created.
+}
+provider "aws" {
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = "us-east-2"
+}
+module "motific-processed-documents"{
+  source = "./src"
+
 }
