ddc1c97ad75b8f85ce4e513b8b184e633223db51
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 17:59:52 -0700

    fix(string)

--- aws_vowel-genai-dev_us-west-2_vpc_peering_compute-to-data_provider.tf
+++ aws_vowel-genai-dev_us-west-2_vpc_peering_compute-to-data_provider.tf
@@ -1,6 +1,6 @@
 # By setting the AWS provider credentials via the data source above, we control in which account and region the resources get created.
 provider "aws" {
-  alias = primary
+  alias       = "primary"
   access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
   region      = "us-east-2"
@@ -19,7 +19,7 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias = secondary
+  alias       = "secondary"
   access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
   region      = "us-west-2"
