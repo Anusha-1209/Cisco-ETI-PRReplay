c65ab6d7eb89e9ec6ccd5d05c980a92f72864e01
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 11:25:55 -0700

    chore: fix motific cluster name issue

--- aws_vowel-genai-dev_us-east-2_eks_motf-dev-use2-1_argocd.tf
+++ aws_vowel-genai-dev_us-east-2_eks_motf-dev-use2-1_argocd.tf
@@ -14,7 +14,7 @@ provider "aws" {
 module "argocd" {
   source              = "git::https://github.com/cisco-eti/sre-tf-module-argo-cluster-enrollment?ref=0.1.0"
   argocd_cluster_name = local.argocd_k8s_name
-  eks_cluster_name    = data.aws_eks_cluster.cluster.name
+  eks_cluster_name    = local.name
   providers = {
     aws.eks    = aws.eks
     aws.argocd = aws.argocd
