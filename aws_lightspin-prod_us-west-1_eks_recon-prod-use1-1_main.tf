ac9dcacf71c570c6affd040c7de1adbaa7e4f7c9
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 16:45:36 -0700

    update s3 bucket

--- aws_lightspin-prod_us-west-1_eks_recon-prod-use1-1_main.tf
+++ aws_lightspin-prod_us-west-1_eks_recon-prod-use1-1_main.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     # This is the name of the backend S3 bucket.
-    bucket = "eticloud-tf-state-nonprod" # UPDATE ME.
+    bucket = "eticloud-tf-state-prod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
     key = "terraform-state/aws/lightspin-prod/us-east-1/eks/recon-prod-use2-1.tfstate"# UPDATE ME.
     # This is the region where the backend S3 bucket is located.
