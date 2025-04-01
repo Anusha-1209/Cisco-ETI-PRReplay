cef1e817e8a2ce27ff97603044d5a71408feafd8
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 11:18:35 -0700

    chore: fix motific provider issue

--- aws_vowel-genai-dev_us-east-2_eks_motf-dev-use2-1_main.tf
+++ aws_vowel-genai-dev_us-east-2_eks_motf-dev-use2-1_main.tf
@@ -18,3 +18,10 @@ module "eks_all_in_one" {
   create_alb_irsa         = "true"
   create_otel_irsa        = "true"
 }
+
+provider "aws" {
+  alias      = "target"
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = local.region
+}
