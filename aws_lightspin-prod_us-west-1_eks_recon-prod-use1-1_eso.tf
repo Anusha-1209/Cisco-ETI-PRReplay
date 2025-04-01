15ac5a30cd37c73fb6db2129725c9a0a2b435358
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 16:06:53 -0700

    Seperated provider file from configuration files

--- aws_lightspin-prod_us-west-1_eks_recon-prod-use1-1_eso.tf
+++ aws_lightspin-prod_us-west-1_eks_recon-prod-use1-1_eso.tf
@@ -4,12 +4,6 @@ data "aws_eks_cluster" "cluster" {
   name       = local.name
 }
 
-data "vault_generic_secret" "cluster_certificate" {
-  depends_on = [module.eks_all_in_one]
-  provider   = vault.eticloud
-  path       = "secret/infra/eks/${local.name}/certificate"
-}
-
 module "eso_eticloud" {
   source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
   cluster_name    = local.name
