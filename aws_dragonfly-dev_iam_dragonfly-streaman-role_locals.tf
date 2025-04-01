84fa2870417d33c59a3b45a57cb743d7fd4d35f7
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 22:27:59 -0700

    feat: add roles for streaman and event manager
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-dev_iam_dragonfly-streaman-role_locals.tf
+++ aws_dragonfly-dev_iam_dragonfly-streaman-role_locals.tf
@@ -1,11 +1,11 @@
 locals {
-  aws_account        = "dragonfly-dev"
-  role_name          = "dragonfly-event-manager-role"
-  role_description   = "IAM Role for dragonfly-event-manager"
+  aws_account      = "dragonfly-dev"
+  role_name        = "dragonfly-streaman-role"
+  role_description = "IAM Role for dragonfly-streaman"
 
   dragonfly_msk_cluster_name = "dragonfly-msk-1"
 
-  arango_connector_plugin_bucket = "dragonfly-dev-binaries-repository"
+  arango_connector_plugin_bucket    = "dragonfly-dev-binaries-repository"
   arangodb_connector_logs_bucket    = "dragonfly-dev-kafka-connector-log-files"
   arangodb_connector_execution_role = "kafka-connect-arangodb-role"
 
