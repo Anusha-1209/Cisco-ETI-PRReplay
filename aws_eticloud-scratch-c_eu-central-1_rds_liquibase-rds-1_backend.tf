9bd0d7a5181c0e99899761f255c31fa2b4eda387
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 20:42:16 -0700

    code removal

--- aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "eticloud-tf-state-nonprod"
-    key    = "terraform-state/aws/eticloud-scratch-c/eu-central-1/rds/liquibase-rds-1.tfstate"
-    region = "us-east-2"
-  }
-}
--- aws_eticloud-scratch-c_eu-central-1_rds_liquibase-rds-1_dependencies.tf
+++
@@ -1,3 +0,0 @@
-data "vault_generic_secret" "aws_infra_credential" {
-  path = "secret/infra/aws/eticloud-scratch-c/terraform_admin"
-}
