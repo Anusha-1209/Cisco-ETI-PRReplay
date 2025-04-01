e4626012ad26f1c096e609ef1bac21b386beab40
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 15:35:29 -0700

    feat(documentdb): typo

--- aws_outshift-common-dev_us-east-2_documentdb_pi-dev-use2-documentdb-1_documentdb.tf
+++ aws_outshift-common-dev_us-east-2_documentdb_pi-dev-use2-documentdb-1_documentdb.tf
@@ -3,7 +3,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket = "eticloud-tf-state-nonprod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key = "terraform-state/awsvowel-genai-dev/us-east-2/documentdb/pi-dev-use2-documentdb-1.tfstate" # UPDATE ME.
+    key = "terraform-state/vowel-genai-dev/us-east-2/documentdb/pi-dev-use2-documentdb-1.tfstate" # UPDATE ME.
     # This is the region where the backend S3 bucket is located.
     region = "us-east-2" # DO NOT CHANGE.
   }
