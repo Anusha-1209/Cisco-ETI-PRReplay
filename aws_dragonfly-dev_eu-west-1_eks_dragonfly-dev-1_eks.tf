07e29b68d3513e2b9338eb4def9c3ac86a2258bc
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 13:59:40 -0700

    chore(dragonfly): move EKS clusters from sre-tf-infra
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-dev_eu-west-1_eks_dragonfly-dev-1_eks.tf
+++ aws_dragonfly-dev_eu-west-1_eks_dragonfly-dev-1_eks.tf
@@ -36,25 +36,73 @@ data "vault_generic_secret" "aws_infra_credential" {
 data "aws_caller_identity" "current" {}
 
 locals {
-  name             = "eks-dragonfly-dev-2"
-  region           = "eu-west-1"
-  aws_account_name = "dragonfly-dev"
+  cluster_name = "eks-dragonfly-dev-2"
 }
-
 module "eks" {
-  source           = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
-  name             = local.name             # EKS cluster name
-  region           = local.region           # AWS provider region
-  aws_account_name = local.aws_account_name # AWS account name
-  cidr             = "10.10.0.0/16"         # VPC CIDR
-  cluster_version  = "1.27"                 # EKS cluster version
+  source          = "git::https://github.com/cisco-eti/sre-tf-module-aws-eks?ref=2.0.1"
+  cluster_name    = local.cluster_name
+  cluster_version = "1.27" # don't roll back!
+  cluster_os      = "AmazonLinux2"
+  vpc_name        = "dragonfly-dev-2-vpc"
 
-  # EKS Managed Private Node Group
-  instance_types = ["m5a.2xlarge"] # EKS instance types
-  min_size       = 5               # EKS node group min size
-  max_size       = 10              # EKS node group max size
-  desired_size   = 5               # EKS node group desired size
+  # Private Node group options
+  create_private_nodegroup            = true            # Defaults to true. Must be true for any of the below to be set by the module,
+  private_node_group_desired_capacity = 5               # The desired number of worker nodes. Once this is set, the number of desired instances must be manually modified.
+  private_node_group_min_capacity     = 5               # The minimum number of worker nodes.
+  private_node_group_max_capacity     = 10              # The maximium number of worker nodes.
+  private_node_group_instance_type    = ["m5a.2xlarge"] # The instance type for the worker nodes.
 
-  # Karpenter
-  create_karpenter_irsa = true # Create Karpenter IRSA
+  # Public Node group options
+  create_public_nodegroup            = false          # Defaults to false. Must be true for any of the below to be set by the module,
+  public_node_group_desired_capacity = 0              # The desired number of worker nodes. Once this is set, the number of desired instances must be manually modified.
+  public_node_group_min_capacity     = 0              # The minimum number of worker nodes.
+  public_node_group_max_capacity     = 0              # The maximium number of worker nodes.
+  public_node_group_instance_type    = ["m5a.xlarge"] # The instance type for the worker nodes.
+
+  # aws-auth configmap
+  create_aws_auth_configmap = false
+  manage_aws_auth_configmap = false # Set to false in SRE module
+
+  aws_auth_additional_roles = [
+    {
+      rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/admin"
+      username = "admin"
+      groups   = ["system:masters"]
+    },
+    {
+      rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/devops",
+      username = "devops",
+      groups   = ["system:masters"]
+    },
+    {
+      rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/KarpenterNodeRole-${local.cluster_name}",
+      username = "system:node:{{EC2PrivateDNSName}}",
+      groups   = ["system:bootstrappers", "system:nodes"]
+    }
+  ]
+
+  cluster_addons = {
+    coredns = {
+      addon_version = "v1.10.1-eksbuild.4"
+    }
+    kube-proxy = {
+      addon_version = "v1.27.6-eksbuild.2"
+    }
+    vpc-cni = {
+      addon_version = "v1.15.1-eksbuild.1"
+      configuration_values = jsonencode({
+        env = {
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION = "false"
+          WARM_PREFIX_TARGET       = "0"
+        }
+      })
+    }
+    aws-ebs-csi-driver = {
+      addon_version = "v1.23.1-eksbuild.1"
+    }
+  }
+  providers = {
+    vault = vault.eticloud
+  }
 }
