d729a6ed6560aea253242310fcd6a5f002a5b8ac
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 16:28:48 -0700

    update code provider

--- aws_lightspin-prod_us-west-1_eks_recon-prod-use1-1_main.tf
+++ aws_lightspin-prod_us-west-1_eks_recon-prod-use1-1_main.tf
@@ -1,27 +1,24 @@
 terraform {
   backend "s3" {
-    bucket = "eticloud-tf-state-prod"
-    key = "terraform-state/aws/lightspin-prod/us-east-1/eks/recon-prod-use2-1.tfstate"
-    region = "us-east-2"
+    # This is the name of the backend S3 bucket.
+    bucket = "eticloud-tf-state-nonprod" # UPDATE ME.
+    # This is the path to the Terraform state file in the backend S3 bucket.
+    key = "terraform-state/aws/lightspin-prod/us-east-1/eks/recon-prod-use2-1.tfstate"# UPDATE ME.
+    # This is the region where the backend S3 bucket is located.
+    region = "us-east-2" # DO NOT CHANGE.
+
   }
 }
 
-data "aws_caller_identity" "current" {}
-
 locals {
-  name             = "recon-prod-use1-1"
+  name             = "lightspin-prod-use1-1"
   region           = "us-east-1"
   aws_account_name = "lightspin-prod"
-  account_id       = data.aws_caller_identity.current.account_id
 }
 
 module "eks_all_in_one" {
-  source           = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=add_provider"
+  source           = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
   name             = local.name             # EKS cluster name
-  provider         = { 
-    aws = aws.external 
-    vault = vault.eticloud
-    }
   region           = local.region           # AWS provider region
   aws_account_name = local.aws_account_name # AWS account name
   cidr             = "10.0.0.0/16"          # VPC CIDR
@@ -29,18 +26,10 @@ module "eks_all_in_one" {
 
   # EKS Managed Private Node Group
   instance_types = ["m6a.2xlarge"] # EKS instance types
-  min_size       = 3               # EKS node group min size
-  max_size       = 5              # EKS node group max size
-  desired_size   = 3               # EKS node group desired size
-  
+  min_size       = 5               # EKS node group min size
+  max_size       = 15              # EKS node group max size
+  desired_size   = 8               # EKS node group desired size
+
+  # Karpenter
   create_karpenter_irsa = true # Create Karpenter IRSA
-  create_alb_irsa = true
-  create_otel_irsa = true
-  additional_aws_auth_configmap_roles = [
-      {
-        rolearn  = "arn:aws:iam::${local.account_id}:role/devops",
-        username = "devops",
-        groups   = ["system:masters"]
-      }
-  ]
 }
