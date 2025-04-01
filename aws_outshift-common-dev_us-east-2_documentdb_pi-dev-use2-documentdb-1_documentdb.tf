52b91eee3b38901c739f9c4b805d697012e628ab
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 15:34:24 -0700

    feat(documentdb): misc value fixes

--- aws_outshift-common-dev_us-east-2_documentdb_pi-dev-use2-documentdb-1_documentdb.tf
+++ aws_outshift-common-dev_us-east-2_documentdb_pi-dev-use2-documentdb-1_documentdb.tf
@@ -3,22 +3,22 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket = "eticloud-tf-state-nonprod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key = "terraform-state/aws/outshift-common-dev/us-east-2/documentdb/pi-dev-use2-documentdb-1.tfstate" # UPDATE ME.
+    key = "terraform-state/awsvowel-genai-dev/us-east-2/documentdb/pi-dev-use2-documentdb-1.tfstate" # UPDATE ME.
     # This is the region where the backend S3 bucket is located.
     region = "us-east-2" # DO NOT CHANGE.
   }
 }
 
-module "documentdb_cluster" {
+module "ppu_dev_documentdb_cluster" {
   source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-documentdb?ref=develop"
-  account_name      = "outshift-common-dev"
+  account_name      = "vowel-genai-dev"  # id 961088030672
   application_name  = "pi-dev-use2-documentdb-1"
   environment       = "dev"
   engine_version    = "5.0.0"
   master_username   = "pi"
-  instances         = 2
-  instance_class    = "db.r6g.large"
-  vpc_data_name     = "venture-data-vpc-name"
+  instances         = 3
+  instance_class    = "db.r6g.xlarge"
+  vpc_data_name     = "motf-dev-use2-data"
   vpc_eks_name      = "venture-eks-vpc-name"
   venture_namespace = "ppu"
 }
