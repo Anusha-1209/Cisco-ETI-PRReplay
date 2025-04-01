9fa8bff3ef295ef4e4025c12f3112712e763c1f5
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 00:01:09 -0700

    Creating Base Branch for PR 366

---
+++ aws_outshift-common-dev_us-east-2_rds_rds-common-dev-1_aurora-postgres.tf
@@ -0,0 +1,52 @@
+terraform {
+  backend "s3" {
+    bucket  = "eticloud-tf-state-nonprod"                                                
+    key     = "terraform-state/aws/eticloud/us-east-2/rds/rds-common-dev-1.tfstate"     
+    region  = "us-east-2"                                                                  
+  }
+}
+
+locals {
+  region           = "us-east-2"
+  aws_account_name = "outshift-common-dev"
+  data_vpc         = "common-dev-vpc-data"
+  eks_vpc          = "comn-dev-use2-1"
+  rds_name         = "rds-common-dev-1"
+}
+
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  provider    = vault.eticloud
+  path        = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
+}
+
+provider "aws" {
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = local.region
+}
+
+data "aws_vpc" "eks_vpc" {
+  filter {
+    name   = "tag:Name"
+    values = [local.eks_vpc]
+  }
+}
+
+module "rds" {
+  source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-aurora-postgres?ref=1.1.0"
+  vpc_name          = local.data_vpc
+  database_name     = "postgressql"
+  db_instance_type  = "db.r5.xlarge"
+  cluster_name      = local.rds_name
+  secret_path       = "secret/eticcprod/infra/aurora-pg/us-east-2/outshft-common-dev/rds-common-dev-1"
+  db_engine_version = "15"
+  db_allowed_cidrs  = [
+    data.aws_vpc.eks_vpc.cidr_block,
+  ]
+}
