98ee96a1fa55351818dea385fee44ef0d61ddd2c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 08:48:04 -0700

    Merge branch 'main' into glabl-rds-common-dev

---
+++ aws_dragonfly-dev_iam_kserve-inference-role_locals.tf
@@ -0,0 +1,13 @@
+locals {
+  aws_account        = "dragonfly-dev"
+  lambda_application = "dragonfly-lambda"
+  role_name          = "dragonfly-kserve-inference-role"
+  role_description   = "IAM Role for kserve inference service"
+
+  # AWS EKS Cluster
+  cluster_name = "eks-dragonfly-dev-2"
+  oidc_id      = trimprefix(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://")
+
+  dragonfly_backend_namespace = "dragonfly-backend"
+  service_account             = "${local.dragonfly_backend_namespace}:dragonfly-ml-inferenceserver"
+}
---
+++ aws_dragonfly-prod_iam_kserve-inference-role_locals.tf
@@ -0,0 +1,12 @@
+locals {
+  aws_account        = "dragonfly-prod"
+  role_name          = "dragonfly-kserve-inference-role"
+  role_description   = "IAM Role for kserve inference service"
+
+  # AWS EKS Cluster
+  cluster_name = "dragonfly-prod-1"
+  oidc_id      = trimprefix(data.aws_eks_cluster.cluster.identity[0].oidc[0].issuer, "https://")
+
+  dragonfly_backend_namespace = "dragonfly-backend"
+  service_account             = "${local.dragonfly_backend_namespace}:dragonfly-ml-inferenceserver"
+}
