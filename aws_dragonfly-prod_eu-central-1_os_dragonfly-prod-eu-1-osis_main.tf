dfd32191a1d22f261a29333cc0cfd24c6a12a198
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-03-31 23:48:13 -0700

    feat(osis): modify ingestion pipelines to use tenant id
    Ref: EXP-2960
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-prod_eu-central-1_os_dragonfly-prod-eu-1-osis_main.tf
+++ aws_dragonfly-prod_eu-central-1_os_dragonfly-prod-eu-1-osis_main.tf
@@ -3,13 +3,10 @@ locals {
     "dragonfly-osis-pipeline-1" = {
 
       topics = [
-        "falco",
         "monitoring",
-        "kg-edges",
-        "kg-nodes",
       ],
       min_units = 1,
-      max_units = 4,
+      max_units = 10,
     },
     "dragonfly-osis-pipeline-2" = {
       topics = [
@@ -17,7 +14,15 @@ locals {
         "attack",
       ]
       min_units = 1,
-      max_units = 4,
+      max_units = 10,
+    },
+    "dragonfly-osis-pipeline-3" = {
+
+      topics = [
+        "falco",
+      ],
+      min_units = 1,
+      max_units = 10,
     },
   }
 }
@@ -26,7 +31,8 @@ resource "awscc_osis_pipeline" "ingestion_pipelines" {
   for_each = local.pipelines
 
   pipeline_name = each.key
-  pipeline_configuration_body = templatefile("./pipeline.yaml", {
+  pipeline_configuration_body = templatefile(
+    each.value.pipeline_template_file, {
     region          = data.aws_region.current.name
     sts_role_arn    = module.pipeline_role.iam_role_arn
     opensearch_host = data.aws_opensearch_domain.dragonfly_prod_eu_1_os.endpoint
---
+++ aws_dragonfly-prod_us-east-2_os_osis_dependencies.tf
@@ -0,0 +1,85 @@
+# AWS credentials
+data "vault_generic_secret" "aws_infra_credential" {
+  path     = "secret/infra/aws/dragonfly-prod/terraform_admin"
+  provider = vault.eticloud
+}
+
+# OS account and region
+data "aws_caller_identity" "current" {} # data.aws_caller_identity.current.account_id
+data "aws_region" "current" {}          # data.aws_region.current.name
+
+# OS domain
+data "aws_opensearch_domain" "dragonfly_prod_1_os" {
+  domain_name = var.domain_name
+}
+
+# MSK cluster
+data "aws_msk_cluster" "dragonfly_msk_1" {
+  cluster_name = var.msk_cluster_name
+}
+
+data "aws_iam_policy_document" "pipeline_opensearch" {
+  statement {
+    effect  = "Allow"
+    actions = ["es:DescribeDomain"]
+    resources = [
+      data.aws_opensearch_domain.dragonfly_prod_1_os.arn,
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "es:ESHttp*",
+    ]
+    resources = [
+      "${data.aws_opensearch_domain.dragonfly_prod_1_os.arn}/*",
+    ]
+  }
+}
+
+data "aws_iam_policy_document" "pipeline_kafka" {
+  statement {
+    effect  = "Allow"
+    actions = ["es:DescribeDomain"]
+    resources = [
+      data.aws_opensearch_domain.dragonfly_prod_1_os.arn,
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kafka-cluster:Connect",
+      "kafka-cluster:AlterCluster",
+      "kafka-cluster:DescribeCluster",
+      "kafka:DescribeClusterV2",
+      "kafka:GetBootstrapBrokers",
+    ]
+    resources = [
+      data.aws_msk_cluster.dragonfly_msk_1.arn,
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kafka-cluster:*Topic*",
+      "kafka-cluster:ReadData",
+    ]
+    resources = [
+      "arn:aws:kafka:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:topic/${var.msk_cluster_name}/*",
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "kafka-cluster:AlterGroup",
+      "kafka-cluster:DescribeGroup"
+    ]
+    resources = [
+      "arn:aws:kafka:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:group/${var.msk_cluster_name}/*",
+    ]
+  }
+}
