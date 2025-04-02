f84433b9d6e285fe37f798239fbae0d967d3eb99
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 23:45:23 -0700

    adding argocd onboarding

--- aws_lightspin-staging_eu-west-1_eks_locals.tf
+++ aws_lightspin-staging_eu-west-1_eks_locals.tf
@@ -2,6 +2,6 @@ locals {
   name             = "cspm-staging-euw1-1"
   region           = "eu-west-1"
   aws_account_name = "lightspin-staging"
-  argocd_k8s_name    = "eks-gitops-genai-1"
+  argocd_k8s_name    = "eks-gitops-cnapp-1"
   argocd_aws_account = "eti-ci"
 }
