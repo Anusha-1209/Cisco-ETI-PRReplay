f4b86aa4c63a3a2dfdcf85776204328d5ea37838
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 21:51:01 -0700

    feat: msk connect in staging env
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

---
+++ aws_dragonfly-staging_eu-west-1_msk_dragonly-msk-connect-1_locals.tf
@@ -0,0 +1,13 @@
+locals {
+  aws_account_name                 = "dragonfly-staging"
+
+  compute_vpc                     = "dragonfly-compute-staging-1-vpc"
+  data_vpc                        = "dragonfly-data-staging-1-vpc"
+
+  arangodb_connector_plugin_name   = "kafka-connect-arangodb-1-2-0-dragonfly-2"
+  arangodb_connector_logs_bucket   = "dragonfly-staging-kafka-connector-log-files"
+  arangodb_connector_plugin_bucket = "dragonfly-staging-binaries-repository"
+  arangodb_connector_plugin_jar    = "kafka-connect-arangodb-1.2.0-dragonfly-3.zip"
+
+  arangodb_secret_name            = "thrill-arangodb"
+}
