7be2f53ca306781f9d48180e17bc60ca451c1486
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 21:20:43 -0700

    Merge branch 'main' into marvin--sqs-pre-process-collect-events_0

---
+++ aws_dragonfly-dev_eu-west-1_msk_dragonly-msk-connect-1_locals.tf
@@ -0,0 +1,13 @@
+locals {
+  aws_account_name                 = "dragonfly-dev"
+
+  compute_vpc                     = "dragonfly-dev-2-vpc"
+  data_vpc                        = "dragonfly-data-vpc"
+
+  arangodb_connector_plugin_name   = "kafka-connect-arangodb-1-2-0-dragonfly-2"
+  arangodb_connector_logs_bucket   = "dragonfly-dev-kafka-connector-log-files"
+  arangodb_connector_plugin_bucket = "dragonfly-dev-binaries-repository"
+  arangodb_connector_plugin_jar    = "kafka-connect-arangodb-1.2.0-dragonfly-3.zip"
+
+  arangodb_secret_name            = "thrill-arangodb"
+}
