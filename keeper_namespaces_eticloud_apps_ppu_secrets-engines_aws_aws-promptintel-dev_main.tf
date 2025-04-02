695601bfe15772d9138d7ffa3ed077751a2fe76b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 21:06:51 -0700

    chore: update role name

--- keeper_namespaces_eticloud_apps_ppu_secrets-engines_aws_aws-promptintel-dev_main.tf
+++ keeper_namespaces_eticloud_apps_ppu_secrets-engines_aws_aws-promptintel-dev_main.tf
@@ -14,6 +14,6 @@ resource "vault_aws_secret_backend_role" "jenkins-role" {
   credential_type = "assumed_role"
 
   role_arns = [
-    aws_iam_role.jenkins-promptintel.arn,
+    data.aws_iam_role.jenkins-promptintel.arn,
   ]
 }
