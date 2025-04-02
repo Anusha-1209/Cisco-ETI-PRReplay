0da2c7988c8bc4279f00ecb55d32cef555883d81
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 14:23:50 -0700

    fix: switch the peering

--- aws_outshift-common-dev_global_cross-region-vpc-peering_cross-account_vpc-peering.tf
+++ aws_outshift-common-dev_global_cross-region-vpc-peering_cross-account_vpc-peering.tf
@@ -7,33 +7,17 @@ terraform {
 }
 
 module "primary_cluster_to_secondary"{
-  source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=main"
+  source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=1.1.1"
   aws_accounts_to_regions = {
     "accepter" = {
-      account_name = "outshift-common-dev"
-      region       = "us-east-2"
-    }
-    "requester" = {
       account_name = "eticloud-preprod"
       region       = "us-east-2"
     }
+    "requester" = {
+      account_name = "outshift-common-dev"
+      region       = "us-east-2"
+    }
   }
-  accepter_vpc_name  = "common-dev-use2-vpc-data"
-  requester_vpc_name  = "nonprod-db-vpc-3-vpc"
+  accepter_vpc_name  = "nonprod-db-vpc-3-vpc"
+  requester_vpc_name  = "common-dev-use2-vpc-data"
 }
-
-# module "secondary_cluster_to_primary"{
-#   source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=1.1.0"
-#   aws_accounts_to_regions = {
-#     "accepter" = {
-#       account_name = "outshift-common-dev"
-#       region       = "us-west-2"
-#     }
-#     "requester" = {
-#       account_name = "outshift-common-dev"
-#       region       = "us-east-2"
-#     }
-#   }
-#   accepter_vpc_name  = "comn-dev-usw2-1"
-#   requester_vpc_name = "common-dev-use2-vpc-data"
-# }
