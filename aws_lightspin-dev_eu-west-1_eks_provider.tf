f8fc96f4765d53655e1b7da08b6e82bdd8d1bee0
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 00:17:19 -0700

    -

--- aws_lightspin-dev_eu-west-1_eks_provider.tf
+++ aws_lightspin-dev_eu-west-1_eks_provider.tf
@@ -3,7 +3,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket = "eticloud-tf-state-prod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key = "terraform-state/aws/lightspin-staging/eu-west-1/eks/lightspin-staging-use2-1.tfstate"# UPDATE ME.
+    key = "terraform-state/aws/lightspin-dev/eu-west-1/eks/lightspin-dev-euw1-1.tfstate"# UPDATE ME.
     # This is the region where the backend S3 bucket is located.
     region = "us-east-2" # DO NOT CHANGE.
 
