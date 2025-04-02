c1a3f89d3da8465d1c1cb50f7afb469a21f6f302
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 17:32:28 -0700

    fix: add generic-user to outshift-users

--- keeper_namespaces_eticloud_outshift-users_main.tf
+++ keeper_namespaces_eticloud_outshift-users_main.tf
@@ -54,6 +54,7 @@ resource "vault_jwt_auth_backend" "oidc" {
   oidc_client_id     = var.oidc_client_id
   oidc_client_secret = data.vault_generic_secret.oidc_credential.data["client_secret"]
   oidc_discovery_url = "https://sso-dbbfec7f.sso.duosecurity.com/oidc/${var.oidc_client_id}"
+  default_role       = "generic-user"
 }
 
 # Outshift Bugbash Vault Role
@@ -171,6 +172,45 @@ resource "vault_policy" "isheriff-team" {
 # Manage auth methods broadly across Vault
 # List, create, update, and delete key/value secrets
 path "secret/isheriff-team/*"
+{
+  capabilities = ["read", "list"]
+}
+EOT
+}
+# Outshift generic user Vault Role
+resource "vault_jwt_auth_backend_role" "generic-user" {
+  depends_on = [vault_policy.generic-user]
+  provider   = vault.venture
+  role_name  = "generic-user"
+  role_type  = "oidc"
+  backend    = vault_jwt_auth_backend.oidc.path
+  allowed_redirect_uris = ["https://keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+    "https://east.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+  "https://west.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback"]
+  bound_audiences = [var.oidc_client_id]
+  bound_claims = {
+    memberof = "CN=outshift-vault-generic-user,OU=Cisco Groups,DC=cisco,DC=com"
+  }
+  disable_bound_claims_parsing = true
+  bound_claims_type            = "string"
+  claim_mappings = {
+    email       = "email",
+    family_name = "family_name",
+    given_name  = "given_name",
+    sub         = "sub"
+  }
+  groups_claim = "memberof"
+  oidc_scopes  = ["profile", "email", "openid"]
+  user_claim   = "sub"
+  token_policies = [vault_policy.generic-user.name]
+}
+resource "vault_policy" "generic-user" {
+  provider = vault.venture
+  name     = "generic-user"
+  policy   = <<EOT
+  # Manage auth methods broadly across Vault
+# List, create, update, and delete key/value secrets
+path "secret/user/*"
 {
   capabilities = ["read", "list"]
 }
