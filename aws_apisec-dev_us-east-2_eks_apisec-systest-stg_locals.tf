146d2f5c72bd3753df707206913f073001d768f4
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 09:33:11 -0700

    Merge branch 'main' into sriaradhyula-patch-8

---
+++ aws_apisec-dev_us-east-2_eks_apisec-systest-stg_locals.tf
@@ -0,0 +1,9 @@
+# This file was created by Outshift Platform Self-Service automation.
+locals {
+  name               = "apisec-systest-stg"
+  region             = "us-east-2"
+  eks_aws_account    = "apisec-dev"
+  vpc_cidr           = "10.11.0.0/16"
+  argocd_k8s_name    = "eks-dev-gitops-1"
+  argocd_aws_account = "eticloud-preprod"
+}
---
+++ aws_dragonfly-staging_eu-west-1_s3_dragonfly-staging-data_locals.tf
@@ -0,0 +1,7 @@
+locals {
+  aws_account_name = "dragonfly-staging"
+  aws_region = "eu-west-1"
+
+  bucket_name = "dragonfly-staging-data"
+  eks_oidc_provider_id = "28C9314DBD532A08FF0B98E387A4A19F"
+}
