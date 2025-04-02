3e1d8fafd21e1e6b85a9486abe5a92166a35981b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 19:26:46 -0700

    chore: add datasource for iam role

--- keeper_namespaces_eticloud_apps_ppu_secrets-engines_aws_aws-promptintel-dev_iam.tf
+++ keeper_namespaces_eticloud_apps_ppu_secrets-engines_aws_aws-promptintel-dev_iam.tf
@@ -6,3 +6,7 @@ resource "aws_iam_access_key" "vault-secret-engine-user-vowel-genai-dev" {
   provider  = aws.vowel-genai-dev
   user      = "vault-secret-engine-user-vowel-genai-dev"
 }
+
+data "aws_iam_role" "jenkins" {
+  name = "jenkins"
+}
