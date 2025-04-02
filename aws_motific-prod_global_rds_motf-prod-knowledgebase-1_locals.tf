99873424f97f00a6ecb70098c4297a48b9b63970
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 12:32:51 -0700

    Merge branch 'main' into create-cross-account-peering

---
+++ aws_motific-prod_global_rds_motf-prod-knowledgebase-1_locals.tf
@@ -0,0 +1,8 @@
+
+locals {
+  vpc_name                = "motf-prod-use2-data"
+  data_secondary_vpc_name = "motf-prod-usw2-data"
+  cluster_vpc_name        = "motf-prod-use2-1"
+  aws_account_name        = "motific-prod"
+  rds_name                = "motf-prod-use2-1-knowledgebase"
+}
