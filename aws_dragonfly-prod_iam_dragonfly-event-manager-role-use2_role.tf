b29e4af8f0972f39498158a243083bde8b3b459c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 16:31:13 -0700

    fix: update sqs queue name

--- aws_dragonfly-prod_iam_dragonfly-event-manager-role-use2_role.tf
+++ aws_dragonfly-prod_iam_dragonfly-event-manager-role-use2_role.tf
@@ -91,7 +91,7 @@ data "aws_iam_policy_document" "sqs_policy" {
       "sqs:*"
     ]
     resources = [
-      "arn:aws:sqs:us-east-1:975854676552:queue-eventsForwarderTopic-lightspin"
+      "arn:aws:sqs:us-east-1:975854676552:queue-eventsForwarderTopic-production"
     ]
   }
 }
