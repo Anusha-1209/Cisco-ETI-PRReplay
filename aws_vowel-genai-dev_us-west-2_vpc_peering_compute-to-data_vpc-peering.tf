aba96dd23ff6a421b3198050f6cd7583abebf3f6
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 09:23:30 -0700

    fix(motific-dev): set tag version for peering

--- aws_vowel-genai-dev_us-west-2_vpc_peering_compute-to-data_vpc-peering.tf
+++ aws_vowel-genai-dev_us-west-2_vpc_peering_compute-to-data_vpc-peering.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 module "vpc_peering_us-east_2_eks_us_west_2_data" {
-  source             = "git::https://github.com/cisco-eti/sre-tf-module-vpc-peering.git?ref=latest"
+  source             = "git::https://github.com/cisco-eti/sre-tf-module-vpc-peering.git?ref=1.0.0"
   aws_account_name   = "vowel-genai-dev"
   accepter_vpc_name  = "motf-dev-use2-1"
   requester_vpc_name = "motf-dev-usw2-data"
@@ -16,7 +16,7 @@ module "vpc_peering_us-east_2_eks_us_west_2_data" {
 }
 
 module "vpc_peering_vowel_dev_1_eks_us_west_2_data" {
-  source             = "git::https://github.com/cisco-eti/sre-tf-module-vpc-peering.git?ref=latest"
+  source             = "git::https://github.com/cisco-eti/sre-tf-module-vpc-peering.git?ref=1.0.0"
   aws_account_name   = "vowel-genai-dev"
   accepter_vpc_name  = "vowel-dev-1-vpc" # where the vowel-dev-1 EKS cluster lives
   requester_vpc_name = "motf-dev-usw2-data"
