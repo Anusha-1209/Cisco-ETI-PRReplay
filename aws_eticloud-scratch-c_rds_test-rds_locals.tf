c4fdbed72e8f55b4031952336f65725f420965f3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 09:59:10 -0700

    chore(rds): requires EKS VPC

--- aws_eticloud-scratch-c_rds_test-rds_locals.tf
+++ aws_eticloud-scratch-c_rds_test-rds_locals.tf
@@ -1,6 +1,7 @@
 
 locals {
   vpc_name         = "rds-dev-euw1-1"
+  cluster_vpc_name = "sre-vpc-playground-1-vpc"
   aws_account_name = "eticloud-scratch-c"
   rds_name         = "test-rds-cluster"
 }
