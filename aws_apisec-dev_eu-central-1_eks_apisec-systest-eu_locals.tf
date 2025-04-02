08782cab819b13eafcdad3727305969475cca92d
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 00:19:28 -0700

    Merge branch 'main' into create-cspm-dev

---
+++ aws_apisec-dev_eu-central-1_eks_apisec-systest-eu_locals.tf
@@ -0,0 +1,9 @@
+# This file was created by Outshift Platform Self-Service automation.
+locals {
+  name               = "apisec-systest-eu"
+  region             = "eu-central-1"
+  eks_aws_account    = "apisec-dev"
+  vpc_cidr           = "10.10.0.0/16"
+  argocd_k8s_name    = "eks-dev-gitops-1"
+  argocd_aws_account = "eticloud-preprod"
+}
