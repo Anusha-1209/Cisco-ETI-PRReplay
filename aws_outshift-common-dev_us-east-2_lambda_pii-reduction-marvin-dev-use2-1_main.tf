175a6510cbcdc16a375fc9ef4debb1c094cd381b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 19:36:59 -0700

    -

--- aws_outshift-common-dev_us-east-2_lambda_pii-reduction-marvin-dev-use2-1_main.tf
+++ aws_outshift-common-dev_us-east-2_lambda_pii-reduction-marvin-dev-use2-1_main.tf
@@ -73,10 +73,8 @@ module "lambda_function_container_image" {
       "Resource": "arn:aws:ecr:us-east-2:626007623524:marvin/images/pii-service/server"
     }
   }
-  environment {
-    variables = {
-      SQS_URL = "https://sqs.us-east-2.amazonaws.com/471112537430/marvin-collect-events-dev-use2-1"
-    }
+  environment_variables {
+    SQS_URL = "https://sqs.us-east-2.amazonaws.com/471112537430/marvin-collect-events-dev-use2-1"
   }
 }
 
