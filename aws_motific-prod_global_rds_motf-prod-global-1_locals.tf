6e80b384a17c2e59ccd4d5818dd158fcf58f0c28
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 12:00:51 -0700

    Merge branch 'main' into marvin-dev-dlq-alarm

---
+++ aws_motific-prod_global_rds_motf-prod-global-1_locals.tf
@@ -0,0 +1,7 @@
+locals {
+  vpc_name                = "motf-prod-use2-data"
+  cluster_vpc_name        = "motf-prod-use2-1"
+  data_secondary_vpc_name = "motf-prod-usw2-data"
+  aws_account_name        = "motific-prod"
+  rds_name                = "motf-prod-use2-1"
+}
