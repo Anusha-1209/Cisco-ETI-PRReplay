55f3c46b886508a4d0d4382df5fa0493982dcddb
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 17:44:50 -0700

    feat(outshift-users): create secrets auto-sync

--- keeper_namespaces_eticloud_outshift-users_autosync.tf
+++ keeper_namespaces_eticloud_outshift-users_autosync.tf
@@ -3,7 +3,12 @@ locals {
     "bugbash",
     "csm",
     "genai",
-    "generic"]
+    "generic"
+    ]
+
+  gpt4o_kv_paths = [
+    "genai"
+    ]
 }
 
 # Sync GPT4 Key
@@ -12,10 +17,21 @@ data "vault_generic_secret" "autosync_llms_azure_openai_gpt4" {
   provider = vault.venture
 }
 
+data "vault_generic_secret" "autosync_llms_azure_openai_gpt4o" {
+  path = "autosync/llms/azure/openai/gpt4o"
+  provider = vault.venture
+}
 
 resource "vault_generic_secret" "llms_azure_openai_gpt4" {
   for_each = toset(local.gpt4_kv_paths)
   provider = vault.venture
   data_json = data.vault_generic_secret.autosync_llms_azure_openai_gpt4.data_json
   path = "${each.value}/llms/azure/openai/gpt4"
+}
+
+resource "vault_generic_secret" "llms_azure_openai_gpt4o" {
+  for_each = toset(local.gpt4o_kv_paths)
+  provider = vault.venture
+  data_json = data.vault_generic_secret.autosync_llms_azure_openai_gpt4o.data_json
+  path = "${each.value}/llms/azure/openai/gpt4o"
 }
