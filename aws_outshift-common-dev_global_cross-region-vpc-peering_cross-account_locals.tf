0204d2ec278b3187cfb3e789c6e8bcd77ab8cb1c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 13:13:23 -0700

    test: use upstream vpc peering module

---
+++ aws_outshift-common-dev_global_cross-region-vpc-peering_cross-account_locals.tf
@@ -0,0 +1,8 @@
+locals {
+  accepter_vpc_name = "nonprod-db-vpc-3-vpc"
+  requester_vpc_name = "common-dev-use2-vpc-data"
+  accepter_region = "us-east-2"
+  requester_region = "us-east-2"
+  accepter_account = "eticloud-preprod"
+  requester_account = "outshift-common-dev"
+}
