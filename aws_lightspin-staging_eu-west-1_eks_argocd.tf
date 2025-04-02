d93a2ee3eaeb7937f1b52e30b474e07044c59bbb
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 23:47:23 -0700

    adding argocd onboarding

--- aws_lightspin-staging_eu-west-1_eks_argocd.tf
+++ aws_lightspin-staging_eu-west-1_eks_argocd.tf
@@ -7,7 +7,7 @@ provider "aws" {
   alias       = "argocd"
   access_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region      = "us-east-2"
+  region      = "eu-north-1"
   max_retries = 3
 }
 
