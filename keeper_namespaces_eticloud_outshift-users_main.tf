b90a946b3b832a345cf2e3a82390f09708cfa861
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 17:32:15 -0700

    Creating Base Branch for PR 435

---
+++ keeper_namespaces_eticloud_outshift-users_main.tf
@@ -0,0 +1,178 @@
+provider "vault" {}
+
+variable "venture_name" {
+  type    = string
+  default = "outshift-users" # <-- requires updating
+}
+
+variable "oidc_client_id" {
+  type    = string
+  default = "DII9TO378NM0ZW02ABST" # <-- requires updating
+}
+
+provider "vault" {
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud/teamsecrets"
+  alias     = "teamsecrets"
+}
+
+provider "vault" {
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+  alias     = "eticloud"
+}
+
+provider "vault" {
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud/outshift-users" # <-- requires updating
+  alias     = "venture"
+}
+resource "vault_namespace" "namespace" {
+  provider = vault.eticloud
+  path     = var.venture_name
+}
+
+# key-value secrets engine
+resource "vault_mount" "kvv2" {
+  provider = vault.venture
+  path     = "secret"
+  type     = "kv"
+  options  = { version = "2" }
+}
+
+# OIDC Credentials
+data "vault_generic_secret" "oidc_credential" {
+  provider = vault.teamsecrets
+  path     = "secret/cisco_sso_auth_clients/vault_oidc_creds"
+}
+
+# oidc auth backend
+resource "vault_jwt_auth_backend" "oidc" {
+  provider           = vault.venture
+  type               = "oidc"
+  path               = "oidc"
+  oidc_client_id     = var.oidc_client_id
+  oidc_client_secret = data.vault_generic_secret.oidc_credential.data["client_secret"]
+  oidc_discovery_url = "https://sso-dbbfec7f.sso.duosecurity.com/oidc/${var.oidc_client_id}"
+}
+
+# Outshift Bugbash Vault Role
+resource "vault_jwt_auth_backend_role" "bugbash" {
+  depends_on = [vault_policy.bugbash]
+  provider   = vault.venture
+  role_name  = "bugbash"
+  role_type  = "oidc"
+  backend    = vault_jwt_auth_backend.oidc.path
+  allowed_redirect_uris = ["https://keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+    "https://east.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+  "https://west.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback"]
+  bound_audiences = [var.oidc_client_id]
+  bound_claims = {
+    memberof = "CN=outshift-vault-bugbash,OU=Cisco Groups,DC=cisco,DC=com"
+  }
+  disable_bound_claims_parsing = true
+  bound_claims_type            = "string"
+  claim_mappings = {
+    email       = "email",
+    family_name = "family_name",
+    given_name  = "given_name",
+    sub         = "sub"
+  }
+  groups_claim = "memberof"
+  oidc_scopes  = ["profile", "email", "openid"]
+  user_claim   = "sub"
+  token_policies = [vault_policy.bugbash.name]
+}
+
+resource "vault_policy" "bugbash" {
+  provider = vault.venture
+  name     = "bugbash"
+  policy   = <<EOT
+# Manage auth methods broadly across Vault
+# List, create, update, and delete key/value secrets
+path "secret/bugbash/*"
+{
+  capabilities = ["read", "list"]
+}
+EOT
+}
+
+# Outshift CSM Vault User Role
+resource "vault_jwt_auth_backend_role" "csm" {
+  depends_on = [vault_policy.csm]
+  provider   = vault.venture
+  role_name  = "csm"
+  role_type  = "oidc"
+  backend    = vault_jwt_auth_backend.oidc.path
+  allowed_redirect_uris = ["https://keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+    "https://east.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+  "https://west.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback"]
+  bound_audiences = [var.oidc_client_id]
+  bound_claims = {
+    memberof = "CN=outshift-vault-csm,OU=Cisco Groups,DC=cisco,DC=com"
+  }
+  disable_bound_claims_parsing = true
+  bound_claims_type            = "string"
+  claim_mappings = {
+    email       = "email",
+    family_name = "family_name",
+    given_name  = "given_name",
+    sub         = "sub"
+  }
+  groups_claim = "memberof"
+  oidc_scopes  = ["profile", "email", "openid"]
+  user_claim   = "sub"
+  token_policies = [vault_policy.csm.name]
+}
+resource "vault_policy" "csm" {
+  provider = vault.venture
+  name     = "csm"
+  policy   = <<EOT
+# Manage auth methods broadly across Vault
+# List, create, update, and delete key/value secrets
+path "secret/csm/*"
+{
+  capabilities = ["read", "list"]
+}
+EOT
+}
+
+# Outshift isheriff-team Vault Role
+resource "vault_jwt_auth_backend_role" "isheriff-team" {
+  depends_on = [vault_policy.isheriff-team]
+  provider   = vault.venture
+  role_name  = "isheriff-team"
+  role_type  = "oidc"
+  backend    = vault_jwt_auth_backend.oidc.path
+  allowed_redirect_uris = ["https://keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+    "https://east.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+  "https://west.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback"]
+  bound_audiences = [var.oidc_client_id]
+  bound_claims = {
+    memberof = "CN=outshift-vault-isheriff-team,OU=Cisco Groups,DC=cisco,DC=com"
+  }
+  disable_bound_claims_parsing = true
+  bound_claims_type            = "string"
+  claim_mappings = {
+    email       = "email",
+    family_name = "family_name",
+    given_name  = "given_name",
+    sub         = "sub"
+  }
+  groups_claim = "memberof"
+  oidc_scopes  = ["profile", "email", "openid"]
+  user_claim   = "sub"
+  token_policies = [vault_policy.isheriff-team.name]
+}
+resource "vault_policy" "isheriff-team" {
+  provider = vault.venture
+  name     = "isheriff-team"
+  policy   = <<EOT
+# Manage auth methods broadly across Vault
+# List, create, update, and delete key/value secrets
+path "secret/isheriff-team/*"
+{
+  capabilities = ["read", "list"]
+}
+EOT
+}
