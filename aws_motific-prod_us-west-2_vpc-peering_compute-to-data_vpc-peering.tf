769c3c32b3965f8be516f66cbe472fc53256f031
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 16:15:47 -0700

    chore(motific): fix peering

--- aws_motific-prod_us-west-2_vpc-peering_compute-to-data_vpc-peering.tf
+++ aws_motific-prod_us-west-2_vpc-peering_compute-to-data_vpc-peering.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 module "motific_prod_vpc_peering_eks_us_west_2_data" {
-  source             = "git::https://github.com/cisco-eti/sre-tf-module-vpc-peering.git?ref=1.1.0"
+  source             = "git::https://github.com/cisco-eti/sre-tf-module-vpc-peering.git?ref=1.1.1"
   aws_accounts_to_regions = {
     "accepter" = {
       account_name = "motific-prod"
