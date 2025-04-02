7429de46b09934fab40f6a13a1719b35df28be29
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 10:32:57 -0700

    fix: revert changes

--- aws_outshift-common-dev_global_cross-region-vpc-peering_cross-account_locals.tf
+++
@@ -1,8 +0,0 @@
-locals {
-  accepter_vpc_name = "nonprod-db-vpc-3-vpc"
-  requester_vpc_name = "common-dev-use2-vpc-data"
-  accepter_region = "us-east-2"
-  requester_region = "us-east-2"
-  accepter_account = "eticloud-preprod"
-  requester_account = "outshift-common-dev"
-}
