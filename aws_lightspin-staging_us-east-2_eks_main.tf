619ff21663f191fb00daad7b7144071e65601b4f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 23:27:21 -0700

    added lightspin staging new

--- aws_lightspin-staging_us-east-2_eks_main.tf
+++ aws_lightspin-staging_us-east-2_eks_main.tf
@@ -3,7 +3,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket = "eticloud-tf-state-prod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key = "terraform-state/aws/lightspin-staging/us-east-1/eks/recon-prod-use2-1.tfstate"# UPDATE ME.
+    key = "terraform-state/aws/lightspin-staging/us-east-2/eks/lightspin-staging-use2-1.tfstate"# UPDATE ME.
     # This is the region where the backend S3 bucket is located.
     region = "us-east-2" # DO NOT CHANGE.
 
@@ -22,6 +22,7 @@ module "eks_all_in_one" {
   region           = local.region           # AWS provider region
   aws_account_name = local.aws_account_name # AWS account name
   cidr             = "10.0.0.0/16"          # VPC CIDR
+  private_subnet_bits = 2                  # Private subnet bits
   cluster_version  = "1.29"                 # EKS cluster version
 
   # EKS Managed Private Node Group
