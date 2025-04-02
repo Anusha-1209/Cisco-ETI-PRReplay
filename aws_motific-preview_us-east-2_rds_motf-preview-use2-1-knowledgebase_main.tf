9fd31baa5e3dcd54b54148a88a3ae416ff950043
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 10:52:23 -0700

    feat(rds): enable global clusters

---
+++ aws_motific-preview_us-east-2_rds_motf-preview-use2-1-knowledgebase_main.tf
@@ -0,0 +1,17 @@
+module "rds" {
+  source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-aurora-postgres?ref=1.1.0"
+  vpc_name          = local.vpc_name
+  database_name     = "postgres"
+  db_instance_type  = "db.r5.xlarge"
+  cluster_name      = local.rds_name
+  secret_path       = "secret/eticcprod/infra/aurora-pg/us-east-2/motific-preview/motf-preview-use2-1-knowledgebase"
+  db_allowed_cidrs  = [data.aws_vpc.cluster_vpc.cidr_block]
+  db_engine_version = "15"
+}
+
+resource "aws_rds_global_cluster" "motf-preview-knowledgebase-global" {
+  global_cluster_identifier    = "motf-preview-knowledgebase-global"
+  # (Optional) Enable to remove DB Cluster members from Global Cluster on destroy. Required with source_db_cluster_identifier.
+  force_destroy                = true
+  source_db_cluster_identifier = module.rds.cluster_arn
+}
