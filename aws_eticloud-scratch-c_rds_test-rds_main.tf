94c5363cefb4aab0a74047bed821978629c45bd5
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 09:57:20 -0700

    chore(rds): fixes

--- aws_eticloud-scratch-c_rds_test-rds_main.tf
+++ aws_eticloud-scratch-c_rds_test-rds_main.tf
@@ -4,7 +4,7 @@ module "rds" {
   database_name     = "postgressql"
   db_instance_type  = "db.r5.xlarge"
   cluster_name      = local.rds_name
-  secret_path       = "secret/eticcprod/infra/aurora-pg/us-east-2/eticloud-scratch-c/rds/"
+  secret_path       = "secret/eticcprod/infra/aurora-pg/us-east-2/eticloud-scratch-c/rds/test-rds-cluster"
   db_allowed_cidrs  = []  # this is a test, not associated to an EKS cluster
   db_engine_version = "15"
 }
