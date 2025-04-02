400d41bd4555ae9e75034276072a5e54d9ecc34c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 20:15:05 -0700

    feat: msk-connect kg connector
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-dev_eu-west-1_msk_dragonly-msk-connect-1_locals.tf
+++ aws_dragonfly-dev_eu-west-1_msk_dragonly-msk-connect-1_locals.tf
@@ -1,6 +1,7 @@
 locals {
   aws_account_name                 = "dragonfly-dev"
-  arangodb_connector_plugin_name     = "kafka-connect-arangodb-1-2-0-dragonfly-2"
+  arangodb_connector_plugin_name   = "kafka-connect-arangodb-1-2-0-dragonfly-2"
   arangodb_connector_logs_bucket   = "dragonfly-dev-kafka-connector-log-files"
   arangodb_connector_plugin_bucket = "dragonfly-dev-binaries-repository"
+    arangodb_connector_plugin_jar = "kafka-connect-arangodb-1-2-0-dragonfly-2.jar"
 }
