4bc9f257fe5807a228baa0e7ec09d53e382a29e0
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 17:14:06 -0700

    fix: update paths

--- keeper_namespaces_eticloud_apps_vowel_approles_main.tf
+++ keeper_namespaces_eticloud_apps_vowel_approles_main.tf
@@ -16,12 +16,6 @@ resource "vault_policy" "ci_jenkins_policy" {
   provider = vault.vowel
   name     = "ci-jenkins"
   policy   = data.local_file.jenkins_policy_hcl.content
-
-  lifecycle {
-    replace_triggered_by = [
-      data.local_file.jenkins_policy_hcl.content
-    ]
-  }
 }
 
 resource "vault_approle_auth_backend_role" "ci_jenkins_approle" {
