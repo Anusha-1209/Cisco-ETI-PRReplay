6ff804b4dd1fc57e27e76fdd596aae20a2b3a7c7
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 20:44:50 -0700

    feat: msk-connect kg connector
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-dev_eu-west-1_msk_dragonly-msk-connect-1_kms.tf
+++ aws_dragonfly-dev_eu-west-1_msk_dragonly-msk-connect-1_kms.tf
@@ -1,16 +1,3 @@
 resource "aws_kms_key" "encryption_key" {
   description = "encryption key for msk-kg-connector secrets"
-
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        "Sid" : "Internal",
-        "Effect" : "Allow",
-        "Principal" : { "AWS" : data.aws_caller_identity.current.arn },
-        "Action" : "kms:*",
-        "Resource" : "*"
-      },
-    ]
-  })
 }
