847fcf99474b717f4d2d7eee648090eb443eaff3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 10:17:26 -0700

    test: use upstream vpc peering module

--- aws_outshift-common-dev_global_cross-region-vpc-peering_cross-account_vpc-peering.tf
+++ aws_outshift-common-dev_global_cross-region-vpc-peering_cross-account_vpc-peering.tf
@@ -6,21 +6,21 @@ terraform {
   }
 }
 
-module "primary_cluster_to_secondary"{
-  source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=1.1.0"
-  aws_accounts_to_regions = {
-    "accepter" = {
-      account_name = "eticloud-preprod"
-      region       = "us-east-2"
-    }
-    "requester" = {
-      account_name = "outshift-common-dev"
-      region       = "us-east-2"
-    }
-  }
-  accepter_vpc_name  = "nonprod-db-vpc-3-vpc"
-  requester_vpc_name  = "common-dev-use2-vpc-data"
-}
+# module "primary_cluster_to_secondary"{
+#   source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=1.1.0"
+#   aws_accounts_to_regions = {
+#     "accepter" = {
+#       account_name = "eticloud-preprod"
+#       region       = "us-east-2"
+#     }
+#     "requester" = {
+#       account_name = "outshift-common-dev"
+#       region       = "us-east-2"
+#     }
+#   }
+#   accepter_vpc_name  = "nonprod-db-vpc-3-vpc"
+#   requester_vpc_name  = "common-dev-use2-vpc-data"
+# }
 
 # module "secondary_cluster_to_primary"{
 #   source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=1.1.0"
@@ -37,3 +37,23 @@ module "primary_cluster_to_secondary"{
 #   accepter_vpc_name  = "comn-dev-usw2-1"
 #   requester_vpc_name = "common-dev-use2-vpc-data"
 # }
+
+// Creates a peering between VPCs different accounts and different regions
+module "multi_account_multi_region" {
+  source = "grem11n/vpc-peering/aws//examples/multi-account-multi-region"
+
+  providers = {
+    aws.this = aws.this
+    aws.peer = aws.peer
+  }
+
+  this_vpc_id = "vpc-0c082861f90a7bd11"
+  peer_vpc_id = "vpc-08cf360be9a13ba25"
+
+  auto_accept_peering = true
+
+  tags = {
+    Name        = "tf-multi-account-multi-region"
+    Environment = "Test"
+  }
+}
