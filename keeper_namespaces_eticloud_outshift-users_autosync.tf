b7b85841730326b67fecd3d4e2dd10e2113a2a21
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 17:38:52 -0700

    feat(outshift-users): create secrets auto-sync

--- keeper_namespaces_eticloud_outshift-users_autosync.tf
+++ keeper_namespaces_eticloud_outshift-users_autosync.tf
@@ -7,7 +7,4 @@ resource "vault_generic_secret" "bugbash_llms_azure_openai_gpt4" {
   path = "bugbash/llms/azure/openai/gpt4"
   provider = vault.venture
   data_json = data.vault_generic_secret.llms_azure_openai_gpt4.data_json
-  lifecycle {
-    replace_triggered_by = [ data.vault_generic_secret.llms_azure_openai_gpt4 ]
-  }
 }
