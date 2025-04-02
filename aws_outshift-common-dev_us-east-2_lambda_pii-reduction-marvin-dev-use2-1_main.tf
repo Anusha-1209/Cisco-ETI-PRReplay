95232c6f920ac9e41a710c3d2d84257c3889f668
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 18:07:55 -0700

    -

--- aws_outshift-common-dev_us-east-2_lambda_pii-reduction-marvin-dev-use2-1_main.tf
+++ aws_outshift-common-dev_us-east-2_lambda_pii-reduction-marvin-dev-use2-1_main.tf
@@ -17,7 +17,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket = "eticloud-tf-state-nonprod" # UPDATE ME.
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key = "terraform-state/aws/outshift-common-dev/us-east-2/lambda/marvin-pii-use2-1.tfstate"
+    key = "terraform-state/aws/outshift-common-dev/us-east-2/lambda/pii-reduction-marvin-dev-use2-1.tfstate"
     # This is the region where the backend S3 bucket is located.
     region = "us-east-2" # DO NOT CHANGE.
 
