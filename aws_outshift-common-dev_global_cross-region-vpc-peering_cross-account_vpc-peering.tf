fe7736859b8678164b72398987d01cc844a6bd93
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 10:12:25 -0700

    chore: create a vpc perring for cross account to test DMS

---
+++ aws_outshift-common-dev_global_cross-region-vpc-peering_cross-account_vpc-peering.tf
@@ -0,0 +1,39 @@
+terraform {
+  backend "s3" {
+    bucket = "eticloud-tf-state-nonprod"
+    key    = "terraform-state/cross-account-vpc-peering/rds-dev-3-data-comn-dev-clusters.tfstate"
+    region = "us-east-2"
+  }
+}
+
+module "primary_cluster_to_secondary"{
+  source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=1.1.0"
+  aws_accounts_to_regions = {
+    "accepter" = {
+      account_name = "eticloud-preproduction"
+      region       = "us-east-2"
+    }
+    "requester" = {
+      account_name = "outshift-common-dev"
+      region       = "us-east-2"
+    }
+  }
+  accepter_vpc_name  = "nonprod-db-vpc-3-vpc"
+  requester_vpc_name  = "comn-dev-use2-1"
+}
+
+# module "secondary_cluster_to_primary"{
+#   source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=1.1.0"
+#   aws_accounts_to_regions = {
+#     "accepter" = {
+#       account_name = "outshift-common-dev"
+#       region       = "us-west-2"
+#     }
+#     "requester" = {
+#       account_name = "outshift-common-dev"
+#       region       = "us-east-2"
+#     }
+#   }
+#   accepter_vpc_name  = "comn-dev-usw2-1"
+#   requester_vpc_name = "common-dev-use2-vpc-data"
+# }
