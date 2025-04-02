942cae5427e543d0fbdcd735620d5b8f33217078
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 21:02:12 -0700

    chore: update role name

--- keeper_namespaces_eticloud_apps_ppu_secrets-engines_aws_aws-promptintel-dev_iam.tf
+++ keeper_namespaces_eticloud_apps_ppu_secrets-engines_aws_aws-promptintel-dev_iam.tf
@@ -45,7 +45,7 @@ EOF
 
 resource "aws_iam_role" "jenkins-promptintel" {
   provider           = aws.vowel-genai-dev
-  name               = "jenkins-promptintel"
+  name               = "jenkins"
   assume_role_policy = jsonencode({
     "Version" : "2012-10-17",
     "Statement" : [
