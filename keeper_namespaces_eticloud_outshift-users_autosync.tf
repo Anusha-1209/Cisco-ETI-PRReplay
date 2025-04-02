119b992bbac64788a182c25d3698933c81e2752f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 22:41:18 -0700

    fix(autosync): add datasources_sharepoint_outshiftgenai

--- keeper_namespaces_eticloud_outshift-users_autosync.tf
+++ keeper_namespaces_eticloud_outshift-users_autosync.tf
@@ -4,11 +4,14 @@ locals {
     "csm",
     "genai",
     "generic"
-    ]
+  ]
 
   gpt4o_kv_paths = [
     "genai"
-    ]
+  ]
+  datasources_sharepoint_outshiftgenai_kv_paths = [
+    "csm"
+  ]
 }
 
 # Sync GPT4 Key
@@ -19,6 +22,11 @@ data "vault_generic_secret" "autosync_llms_azure_openai_gpt4" {
 
 data "vault_generic_secret" "autosync_llms_azure_openai_gpt4o" {
   path = "autosync/llms/azure/openai/gpt4o"
+  provider = vault.venture
+}
+
+data "vault_generic_secret" "autosync_datasources_sharepoint_outshiftgenai" {
+  path = "autosync/datasources/sharepoint/OutshiftGenAI"
   provider = vault.venture
 }
 
@@ -34,4 +42,11 @@ resource "vault_generic_secret" "llms_azure_openai_gpt4o" {
   provider = vault.venture
   data_json = data.vault_generic_secret.autosync_llms_azure_openai_gpt4o.data_json
   path = "${each.value}/llms/azure/openai/gpt4o"
+}
+
+resource "vault_generic_secret" "datasources_sharepoint_outshiftgenai" {
+  for_each = toset(local.datasources_sharepoint_outshiftgenai_kv_paths)
+  provider = vault.venture
+  data_json = data.vault_generic_secret.autosync_datasources_sharepoint_outshiftgenai.data_json
+  path = "${each.value}/datasources/sharepoint/OutshiftGenAI"
 }
