3ed4071757096fcfea97164fdd3da8d689a0caea
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-01 21:41:30 -0700

    -

--- aws_outshift-common-prod_eu-central-1_lambda_pii-reduction-marvin-prod-euc1-1_main.tf
+++ aws_outshift-common-prod_eu-central-1_lambda_pii-reduction-marvin-prod-euc1-1_main.tf
@@ -75,7 +75,7 @@ module "lambda_function_container_image" {
     }
   }
   docker_additional_options = [
-    "-e", "SQS_URL='https://sqs.us-east-2.amazonaws.com/637423477129/marvin-collect-events-prod-ecu1-1'"
+    "-e", "SQS_URL='https://sqs.eu-central-1.amazonaws.com/637423477129/marvin-collect-events-prod-ecu1-1'"
   ]
 }
 
