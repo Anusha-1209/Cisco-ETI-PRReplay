2246ee42b81aee51d892badaa4dc16b9614131d0
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 15:33:46 -0700

    -

--- aws_dragonfly-staging_eu-west-1_s3_dragonfly-staging-data_iam.tf
+++ aws_dragonfly-staging_eu-west-1_s3_dragonfly-staging-data_iam.tf
@@ -55,11 +55,11 @@ resource "aws_iam_role_policy_attachment" "attach_s3_policy" {
   policy_arn = aws_iam_policy.s3_access_policy.arn
 }
 
-# Attach the existing "inference-client-policy" to the role
-resource "aws_iam_role_policy_attachment" "attach_inference_client_policy" {
-  role       = aws_iam_role.eks_s3_access_role.name
-  policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/inference-client-policy"
-}
+# # Attach the existing "inference-client-policy" to the role
+# resource "aws_iam_role_policy_attachment" "attach_inference_client_policy" {
+#   role       = aws_iam_role.eks_s3_access_role.name
+#   policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/inference-client-policy"
+# }
 
 # OIDC Provider URL
 locals {
