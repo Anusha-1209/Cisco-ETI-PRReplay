e8ee8002b21850a4670e9bc2abd47412b181189f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 10:04:45 -0700

    fix(rds): remove RDS proxy

--- aws_motific-prod_iam_enhanced_monitoring_backend.tf
+++ aws_motific-prod_iam_enhanced_monitoring_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "eticloud-tf-state-prod"                               
-    key    = "terraform-state/aws/motific-prod/iam/iam-roles.tfstate" 
-    region = "us-east-2"                                               
+    bucket = "eticloud-tf-state-prod"
+    key    = "terraform-state/aws/motific-prod/iam/enhanced-monitoring-iam-roles.tfstate"
+    region = "us-east-2"
  }
 }
