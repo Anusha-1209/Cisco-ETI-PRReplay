dfe4b8d02435adf0cfa732e6cdf94a03361caee2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 13:05:08 -0700

    fix(lightspin-prod): fix region

--- aws_lightspin-prod_us-east-2_rds_backend.tf
+++ aws_lightspin-prod_us-east-2_rds_backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "eticloud-tf-state-prod"
-    key            = "terraform-state/aws/lightspin-prod/global/rds/rds.tfstate"
+    key            = "terraform-state/aws/lightspin-prod/us-east-2/rds/rds.tfstate"
     region         = "us-east-2"
     dynamodb_table = "eticloud-tf-locks"
     encrypt        = true
