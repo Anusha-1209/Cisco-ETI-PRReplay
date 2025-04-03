08eb067fd9ce866e79bd54721fcf28e4ecb7ee65
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 02:08:42 -0700

    align TF cspm-dev

--- aws_lightspin-dev_eu-west-1_eks_cspm-dev-euw1-1_main.tf
+++ aws_lightspin-dev_eu-west-1_eks_cspm-dev-euw1-1_main.tf
@@ -34,5 +34,24 @@ module "eks_all_in_one" {
         username = "devops",
         groups   = ["system:masters"]
       }
-    ]
+      ]
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+      configuration_values = jsonencode({
+        env = {
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
+    # aws-ebs-csi-driver is not included to prevent installation
+  }
 }
