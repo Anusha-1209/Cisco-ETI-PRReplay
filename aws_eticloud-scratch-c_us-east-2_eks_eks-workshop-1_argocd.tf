8d83f6f5bbeec30996b70e84b59cf741816f8223
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 23:03:17 -0700

    Creating Base Branch for PR 548

---
+++ aws_eticloud-scratch-c_us-east-2_eks_eks-workshop-1_argocd.tf
@@ -0,0 +1,46 @@
+# This file was created by Outshift Platform Self-Service automation.
+
+locals {
+  argocd_k8s_name    = "eks-gitops-genai-1"
+  argocd_aws_account = "eti-ci"
+}
+
+data "vault_generic_secret" "aws_argocd_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/${local.argocd_aws_account}/terraform_admin"
+}
+
+data "aws_eks_cluster" "argocd" {
+  name       = local.argocd_k8s_name
+  provider   = aws.argocd
+  depends_on = [module.eks_all_in_one]
+}
+data "aws_eks_cluster_auth" "argocd" {
+  name       = local.argocd_k8s_name
+  provider   = aws.argocd
+  depends_on = [module.eks_all_in_one]
+}
+
+provider "aws" {
+  alias       = "argocd"
+  access_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "us-east-2"
+  max_retries = 3
+}
+
+module "argocd" {
+  source            = "git::https://github.com/cisco-eti/sre-tf-module-argo-cluster-enrollment?ref=0.1.1"
+  eks_cluster_name  = local.name
+  eks_cluster_host  = data.aws_eks_cluster.eks.endpoint
+  eks_cluster_ca    = data.aws_eks_cluster.eks.certificate_authority[0].data
+  eks_cluster_token = data.aws_eks_cluster_auth.eks.token
+
+  argocd_cluster_host  = data.aws_eks_cluster.argocd.endpoint
+  argocd_cluster_token = data.aws_eks_cluster_auth.argocd.token
+  argocd_cluster_ca    = data.aws_eks_cluster.argocd.certificate_authority[0].data
+  providers = {
+    aws.eks    = aws.eks
+    aws.argocd = aws.argocd
+  }
+}
