4d057324067be1c2aa025d491bcb278aa08cfba2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 09:45:06 -0700

    Creating Base Branch for PR 567

---
+++ aws_lightspin-dev_eu-west-1_eks_cspm-dev-euw1-1_main.tf
@@ -0,0 +1,44 @@
+module "eks_all_in_one" {
+  source           = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
+  name             = local.name             # EKS cluster name
+  region           = local.region           # AWS provider region
+  aws_account_name = local.eks_aws_account_name # AWS account name
+  cidr             = "10.0.0.0/16"          # VPC CIDR
+  private_subnet_bits = 2                  # Private subnet bits
+  public_subnet_mask = 196
+  intra_subnet_mask = 200
+  cluster_version  = "1.30"                 # EKS cluster version
+
+  # EKS Managed Private Node Group
+  instance_types = ["m6a.2xlarge"] # EKS instance types
+  min_size       = 5               # EKS node group min size
+  max_size       = 15              # EKS node group max size
+  desired_size   = 7               # EKS node group desired size
+
+  # Karpenter
+  create_karpenter_irsa = true # Create Karpenter IRSA
+  create_otel_irsa = true
+  create_alb_irsa = true
+  create_ebs_csi_irsa = true 
+  create_efs_csi_irsa = true 
+
+  cluster_addons = {
+    coredns = {
+      most_recent = true
+    }
+    kube-proxy = {
+      most_recent = true
+    }
+    vpc-cni = {
+      most_recent = true
+      configuration_values = jsonencode({
+        env = {
+          # Reference docs https://docs.aws.amazon.com/eks/latest/userguide/cni-increase-ip-addresses.html
+          ENABLE_PREFIX_DELEGATION = "true"
+          WARM_PREFIX_TARGET       = "1"
+        }
+      })
+    }
+    # aws-ebs-csi-driver is not included to prevent installation
+  }
+}
