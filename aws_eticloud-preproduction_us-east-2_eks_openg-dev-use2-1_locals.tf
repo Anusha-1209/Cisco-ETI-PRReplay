c402070d7e29175b570b917625bfcae66f64fd22
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 13:40:34 -0700

    fix: change cidr range for openg-dev-use2-1

--- aws_eticloud-preproduction_us-east-2_eks_openg-dev-use2-1_locals.tf
+++ aws_eticloud-preproduction_us-east-2_eks_openg-dev-use2-1_locals.tf
@@ -3,7 +3,7 @@ locals {
   name               = "openg-dev-use2-1"
   region             = "us-east-2"
   eks_aws_account    = "eticloud-preprod"
-  vpc_cidr           = "10.0.0.0/16"
+  vpc_cidr           = "10.1.0.0/16"
   argocd_k8s_name    = "eks-dev-gitops-1"
   argocd_aws_account = "eticloud-preprod"
 }
