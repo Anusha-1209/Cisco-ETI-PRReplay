3914d7f3a9446861be7e5af11adeda994731096a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 11:22:12 -0700

    feat(dragonfly-prod): msk connect in prod env
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-prod_eu-central-1_msk_dragonly-msk-connect-1_locals.tf
+++ aws_dragonfly-prod_eu-central-1_msk_dragonly-msk-connect-1_locals.tf
@@ -4,8 +4,8 @@ locals {
   compute_vpc = "dragonfly-prod-euc1-1"
   data_vpc    = "dragonfly-prod-data-euc1-1"
 
-  arangodb_connector_plugin_name   = "kafka-connect-arangodb-1-2-0-dragonfly-5"
+  arangodb_connector_plugin_name   = "kafka-connect-arangodb-1-2-0-dragonfly-6"
   arangodb_connector_logs_bucket   = "dragonfly-prod-euc1-kafka-connector-log-files"
   arangodb_connector_plugin_bucket = "dragonfly-prod-euc1-binaries-repository"
-  arangodb_connector_plugin_jar    = "kafka-connect-arangodb-1.2.0-dragonfly-5.zip"
+  arangodb_connector_plugin_jar    = "kafka-connect-arangodb-1.2.0-dragonfly-6.zip"
 }
--- aws_dragonfly-prod_us-east-2_msk_dragonly-msk-connect-1_locals.tf
+++ aws_dragonfly-prod_us-east-2_msk_dragonly-msk-connect-1_locals.tf
@@ -4,8 +4,8 @@ locals {
   compute_vpc = "dragonfly-compute-prod-1-vpc"
   data_vpc    = "dragonfly-data-prod-1-vpc"
 
-  arangodb_connector_plugin_name   = "kafka-connect-arangodb-1-2-0-dragonfly-5"
+  arangodb_connector_plugin_name   = "kafka-connect-arangodb-1-2-0-dragonfly-6"
   arangodb_connector_logs_bucket   = "dragonfly-prod-kafka-connector-log-files"
   arangodb_connector_plugin_bucket = "dragonfly-prod-binaries-repository"
-  arangodb_connector_plugin_jar    = "kafka-connect-arangodb-1.2.0-dragonfly-5.zip"
+  arangodb_connector_plugin_jar    = "kafka-connect-arangodb-1.2.0-dragonfly-6.zip"
 }
