11a3a3b30e931caeb7beb3ea676cf08769d7da4d
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 14:13:42 -0700

    fix: revert previous temp changes

--- aws_outshift-common-prod_us-east-2_eks_plat-prod-use2-1_locals.tf
+++ aws_outshift-common-prod_us-east-2_eks_plat-prod-use2-1_locals.tf
@@ -1,10 +1,8 @@
 # This file was created by Outshift Platform Self-Service automation.
 locals {
-  name               = "plat-prod-use2-1"
-  region             = "us-east-2"
-  eks_aws_account    = "outshift-common-dev"
-  vpc_cidr           = "10.100.0.0/16"
-  argocd_k8s_name    = "eks-gitops-1"
-  argocd_aws_account = "eti-ci"
-  account_id         = data.aws_caller_identity.current.account_id
+  name            = "plat-prod-use2-1"
+  region          = "us-east-2"
+  eks_aws_account = "outshift-common-dev"
+  vpc_cidr        = "10.100.0.0/16"
+  account_id      = data.aws_caller_identity.current.account_id
 }
