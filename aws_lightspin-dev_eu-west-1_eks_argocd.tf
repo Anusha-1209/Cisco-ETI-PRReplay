ac1e13b46ebd1a430ab448077be3cb0da6deb569
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 23:39:12 -0700

    added ip for legacy karpenter

--- aws_lightspin-dev_eu-west-1_eks_argocd.tf
+++ aws_lightspin-dev_eu-west-1_eks_argocd.tf
@@ -1,15 +1,15 @@
-data "vault_generic_secret" "aws_argocd_infra_credential" {
-  provider = vault.eticloud
-  path     = "secret/infra/aws/${local.argocd_aws_account_name}/terraform_admin"
-}
+# data "vault_generic_secret" "aws_argocd_infra_credential" {
+#   provider = vault.eticloud
+#   path     = "secret/infra/aws/${local.argocd_aws_account_name}/terraform_admin"
+# }
 
-provider "aws" {
-  alias       = "argocd"
-  access_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region      = local.argocd_k8s_region
-  max_retries = 3
-}
+# provider "aws" {
+#   alias       = "argocd"
+#   access_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_ACCESS_KEY_ID"]
+#   secret_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+#   region      = local.argocd_k8s_region
+#   max_retries = 3
+# }
 
 # module "argocd" {
 #   source            = "git::https://github.com/cisco-eti/sre-tf-module-argo-cluster-enrollment?ref=0.1.1"
