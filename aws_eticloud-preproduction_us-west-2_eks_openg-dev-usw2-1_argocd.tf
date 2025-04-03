121a3ad81fc3cfe222628df9e76dfc2ec45ddfbf
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 13:43:41 -0700

    fix: use us-east-2 region for argocd provider

--- aws_eticloud-preproduction_us-west-2_eks_openg-dev-usw2-1_argocd.tf
+++ aws_eticloud-preproduction_us-west-2_eks_openg-dev-usw2-1_argocd.tf
@@ -9,7 +9,7 @@ provider "aws" {
   alias       = "argocd"
   access_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region      = "us-west-2"
+  region      = "us-east-2"
   max_retries = 3
 }
 
