4f41c11f4546c5b6211de835dfdbcdfd4b28487f
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 23:49:27 -0700

    added ip for legacy karpenter

--- aws_lightspin-dev_eu-west-1_vpc-peering_cspm-dev-euw1-1-peering_instanceprofile.tf
+++ aws_lightspin-dev_eu-west-1_vpc-peering_cspm-dev-euw1-1-peering_instanceprofile.tf
@@ -1,3 +1,8 @@
+locals {
+  name             = "cspm-dev-euw1-1"
+}
+
+
 # Instance profile for legacy karpenter
 resource "aws_iam_instance_profile" "ip" {
   name = "KarpenterNodeInstanceProfile-${local.name}"
