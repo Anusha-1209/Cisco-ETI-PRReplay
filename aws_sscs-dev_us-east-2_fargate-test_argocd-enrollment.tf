a283b513170c8a221b6cbf9caec542b54db359c0
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 02:44:05 -0700

    fargate test

--- aws_sscs-dev_us-east-2_fargate-test_argocd-enrollment.tf
+++ aws_sscs-dev_us-east-2_fargate-test_argocd-enrollment.tf
@@ -1,9 +1,9 @@
-# module "argocd" {
-#   source              = "git::https://github.com/cisco-eti/sre-tf-module-argo-cluster-enrollment?ref=main"
-#   argocd_cluster_name = "eks-gitops-cnapp-1"
-#   eks_cluster_name    = var.eks_name
-#   providers = {
-#     aws.eks = aws.eks
-#     aws.argocd = aws.argocd
-#   }
-# }
+module "argocd" {
+  source              = "git::https://github.com/cisco-eti/sre-tf-module-argo-cluster-enrollment?ref=main"
+  argocd_cluster_name = "eks-gitops-cnapp-1"
+  eks_cluster_name    = var.eks_name
+  providers = {
+    aws.eks = aws.eks
+    aws.argocd = aws.argocd
+  }
+}
