4ab756011ad9978ad3e230c3134f5c4f3edb8975
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 09:48:05 -0700

    -

--- aws_lightspin-dev_eu-west-1_eks_cspm-dev-euw1-1_instanceprofile.tf
+++ aws_lightspin-dev_eu-west-1_eks_cspm-dev-euw1-1_instanceprofile.tf
@@ -1,39 +1,3 @@
-provider "vault" {
-  alias     = "eticloud"
-  address   = "https://keeper.cisco.com"
-  namespace = "eticloud"
-}
-
-data "vault_generic_secret" "aws_infra_credential" {
-  provider = vault.eticloud
-  path     = "secret/infra/aws/${local.eks_aws_account_name}/terraform_admin"
-}
-
-provider "aws" {
-  alias       = "eks"
-  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region      = local.region
-  max_retries = 3
-  default_tags {
-    tags = {
-      ApplicationName    = local.name
-      CiscoMailAlias     = "eti-sre-admins@cisco.com"
-      DataClassification = "Cisco Restricted"
-      DataTaxonomy       = "Cisco Operations Data"
-      Environment        = "NonProd"
-      ResourceOwner      = "Outshift SRE"
-    }
-  }
-}
-
-locals {
-  name             = "cspm-dev-euw1-1"
-  region           = "eu-west-1"
-  eks_aws_account_name = "lightspin-dev"
-}
-
-
 # Instance profile for legacy karpenter
 resource "aws_iam_instance_profile" "ip" {
   provider = aws.eks
