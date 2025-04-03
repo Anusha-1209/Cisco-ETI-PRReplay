b980c52e0a0d5e624b37cd2ebb9d4fbc3324a975
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 14:29:34 -0700

    Creating Base Branch for PR 601

---
+++ aws_outshift-common-prod_us-east-2_eks_plat-prod-use2-1_locals.tf
@@ -0,0 +1,8 @@
+# This file was created by Outshift Platform Self-Service automation.
+locals {
+  name            = "plat-prod-use2-1"
+  region          = "us-east-2"
+  eks_aws_account = "outshift-common-dev"
+  vpc_cidr        = "10.100.0.0/16"
+  account_id      = "058264538874"
+}
