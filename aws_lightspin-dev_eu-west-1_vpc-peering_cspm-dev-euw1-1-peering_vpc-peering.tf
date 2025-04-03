8b11c7a0b703ccae4055c0b8a4e2b336711320ae
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 23:33:11 -0700

    vpc-peering

---
+++ aws_lightspin-dev_eu-west-1_vpc-peering_cspm-dev-euw1-1-peering_vpc-peering.tf
@@ -0,0 +1,19 @@
+terraform {
+  backend "s3" {
+    bucket = "eticloud-tf-state-nonprod"
+    key    = "terraform-state/vpc-peering/eu-west-1/cspm-dev-euw1-1-data.tfstate"
+    region = "us-east-2"
+  }
+}
+
+module "vpc_peering_primary_to_primary"{
+  source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=1.1.1"
+  aws_accounts_to_regions = {
+    "common" = {
+      account_name = "lightspin-dev"
+      region       = "eu-west-1"
+    }
+  }
+  accepter_vpc_name  = "cspm-dev-euw1-1"
+  requester_vpc_name = "lightspin-dev-1-vpc"
+}
