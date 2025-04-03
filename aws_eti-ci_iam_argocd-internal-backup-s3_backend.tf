c7d8edaa76efa2e487f270141ccfcabf6ae0520a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 23:08:13 -0700

    chore: create argocd-internal-backup-user IAM user

---
+++ aws_eti-ci_iam_argocd-internal-backup-s3_backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "eticloud-tf-state-nonprod"
+    key            = "terraform-state/aws-eti-ci/iam/argocd-internal-backup-s3.tfstate"
+    region         = "us-east-2"
+    dynamodb_table = "eticloud-tf-locks"
+    encrypt        = true
+  }
+}
