fac1113b0e84dbcb5263cb7be1db66b25cd8a2d3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 13:36:24 -0700

    fix: using wrong data resource

--- aws_eticloud-preproduction_us-west-2_eks_openg-dev-usw2-1_eso.tf
+++ aws_eticloud-preproduction_us-west-2_eks_openg-dev-usw2-1_eso.tf
@@ -8,7 +8,7 @@ module "eso_eticloud" {
   source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
   cluster_name    = local.name
   vault_namespace = "eticloud"
-  kubernetes_host = data.aws_eks_cluster.cluster.endpoint
+  kubernetes_host = data.aws_eks_cluster.eks.endpoint
   kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
   policies        = ["external-secrets-${local.name}"]
 }
