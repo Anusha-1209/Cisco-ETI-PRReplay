84efce48b3fbb0686d2ded833a8d8df08f8664c5
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 10:14:12 -0700

    chore(github.com/cisco-eti): reconcile org secrets and variables

--- github.com_outshift-platform_gh_actions_org_secrets.tf
+++ github.com_outshift-platform_gh_actions_org_secrets.tf
@@ -11,6 +11,11 @@ data "vault_generic_secret" "GHCR_TOKEN" {
 data "github_actions_organization_public_key" "gha_org_public_key" {
 }
 
+data "vault_generic_secret" "gha_approle" {
+  provider = vault.eticloud
+  path     = "ci/gha/approle"
+}
+
 data "sodium_encrypted_item" "GHCR_TOKEN" {
   public_key_base64 = data.github_actions_organization_public_key.gha_org_public_key.key
   content_base64 = base64encode(data.vault_generic_secret.GHCR_TOKEN.data["PAT"])
@@ -23,12 +28,12 @@ data "sodium_encrypted_item" "GHCR_USERNAME" {
 
 data "sodium_encrypted_item" "VAULT_APPROLE_ROLE_ID" {
   public_key_base64 = data.github_actions_organization_public_key.gha_org_public_key.key
-  content_base64 = base64encode(vault_approle_auth_backend_role.github_actions.role_id)
+  content_base64 = base64encode(data.vault_generic_secret.gha_approle.data["VAULT_APPROLE_ROLE_ID"])
 }
 
 data "sodium_encrypted_item" "VAULT_APPROLE_SECRET_ID" {
   public_key_base64 = data.github_actions_organization_public_key.gha_org_public_key.key
-  content_base64 = base64encode(vault_approle_auth_backend_role_secret_id.github_actions.secret_id)
+  content_base64 = base64encode(data.vault_generic_secret.gha_approle.data["VAULT_APPROLE_SECRET_ID"])
 }
 
 data "sodium_encrypted_item" "WEBEX_PLATFORM_NOTIFICATION_ROOM_ID" {
