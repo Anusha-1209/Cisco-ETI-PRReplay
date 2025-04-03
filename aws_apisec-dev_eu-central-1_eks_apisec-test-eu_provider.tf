faa8e85c642a4dc8d9f4fd304b29028576088293
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 22:04:39 -0700

    fix: add the provider config back

--- aws_apisec-dev_eu-central-1_eks_apisec-test-eu_provider.tf
+++ aws_apisec-dev_eu-central-1_eks_apisec-test-eu_provider.tf
@@ -27,3 +27,18 @@ provider "aws" {
     }
   }
 }
+
+# This file was created by Outshift Platform Self-Service automation.
+
+data "vault_generic_secret" "aws_argocd_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/${local.argocd_aws_account}/terraform_admin"
+}
+
+provider "aws" {
+  alias       = "argocd"
+  access_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "us-east-2"
+  max_retries = 3
+}
