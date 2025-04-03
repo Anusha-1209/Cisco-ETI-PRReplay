5af5dbf85b05233a83dff5898550a309dfea6c9a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 02:56:32 -0700

    fargate test

--- aws_sscs-dev_us-east-2_fargate-test_argocd-enrollment.tf
+++ aws_sscs-dev_us-east-2_fargate-test_argocd-enrollment.tf
@@ -1,9 +1,36 @@
+# This file was created by Outshift Platform Self-Service automation.
+locals {
+  argocd_k8s_name    = "eks-dev-gitops-1"
+  argocd_aws_account = "eticloud-preprod"
+}
+
+
+
+data "vault_generic_secret" "aws_argocd_infra_credential" {
+  provider = vault.eticloud
+  path     = "secret/infra/aws/${local.argocd_aws_account}/terraform_admin"
+}
+
+provider "aws" {
+  alias       = "argocd"
+  access_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = "us-east-2"
+  max_retries = 3
+}
+
 module "argocd" {
-  source              = "git::https://github.com/cisco-eti/sre-tf-module-argo-cluster-enrollment?ref=0.1.0"
-  argocd_cluster_name = "eks-gitops-cnapp-1"
-  eks_cluster_name    = var.eks_name
+  source            = "git::https://github.com/cisco-eti/sre-tf-module-argo-cluster-enrollment?ref=0.1.1"
+  eks_cluster_name  = var.eks_name
+  eks_cluster_host  = data.aws_eks_cluster.eks.endpoint
+  eks_cluster_ca    = data.aws_eks_cluster.eks.certificate_authority[0].data
+  eks_cluster_token = data.aws_eks_cluster_auth.eks.token
+
+  argocd_cluster_host  = data.aws_eks_cluster.argocd.endpoint
+  argocd_cluster_token = data.aws_eks_cluster_auth.argocd.token
+  argocd_cluster_ca    = data.aws_eks_cluster.argocd.certificate_authority[0].data
   providers = {
-    aws.eks = aws.eks
+    aws.eks    = aws
     aws.argocd = aws.argocd
   }
 }
