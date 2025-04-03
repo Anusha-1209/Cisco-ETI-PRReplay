5ba61a902547dbe5d7f4435881d7fcc49bd65aab
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 11:43:40 -0700

    chore: update the vpc name

--- aws_vowel-genai-dev_us-east-2_vpc_motf-mm-dev-1_vpc.tf
+++ aws_vowel-genai-dev_us-east-2_vpc_motf-mm-dev-1_vpc.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "eticloud-tf-state-nonprod"
-    key    = "terraform-state/vpc/us-east-2/motf-mm-dev-1-vpc.tfstate"
+    key    = "terraform-state/vpc/us-east-2/motf-mm-dev-1-data.tfstate"
     region = "us-east-2"
   }
 }
@@ -12,7 +12,6 @@ provider "vault" {
   namespace = "eticloud"
 }
 
-# AWS credentails are the same of cwpp-dev vault secret (we deploy on same aws account)
 data "vault_generic_secret" "aws_infra_credential" {
   path     = "secret/infra/aws/vowel-genai-dev/terraform_admin"
   provider = vault.eticloud
@@ -25,7 +24,7 @@ provider "aws" {
   max_retries = 3
   default_tags {
     tags = {
-      ApplicationName    = "motf-mm-dev-1-vpc"
+      ApplicationName    = "motf-mm-dev-1-data"
       CiscoMailAlias     = "eti-sre-admins@cisco.com"
       DataClassification = "Cisco Confidential"
       DataTaxonomy       = "Cisco Operations Data"
@@ -39,7 +38,7 @@ module "vpc" {
   source                          = "git::https://github.com/cisco-eti/sre-tf-module-aws-vpc?ref=2.0.6"
   region                          = "us-east-2"
   vpc_cidr                        = "10.34.0.0/16"
-  vpc_name                        = "motf-mm-dev-1-vpc"
+  vpc_name                        = "motf-mm-dev-1-data"
   cluster_name                    = "vowel-dev-1"
   create_database_subnet_group    = true
   create_elasticache_subnet_group = false
