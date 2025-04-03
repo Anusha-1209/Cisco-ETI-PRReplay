1a3d19e475c60f2db6e99b984fed5ff637606a5e
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 11:24:24 -0700

    feat(dragonfly-prod): msk connect in prod env
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-staging_eu-west-1_msk_dragonly-msk-connect-1_locals.tf
+++ aws_dragonfly-staging_eu-west-1_msk_dragonly-msk-connect-1_locals.tf
@@ -4,10 +4,10 @@ locals {
   compute_vpc                     = "dragonfly-compute-staging-1-vpc"
   data_vpc                        = "dragonfly-data-staging-1-vpc"
 
-  arangodb_connector_plugin_name   = "kafka-connect-arangodb-1-2-0-dragonfly-4"
+  arangodb_connector_plugin_name   = "kafka-connect-arangodb-1-2-0-dragonfly-6"
   arangodb_connector_logs_bucket   = "dragonfly-staging-kafka-connector-log-files"
   arangodb_connector_plugin_bucket = "dragonfly-staging-binaries-repository"
-  arangodb_connector_plugin_jar    = "kafka-connect-arangodb-1.2.0-dragonfly-4.zip"
+  arangodb_connector_plugin_jar    = "kafka-connect-arangodb-1.2.0-dragonfly-6.zip"
 
   arangodb_secret_name            = "thrill-arangodb"
 }
