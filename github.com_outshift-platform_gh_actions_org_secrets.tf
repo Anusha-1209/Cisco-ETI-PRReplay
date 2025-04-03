750383e809907b6994b9bdd1b9d7bcb73a01c6fe
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 01:19:02 -0700

    fix(github.com): update GHA secrets

--- github.com_outshift-platform_gh_actions_org_secrets.tf
+++ github.com_outshift-platform_gh_actions_org_secrets.tf
@@ -8,22 +8,21 @@ data "github_actions_organization_public_key" "gha_org_public_key" {
 
 data "sodium_encrypted_item" "GHCR_TOKEN" {
   public_key_base64 = data.github_actions_organization_public_key.gha_org_public_key.key
-  content_base64 = base64encode(data.vault_generic_secret.gha_ci_secrets.data["GHEC_PAT"])
+  content_base64 = base64encode(data.vault_generic_secret.generic_user_gh_token.data["GHA_GHCR_TOKEN"])
 }
 
 data "sodium_encrypted_item" "GHCR_USERNAME" {
   public_key_base64 = data.github_actions_organization_public_key.gha_org_public_key.key
   content_base64 = base64encode(data.vault_generic_secret.gha_ci_secrets.data["GHEC_USERNAME"])
 }
-
 data "sodium_encrypted_item" "VAULT_APPROLE_ROLE_ID" {
   public_key_base64 = data.github_actions_organization_public_key.gha_org_public_key.key
-  content_base64 = base64encode(data.vault_generic_secret.gha_ci_secrets.data["VAULT_APPROLE_ROLE_ID"])
+  content_base64 = base64encode(vault_approle_auth_backend_role.github_actions.role_id)
 }
 
 data "sodium_encrypted_item" "VAULT_APPROLE_SECRET_ID" {
   public_key_base64 = data.github_actions_organization_public_key.gha_org_public_key.key
-  content_base64 = base64encode(data.vault_generic_secret.gha_ci_secrets.data["VAULT_APPROLE_SECRET_ID"])
+  content_base64 = base64encode(vault_approle_auth_backend_role_secret_id.github_actions.secret_id)
 }
 
 data "sodium_encrypted_item" "WEBEX_PLATFORM_NOTIFICATION_ROOM_ID" {
@@ -51,4 +50,8 @@ resource "github_actions_organization_secret" "dynamic" {
   secret_name      = each.key
   visibility       = "private"
   encrypted_value  = each.value
+
+  lifecycle {
+    ignore_changes = [ encrypted_value ]
+  }
 }
