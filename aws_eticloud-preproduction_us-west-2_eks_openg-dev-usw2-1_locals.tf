e1ec44944c33dd3a0ea16a9e144bd0649412daf6
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 13:27:50 -0700

    SRE-9807 feat: create new OPENG-DEV cluster for platform services dev env

---
+++ aws_eticloud-preproduction_us-west-2_eks_openg-dev-usw2-1_locals.tf
@@ -0,0 +1,9 @@
+# This file was created by Outshift Platform Self-Service automation.
+locals {
+  name               = "openg-dev-usw2-1"
+  region             = "us-west-2"
+  eks_aws_account    = "eticloud-preproduction"
+  vpc_cidr           = "10.2.0.0/16"
+  argocd_k8s_name    = "eks-dev-gitops-1"
+  argocd_aws_account = "eticloud-preproduction"
+}
