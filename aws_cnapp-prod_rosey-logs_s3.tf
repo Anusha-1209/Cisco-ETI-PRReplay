52e0dbf4f262a8f08ff859a43f1ece60179b806e
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 01:34:40 -0700

    chore(data-sync): remove unneeded S3 bucket for EU logs

--- aws_cnapp-prod_rosey-logs_s3.tf
+++ aws_cnapp-prod_rosey-logs_s3.tf
@@ -1,9 +1,6 @@
 # https://github.com/hashicorp/terraform/issues/24476
 # have to repeat module call due to using 2 providers for each AWS region
 module "s3-us" {
-  providers = {
-    aws = aws.us
-  }
   source      = "git::https://github.com/cisco-eti/sre-tf-module-aws-s3.git?ref=1.0.2"
   bucket_name = "rosey-logs-us"
   # Continuous Security Buddy Tags.
@@ -17,39 +14,9 @@ module "s3-us" {
   CSBDataTaxonomy       = "Cisco Operations Data"
 }
 
-module "s3-eu" {
-  providers = {
-    aws = aws.eu
-  }
-  source      = "git::https://github.com/cisco-eti/sre-tf-module-aws-s3.git?ref=1.0.2"
-  bucket_name = "rosey-logs-eu"
-  # Continuous Security Buddy Tags.
-  # For more information, see the CSB tagging Sharepoint page here:
-  # https://cisco.sharepoint.com/Sites/CSB/SitePages/Security%20Tagging%20and%20Audit%20in%20AWS.aspx
-  CSBDataClassification = "Cisco Highly Confidential"
-  CSBEnvironment        = "Prod"
-  CSBApplicationName    = "rosey-logs-eu"
-  CSBResourceOwner      = "eti"
-  CSBCiscoMailAlias     = "eti-sre@cisco.com"
-  CSBDataTaxonomy       = "Cisco Operations Data"
-}
-
 resource "aws_s3_bucket_lifecycle_configuration" "rosey_logs_us" {
   provider = aws.us
   bucket   = "rosey-logs-us"
-  rule {
-    id     = "TTL-policy"
-    status = "Enabled"
-
-    expiration {
-      days = 7
-    }
-  }
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "rosey_logs_eu" {
-  provider = aws.eu
-  bucket   = "rosey-logs-eu"
   rule {
     id     = "TTL-policy"
     status = "Enabled"
