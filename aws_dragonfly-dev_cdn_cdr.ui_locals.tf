6ac333552baa0ee48033969f19ec21aa9e17cd92
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 23:57:25 -0700

    chore: update acm arn

--- aws_dragonfly-dev_cdn_cdr.ui_locals.tf
+++ aws_dragonfly-dev_cdn_cdr.ui_locals.tf
@@ -1,4 +1,5 @@
 locals {
-  aws_account        = "eticloud"
-  bucket_domain_name = "dragonfly-dev-cdr-ui.s3.eu-west-1.amazonaws.com"
+  aws_account         = "eticloud"
+  bucket_domain_name  = "dragonfly-dev-cdr-ui.s3.eu-west-1.amazonaws.com"
+  acm_certificate_arn = "arn:aws:acm:eu-west-1:626007623524:certificate/65ef0295-b717-485f-ad71-6d298dd6369e"
 }
