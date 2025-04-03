3032abb77ed0252909c769555d1e81f5505739e3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 11:05:55 -0700

    feat(dragonfly-prod): msk connect in prod env
    Signed-off-by: Mauro Sardara <msardara@cisco.com>

--- aws_dragonfly-prod_eu-central-1_msk_dragonly-msk-connect-1_backend.tf
+++ aws_dragonfly-prod_eu-central-1_msk_dragonly-msk-connect-1_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eticloud-tf-state-nonprod"
+    bucket = "eticloud-tf-state-prod"
     key    = "terraform-state/aws-dragonfly-production/msk/eu-central-1/dragonfly-msk-connect-1.tfstate"
     region = "us-east-2"
   }
--- aws_dragonfly-prod_us-east-2_msk_dragonly-msk-connect-1_backend.tf
+++ aws_dragonfly-prod_us-east-2_msk_dragonly-msk-connect-1_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eticloud-tf-state-nonprod"
+    bucket = "eticloud-tf-state-prod"
     key    = "terraform-state/aws-dragonfly-production/msk/us-east-2/dragonfly-msk-connect-1.tfstate"
     region = "us-east-2"
   }
