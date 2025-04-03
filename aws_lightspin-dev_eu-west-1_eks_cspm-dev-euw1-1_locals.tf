267eedab6a46de261aebdb5c557e2757967d6abd
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 02:00:43 -0700

    -

--- aws_lightspin-dev_eu-west-1_eks_cspm-dev-euw1-1_locals.tf
+++ aws_lightspin-dev_eu-west-1_eks_cspm-dev-euw1-1_locals.tf
@@ -2,7 +2,7 @@ locals {
   name             = "cspm-dev-euw1-1"
   region           = "eu-west-1"
   eks_aws_account_name = "lightspin-dev"
-  argocd_k8s_name    = "eks-gitops-cnapp-1"
-  argocd_k8s_region  = "eu-north-1"
-  argocd_aws_account_name = "eti-ci"
+  # argocd_k8s_name    = "eks-gitops-cnapp-1"
+  # argocd_k8s_region  = "eu-north-1"
+  # argocd_aws_account_name = "eti-ci"
 }
