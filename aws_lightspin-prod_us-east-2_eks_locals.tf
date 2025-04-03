3915034a8e08de788adf0967474c2d8f0081a3e2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 22:07:09 -0700

    feat: Add light-prod-1

---
+++ aws_lightspin-prod_us-east-2_eks_locals.tf
@@ -0,0 +1,8 @@
+locals {
+  name             = "cspm-prod-use2-1"
+  region           = "us-east-2"
+  eks_aws_account_name = "lightspin-prod"
+  argocd_k8s_name    = "eks-gitops-cnapp-1"
+  argocd_k8s_region  = "eu-north-1"
+  argocd_aws_account_name = "eti-ci"
+}
