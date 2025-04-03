b8ae4c7d9c7b3375b9cc1ba2e9d393709be1f4b3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 13:07:41 -0700

    fix: issue with sebnets being changed

--- aws_outshift-common-staging_us-east-2_eks_comn-staging-use2-1_main.tf
+++ aws_outshift-common-staging_us-east-2_eks_comn-staging-use2-1_main.tf
@@ -18,11 +18,13 @@ locals {
 module "eks_all_in_one" {
   source = "git::https://github.com/cisco-eti/sre-tf-module-eks-allinone.git?ref=latest"
 
-  name             = local.name             # EKS cluster name
-  region           = local.region           # AWS provider region
-  aws_account_name = local.aws_account_name # AWS account name
-  cidr             = "10.0.0.0/16"          # VPC CIDR
-  cluster_version  = "1.29"                 # EKS cluster version
+  name               = local.name             # EKS cluster name
+  region             = local.region           # AWS provider region
+  aws_account_name   = local.aws_account_name # AWS account name
+  cidr               = "10.0.0.0/16"          # VPC CIDR
+  public_subnet_mask = 196
+  intra_subnet_mask  = 200
+  cluster_version    = "1.29"                 # EKS cluster version
 
   # EKS Managed Private Node Group
   instance_types = ["m6a.2xlarge"] # EKS instance types
