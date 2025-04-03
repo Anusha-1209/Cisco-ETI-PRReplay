b0a6927978d1f84b6c1ac5e8d30a1f5691811c02
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-03 02:42:02 -0700

    fargate test

---
+++ aws_sscs-dev_us-east-2_fargate-test_variables.tf
@@ -0,0 +1,9 @@
+variable "aws_default_region" {
+    description = "Change this default to move the VPC into a a different region."
+    type        = string
+}
+
+variable "eks_name" {
+    type        = string
+  
+}
