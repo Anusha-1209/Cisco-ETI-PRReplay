a0f8c7c06159e4c3250675dafd3f25afac47dea5
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-02 23:04:40 -0700

    fix: remove delete clusters

--- aws_eticloud-scratch-c_us-east-2_eks_ub20-129-root_locals.tf
+++
@@ -1,7 +0,0 @@
-# This file was created by Outshift Platform Self-Service automation.
-locals {
-  name             = "ub20-129-root"
-  region           = "us-east-2"
-  aws_account_name = "eticloud-scratch-c"
-  vpc_cidr         = "10.0.0.0/16"
-}
--- aws_eticloud-scratch-c_us-east-2_eks_validate-baseapps_locals.tf
+++
@@ -1,9 +0,0 @@
-# This file was created by Outshift Platform Self-Service automation.
-locals {
-  name               = "validate-baseapps"
-  argocd_k8s_name    = "eks-dev-gitops-1"
-  argocd_aws_account = "eticloud-preprod"
-  region             = "us-east-2"
-  aws_account_name   = "eticloud-scratch-c"
-  vpc_cidr           = "10.0.0.0/16"
-}
