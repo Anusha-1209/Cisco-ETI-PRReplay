c6ecebf3c0a3d3ea2300ba498eee7eca60864ddc
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 13:16:29 -0700

    chore: add vpc peering between the new eks clusters and mongodb vpc

--- aws_outshift-common-staging_global_cross-region-vpc-peering_cross-account_vpc-peering.tf
+++ aws_outshift-common-staging_global_cross-region-vpc-peering_cross-account_vpc-peering.tf
@@ -21,3 +21,35 @@ module "primary_cluster_to_secondary"{
   accepter_vpc_name  = "prod-db-vpc-1"
   requester_vpc_name  = "common-staging-use2-vpc-data"
 }
+
+module "primary_eks_cluster_to_secondary"{
+  source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=1.1.1"
+  aws_accounts_to_regions = {
+    "accepter" = {
+      account_name = "eticloud"
+      region       = "us-east-2"
+    }
+    "requester" = {
+      account_name = "outshift-common-staging"
+      region       = "us-east-2"
+    }
+  }
+  accepter_vpc_name  = "prod-db-vpc-1"
+  requester_vpc_name  = "common-staging-use2-1"
+}
+
+module "secondary_eks_cluster_to_secondary"{
+  source             = "git::https://github.com/cisco-eti/sre-tf-module-multi-region-vpc-peering.git?ref=1.1.1"
+  aws_accounts_to_regions = {
+    "accepter" = {
+      account_name = "eticloud"
+      region       = "us-east-2"
+    }
+    "requester" = {
+      account_name = "outshift-common-staging"
+      region       = "us-west-2"
+    }
+  }
+  accepter_vpc_name  = "prod-db-vpc-1"
+  requester_vpc_name  = "common-staging-usw2-1"
+}
