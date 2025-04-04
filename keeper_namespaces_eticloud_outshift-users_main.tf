b35879aa016588fb66d8116fed43069644fd15db
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 12:20:50 -0700

    chore: add keeper kv for smith

--- keeper_namespaces_eticloud_outshift-users_main.tf
+++ keeper_namespaces_eticloud_outshift-users_main.tf
@@ -61,6 +61,13 @@ resource "vault_mount" "generic" {
   options  = { version = "2" }
 }
 
+resource "vault_mount" "smith" {
+  provider = vault.venture
+  path     = "smith"
+  type     = "kv"
+  options  = { version = "2" }
+}
+
 # OIDC Credentials
 data "vault_generic_secret" "oidc_credential" {
   provider = vault.teamsecrets
@@ -232,6 +239,45 @@ resource "vault_policy" "generic-user" {
   # Manage auth methods broadly across Vault
 # List, create, update, and delete key/value secrets
 path "generic/*"
+{
+  capabilities = ["read", "list"]
+}
+EOT
+}
+# Outshift smith Vault Role
+resource "vault_jwt_auth_backend_role" "smith" {
+  depends_on = [vault_policy.smith]
+  provider   = vault.venture
+  role_name  = "smith"
+  role_type  = "oidc"
+  backend    = vault_jwt_auth_backend.oidc.path
+  allowed_redirect_uris = ["https://keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+    "https://east.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+  "https://west.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback"]
+  bound_audiences = [var.oidc_client_id]
+  bound_claims = {
+    memberof = "CN=outshift-vault-smith,OU=Cisco Groups,DC=cisco,DC=com"
+  }
+  disable_bound_claims_parsing = true
+  bound_claims_type            = "string"
+  claim_mappings = {
+    email       = "email",
+    family_name = "family_name",
+    given_name  = "given_name",
+    sub         = "sub"
+  }
+  groups_claim = "memberof"
+  oidc_scopes  = ["profile", "email", "openid"]
+  user_claim   = "sub"
+  token_policies = [vault_policy.smith.name]
+}
+resource "vault_policy" "smith" {
+  provider = vault.venture
+  name     = "smith"
+  policy   = <<EOT
+# Manage auth methods broadly across Vault
+# List, create, update, and delete key/value secrets
+path "smith/*"
 {
   capabilities = ["read", "list"]
 }
