b9dc434426b9e87b25688b56eb53a0ab35488227
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 13:58:26 -0700

    chore: use local var

--- aws_outshift-common-dev_us-east-2_eks_jarvis-sandbox_provider.tf
+++ aws_outshift-common-dev_us-east-2_eks_jarvis-sandbox_provider.tf
@@ -14,7 +14,7 @@ provider "aws" {
   alias       = "eks"
   access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region      = "us-east-2"
+  region      = local.region
   max_retries = 3
   default_tags {
     tags = {
