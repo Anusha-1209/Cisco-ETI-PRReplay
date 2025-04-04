164805f57d59d42297f3d42546a6aa3745f872ad
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 12:54:59 -0700

    chore: create smith openai project

--- azure_eti-scratch_azure-openai_project-agents-akram-sheriff.tf
+++ azure_eti-scratch_azure-openai_project-agents-akram-sheriff.tf
@@ -1,24 +1,26 @@
 locals {
-  name   = "akram-sheriff-project-agents"
+  name   = "smith-project-agents"
   region = "eastus"
 }
-resource "azurerm_resource_group" "akram-sheriff-project-agents" {
+resource "azurerm_resource_group" "smith-project-agents" {
   name     = local.name
   location = local.region
 }
 
-resource "azurerm_cognitive_account" "akram-sheriff-project-agents" {
+resource "azurerm_cognitive_account" "smith-project-agents" {
   name                  = "${local.name}"
   custom_subdomain_name = "${local.name}"
-  location              = azurerm_resource_group.akram-sheriff-project-agents.location
-  resource_group_name   = azurerm_resource_group.akram-sheriff-project-agents.name
+  location              = azurerm_resource_group.smith-project-agents.location
+  resource_group_name   = azurerm_resource_group.smith-project-agents.name
   kind                  = "OpenAI"
   sku_name              = "S0"
 }
 
-resource "azurerm_cognitive_deployment" "akram-sheriff-project-agents-gpt4o" {
+z
+
+resource "azurerm_cognitive_deployment" "smith-project-agents-gpt4o" {
   name                 = "gpt-4o"
-  cognitive_account_id = azurerm_cognitive_account.akram-sheriff-project-agents.id
+  cognitive_account_id = azurerm_cognitive_account.smith-project-agents.id
   rai_policy_name      = "HIGH_INPUT_OUTPUT_FILTER"
   model {
     format  = "OpenAI"
@@ -32,9 +34,9 @@ resource "azurerm_cognitive_deployment" "akram-sheriff-project-agents-gpt4o" {
   }
 }
 
-# resource "azurerm_cognitive_deployment" "akram-sheriff-project-agents-gpt4o-mini" {
+# resource "azurerm_cognitive_deployment" "smith-project-agents-gpt4o-mini" {
 #   name                 = "gpt-4o-mini"
-#   cognitive_account_id = azurerm_cognitive_account.akram-sheriff-project-agents.id
+#   cognitive_account_id = azurerm_cognitive_account.smith-project-agents.id
 #   rai_policy_name      = "HIGH_INPUT_OUTPUT_FILTER"
 #   model {
 #     format  = "OpenAI"
