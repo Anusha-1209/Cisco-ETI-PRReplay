1415326d4089edab2351bd9c89a7fcf6a93fa3c3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 11:46:57 -0700

    chore: update eso for sre namespace for plat clusters

--- aws_outshift-common-dev_us-east-2_eks_plat-dev-use2-1_eso.tf
+++ aws_outshift-common-dev_us-east-2_eks_plat-dev-use2-1_eso.tf
@@ -5,11 +5,19 @@ data "vault_generic_secret" "cluster_certificate" {
 }
 
 module "eso_eticloud" {
-  source = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
-
+  source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
   cluster_name    = local.name
   vault_namespace = "eticloud"
   kubernetes_host = data.aws_eks_cluster.eks.endpoint
+  kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
+  policies        = ["external-secrets-${local.name}"]
+}
+
+module "eso_eticloud_apps_sre" {
+  source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
+  cluster_name    = local.name
+  vault_namespace = "eticloud/apps/sre"
+  kubernetes_host = data.aws_eks_cluster.cluster.endpoint
   kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
   policies        = ["external-secrets-${local.name}"]
 }
