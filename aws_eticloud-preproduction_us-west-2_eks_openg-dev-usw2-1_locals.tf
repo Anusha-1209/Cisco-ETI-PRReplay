239c9984bc16a382b7c90f1ca67a6d5b36422625
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 11:52:54 -0700

    chore: delete resources that were removed

--- aws_eticloud-preproduction_us-west-2_eks_openg-dev-usw2-1_locals.tf
+++
@@ -1,9 +0,0 @@
-# This file was created by Outshift Platform Self-Service automation.
-locals {
-  name               = "openg-dev-usw2-1"
-  region             = "us-west-2"
-  eks_aws_account    = "eticloud-preprod"
-  vpc_cidr           = "10.2.0.0/16"
-  argocd_k8s_name    = "eks-dev-gitops-1"
-  argocd_aws_account = "eticloud-preprod"
-}
