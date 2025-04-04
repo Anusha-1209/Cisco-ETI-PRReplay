881cef566d1c018b87f24543018c1482b3d947b5
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 15:15:46 -0700

    chore: Create new Azure OpenAI API key for outshift_foundationa_services application

--- keeper_namespaces_eticloud_outshift-users_main.tf
+++ keeper_namespaces_eticloud_outshift-users_main.tf
@@ -157,3 +157,43 @@ path "engineering_rd/*"
 }
 EOT
 }
+
+# Outshift outshift_foundational_services Vault Role
+resource "vault_jwt_auth_backend_role" "outshift_foundational_services" {
+  depends_on = [vault_policy.outshift_foundational_services]
+  provider   = vault.venture
+  role_name  = "outshift_foundational_services"
+  role_type  = "oidc"
+  backend    = vault_jwt_auth_backend.oidc.path
+  allowed_redirect_uris = ["https://keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
+    "http://localhost:8250/oidc/callback",
+  "https://west.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback"]
+  bound_audiences = [var.oidc_client_id]
+  bound_claims = {
+    memberof = "CN=outshift-vault-outshift-foundational-services,OU=Cisco Groups,DC=cisco,DC=com"
+  }
+  disable_bound_claims_parsing = true
+  bound_claims_type            = "string"
+  claim_mappings = {
+    email       = "email",
+    family_name = "family_name",
+    given_name  = "given_name",
+    sub         = "sub"
+  }
+  groups_claim = "memberof"
+  oidc_scopes  = ["profile", "email", "openid"]
+  user_claim   = "sub"
+  token_policies = [vault_policy.outshift_foundational_services.name]
+}
+resource "vault_policy" "outshift_foundational_services" {
+  provider = vault.venture
+  name     = "outshift_foundational_services"
+  policy   = <<EOT
+# Manage auth methods broadly across Vault
+# List, create, update, and delete key/value secrets
+path "outshift_foundational_services/*"
+{
+  capabilities = ["read", "list"]
+}
+EOT
+}
