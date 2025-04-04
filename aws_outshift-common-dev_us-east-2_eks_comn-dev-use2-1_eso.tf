3b6b7bb7abb44df136e2d11d9bb28e50a60275d5
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 12:12:55 -0700

    fix(comn-dev-use2-1): add ESO eticloud/apps/research; remove appnet

--- aws_outshift-common-dev_us-east-2_eks_comn-dev-use2-1_eso.tf
+++ aws_outshift-common-dev_us-east-2_eks_comn-dev-use2-1_eso.tf
@@ -35,6 +35,16 @@ module "eso_eticloud" {
   policies        = ["external-secrets-${local.name}"]
 }
 
+module "eso_eticloud_apps_appnet" {
+  count = 0 # This module is not needed for this cluster
+  source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
+  cluster_name    = local.name
+  vault_namespace = "eticloud/apps/appnet"
+  kubernetes_host = data.aws_eks_cluster.cluster.endpoint
+  kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
+  policies        = ["external-secrets-${local.name}"]
+}
+
 module "eso_eticloud_apps_research" {
   source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
   cluster_name    = local.name
@@ -43,7 +53,6 @@ module "eso_eticloud_apps_research" {
   kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
   policies        = ["external-secrets-${local.name}"]
 }
-
 module "eso_eticloud_apps_sre" {
   source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
   cluster_name    = local.name
