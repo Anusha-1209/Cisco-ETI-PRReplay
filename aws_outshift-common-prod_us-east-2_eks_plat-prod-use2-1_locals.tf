d8c1bce58f50255d6a6e7c74964b36714de4405b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 09:49:15 -0700

    fix: wrong aws account in the locals

--- aws_outshift-common-prod_us-east-2_eks_plat-prod-use2-1_locals.tf
+++ aws_outshift-common-prod_us-east-2_eks_plat-prod-use2-1_locals.tf
@@ -2,7 +2,7 @@
 locals {
   name            = "plat-prod-use2-1"
   region          = "us-east-2"
-  eks_aws_account = "outshift-common-dev"
+  eks_aws_account = "outshift-common-prod"
   vpc_cidr        = "10.100.0.0/16"
   account_id      = "058264538874"
 }
