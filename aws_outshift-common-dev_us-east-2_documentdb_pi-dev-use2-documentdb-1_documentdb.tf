0f85349b5d05c3542d79a957e1500690b7940722
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 11:09:22 -0700

    chore: upgrade documentDB module

--- aws_outshift-common-dev_us-east-2_documentdb_pi-dev-use2-documentdb-1_documentdb.tf
+++ aws_outshift-common-dev_us-east-2_documentdb_pi-dev-use2-documentdb-1_documentdb.tf
@@ -7,16 +7,18 @@ terraform {
 }
 
 module "ppu_dev_documentdb_cluster" {
-  source            = "git::https://github.com/cisco-eti/sre-tf-module-aws-documentdb?ref=1.0.0"
-  account_name      = "outshift-common-dev" # id 471112537430
-  application_name  = "genie-dev-use2-documentdb-1"
-  environment       = "dev"
-  engine_version    = "5.0.0"
-  master_username   = "genie"
-  instances         = 3
-  instance_class    = "db.r6g.xlarge"
-  vpc_data_name     = "common-dev-use2-vpc-data"
-  vpc_eks_name      = "comn-dev-use2-1"
-  venture_namespace = "genie"
-  secret_path       = "secret/dev"
+  source                = "git::https://github.com/cisco-eti/sre-tf-module-aws-documentdb?ref=1.0.1"
+  account_name          = "outshift-common-dev" # id 471112537430
+  application_name_tag  = "outshift_common_services"
+  component_tag         = "genie"
+  resource_owner_tag    = "genie"
+  environment           = "dev"
+  engine_version        = "5.0.0"
+  master_username       = "genie"
+  instances             = 3
+  instance_class        = "db.r6g.xlarge"
+  vpc_data_name         = "common-dev-use2-vpc-data"
+  vpc_eks_name          = "comn-dev-use2-1"
+  venture_namespace     = "genie"
+  secret_path           = "secret/dev"
 }
