8c3b86e5ee2ba59babae86061df08690ee968b13
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 14:08:57 -0700

    chore: update jarvis finops tags

--- aws_outshift-common-dev_us-east-2_eks_jarvis-sandbox_data.tf
+++ aws_outshift-common-dev_us-east-2_eks_jarvis-sandbox_data.tf
@@ -2,12 +2,12 @@ data "aws_caller_identity" "current" {}
 
 data "aws_eks_cluster" "eks" {
   name       = local.name
-  provider   = aws.eks
+  #provider   = aws.eks
   depends_on = [module.eks_all_in_one]
 }
 
 data "aws_eks_cluster_auth" "eks" {
   name       = local.name
-  provider   = aws.eks
+  #provider   = aws.eks
   depends_on = [module.eks_all_in_one]
 }
