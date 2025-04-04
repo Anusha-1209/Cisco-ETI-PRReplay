5acd7899b68357193e2ec0517803c9286ab469cc
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 10:55:38 -0700

    chore: update provider config

---
+++ aws_outshift-common-dev_global_rds_global-rds-iam-dev-1_providers.tf
@@ -0,0 +1,63 @@
+# provider "vault" {
+#   alias     = "eticloud"
+#   address   = "https://keeper.cisco.com"
+#   namespace = "eticloud"
+# }
+
+# provider "vault" {
+#   alias     = "teamsecrets"
+#   address   = "https://keeper.cisco.com"
+#   namespace = "eticloud/teamsecrets"
+# }
+
+# provider "aws" {
+#   alias       = "primary"
+#   access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+#   secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+#   region      = "us-east-2"
+#   max_retries = 3
+#   default_tags {
+#     tags = {
+#       ApplicationName    = "global-rds-iam-dev-1_primary"
+#       CiscoMailAlias     = "eti-sre-admins@cisco.com"
+#       DataClassification = "Cisco Confidential"
+#       DataTaxonomy       = "Cisco Operations Data"
+#       EnvironmentName    = "NonProd"
+#       ResourceOwner      = "ETI SRE"
+#     }
+#   }
+# }
+
+# provider "aws" {
+#   alias       = "secondary"
+#   access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+#   secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+#   region      = "us-west-2"
+#   max_retries = 3
+#   default_tags {
+#     tags = {
+#       ApplicationName    = "global-rds-iam-dev-1_secondary"
+#       CiscoMailAlias     = "eti-sre-admins@cisco.com"
+#       DataClassification = "Cisco Confidential"
+#       DataTaxonomy       = "Cisco Operations Data"
+#       EnvironmentName    = "NonProd"
+#       ResourceOwner      = "ETI SRE"
+#     }
+#   }
+# }
+
+# terraform {
+#   required_version = ">= 1.5.5"
+
+#   required_providers {
+#     aws = {
+#       source                = "hashicorp/aws"
+#       version               = "~> 5.0"
+#       configuration_aliases = [aws.primary, aws.secondary]
+#     }
+#     vault = {
+#       source  = "hashicorp/vault"
+#       version = "~> 3.0"
+#     }
+#   }
+# }
