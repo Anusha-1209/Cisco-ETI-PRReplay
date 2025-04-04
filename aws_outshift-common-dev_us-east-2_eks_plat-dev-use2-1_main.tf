cae0d61ab8320b2b67adc3cd1c5300c116149778
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 09:47:26 -0700

    fix: recreate plat-prod-use2-1

--- aws_outshift-common-dev_us-east-2_eks_plat-dev-use2-1_main.tf
+++ aws_outshift-common-dev_us-east-2_eks_plat-dev-use2-1_main.tf
@@ -22,12 +22,12 @@ module "eks_all_in_one" {
 
   additional_aws_auth_configmap_roles = [
     {
-      rolearn  = "arn:aws:iam::626007623524:user/terraform_admin",
+      rolearn  = "arn:aws:iam::${local.account_id}:user/terraform_admin",
       username = "terraform_admin",
       groups   = ["system:masters"]
     }
     , {
-      rolearn  = "arn:aws:iam::626007623524:role/devops",
+      rolearn  = "arn:aws:iam::${local.account_id}:role/devops",
       username = "devops",
       groups   = ["system:masters"]
     }
