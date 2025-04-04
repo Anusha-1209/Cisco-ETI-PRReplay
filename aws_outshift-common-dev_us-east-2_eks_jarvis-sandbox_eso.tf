2dd98644c6ee244095b75768d543582ce8af4688
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 14:24:55 -0700

    chore: fix account id

--- aws_outshift-common-dev_us-east-2_eks_jarvis-sandbox_eso.tf
+++ aws_outshift-common-dev_us-east-2_eks_jarvis-sandbox_eso.tf
@@ -13,3 +13,12 @@ module "eso_eticloud" {
   kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
   policies        = ["external-secrets-${local.name}"]
 }
+
+module "eso_eticloud_apps_sre" {
+  source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
+  cluster_name    = local.name
+  vault_namespace = "eticloud/apps/sre"
+  kubernetes_host = data.aws_eks_cluster.eks.endpoint
+  kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
+  policies        = ["external-secrets-${local.name}"]
+}
