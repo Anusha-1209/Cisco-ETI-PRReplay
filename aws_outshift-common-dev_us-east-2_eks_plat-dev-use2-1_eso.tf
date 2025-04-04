386df35811c2f1f3ee4e2e89d17571d7a53ae3f6
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 11:48:05 -0700

    fix: data values usage

--- aws_outshift-common-dev_us-east-2_eks_plat-dev-use2-1_eso.tf
+++ aws_outshift-common-dev_us-east-2_eks_plat-dev-use2-1_eso.tf
@@ -17,7 +17,7 @@ module "eso_eticloud_apps_sre" {
   source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
   cluster_name    = local.name
   vault_namespace = "eticloud/apps/sre"
-  kubernetes_host = data.aws_eks_cluster.cluster.endpoint
+  kubernetes_host = data.aws_eks_cluster.eks.endpoint
   kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
   policies        = ["external-secrets-${local.name}"]
 }
