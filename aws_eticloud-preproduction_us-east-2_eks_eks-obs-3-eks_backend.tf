8ddc4949a6468048e64824cb8de4326b5650a80b
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 12:04:38 -0700

    chore: delete files

--- aws_eticloud-preproduction_us-east-2_eks_eks-obs-3-eks_backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "eticloud-tf-state-nonprod"
-    key    = "terraform-state/aws-eticloud-preprod/eks/us-east-2/eks-obs-3-eks.tfstate"
-    region = "us-east-2"
-  }
-}
