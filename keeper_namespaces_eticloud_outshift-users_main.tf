c7b3e7ca079c76a15ffb394e71ebfc3a10a7c5a8
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-04 14:52:20 -0700

    chore: remove bugbash - not used resources

--- keeper_namespaces_eticloud_outshift-users_main.tf
+++ keeper_namespaces_eticloud_outshift-users_main.tf
@@ -33,13 +33,6 @@ resource "vault_namespace" "namespace" {
 }
 
 # key-value secrets engine
-resource "vault_mount" "bugbash" {
-  provider = vault.venture
-  path     = "bugbash"
-  type     = "kv"
-  options  = { version = "2" }
-}
-
 resource "vault_mount" "csm" {
   provider = vault.venture
   path     = "csm"
@@ -83,47 +76,6 @@ resource "vault_jwt_auth_backend" "oidc" {
   oidc_client_secret = data.vault_generic_secret.oidc_credential.data["client_secret"]
   oidc_discovery_url = "https://sso-dbbfec7f.sso.duosecurity.com/oidc/${var.oidc_client_id}"
   default_role       = "generic-user"
-}
-
-# Outshift Bugbash Vault Role
-resource "vault_jwt_auth_backend_role" "bugbash" {
-  depends_on = [vault_policy.bugbash]
-  provider   = vault.venture
-  role_name  = "bugbash"
-  role_type  = "oidc"
-  backend    = vault_jwt_auth_backend.oidc.path
-  allowed_redirect_uris = ["https://keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
-    "https://east.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback",
-  "https://west.keeper.cisco.com/ui/vault/auth/oidc/oidc/callback"]
-  bound_audiences = [var.oidc_client_id]
-  bound_claims = {
-    memberof = "CN=outshift-vault-bugbash,OU=Cisco Groups,DC=cisco,DC=com"
-  }
-  disable_bound_claims_parsing = true
-  bound_claims_type            = "string"
-  claim_mappings = {
-    email       = "email",
-    family_name = "family_name",
-    given_name  = "given_name",
-    sub         = "sub"
-  }
-  groups_claim = "memberof"
-  oidc_scopes  = ["profile", "email", "openid"]
-  user_claim   = "sub"
-  token_policies = [vault_policy.bugbash.name]
-}
-
-resource "vault_policy" "bugbash" {
-  provider = vault.venture
-  name     = "bugbash"
-  policy   = <<EOT
-# Manage auth methods broadly across Vault
-# List, create, update, and delete key/value secrets
-path "bugbash/*"
-{
-  capabilities = ["read", "list"]
-}
-EOT
 }
 
 # Outshift CSM Vault User Role
