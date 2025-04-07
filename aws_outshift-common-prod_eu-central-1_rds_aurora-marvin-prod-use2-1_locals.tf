4d981321038c466ef5b652cfc8544fd17a0507f8
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 12:10:42 -0700

    Creating Base Branch for PR 766

---
+++ aws_outshift-common-prod_eu-central-1_rds_aurora-marvin-prod-use2-1_locals.tf
@@ -0,0 +1,7 @@
+
+locals {
+  vpc_name         = "marvin-prod-euc1-data"
+  cluster_vpc_name = "marvin-prod-euc1-1"
+  aws_account_name = "outshift-common-prod"
+  rds_name         = "marvin-prod-euc1-1"
+}
