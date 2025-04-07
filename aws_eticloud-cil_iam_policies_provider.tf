adc513f64a7349cda497c85f2e861bc2d5908d10
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-06 23:48:24 -0700

    Creating Base Branch for PR 691

---
+++ aws_eticloud-cil_iam_policies_provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      # version = "~> 3.0"
+    }
+  }
+}
+
+data "vault_generic_secret" "aws_infra_credential" {
+  path = "secret/eticcprod/infra/eticloud-cil/aws"  
+}
+variable "AWS_INFRA_REGION" {
+  description = "AWS Region"
+  default     = "us-east-2"
+}
+# DNS AWS Provider
+provider "aws" {
+  access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region      = var.AWS_INFRA_REGION
+  max_retries = 3
+}
