07a0d83d835da07d67408ddc2d7ed61651e07fe9
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 08:55:39 -0700

    chore: remove some gbear s3 resources

--- aws_eticloud_account_iam_great-bear_policies_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "eticloud-tf-state"
-    key            = "backend/great-bear-eticloud-iam-policies.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "eticloud-tf-locks"
-    encrypt        = true
-  }
-}
--- aws_eticloud_account_iam_great-bear_users_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "eticloud-tf-state-prod"
-    key            = "terraform-state/iam/global/gbear-route53-iam-user.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "eticloud-tf-locks"
-    encrypt        = true
-  }
-}
