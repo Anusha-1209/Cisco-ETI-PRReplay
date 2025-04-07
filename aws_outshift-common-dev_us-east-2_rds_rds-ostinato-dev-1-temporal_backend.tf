5b926a34f3015ff43867bb8ffcfcf7549a4d82a6
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 10:46:28 -0700

    chore: adding ostinato RDS + roles

---
+++ aws_outshift-common-dev_us-east-2_rds_rds-ostinato-dev-1-temporal_backend.tf
@@ -0,0 +1,7 @@
+terraform {
+  backend "s3" {
+    bucket = "eticloud-tf-state-nonprod" 
+    key    = "terraform-state/aurora-postgres/us-east-2/ostinato-dev-1-temporal.tfstate"
+    region = "us-east-2"
+  }
+}
