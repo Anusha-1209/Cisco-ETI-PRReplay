e7318333b460bd1dcebc410f5b60160f362cc0c1
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 10:31:51 -0700

    chore: remove files

--- aws_outshift-common-staging_us-east-2_msk_marvin-use2-1_kms.tf
+++
@@ -1,3 +0,0 @@
-resource "aws_kms_key" "encryption_key" {
-  description = "marvin-staging-use2-1-msk MSK cluster encryption key"
-}
--- aws_outshift-common-staging_us-east-2_rds_aurora-marvin-staging-use2-1_locals.tf
+++
@@ -1,7 +0,0 @@
-
-locals {
-  vpc_name         = "marvin-staging-use2-data"
-  cluster_vpc_name = "marvin-stage-use2-1"
-  aws_account_name = "outshift-common-staging"
-  rds_name         = "marvin-staging-use2-1"
-}
