2ec1a29de2c667cca918d29573a14562aef8a034
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 16:26:49 -0700

    chore: bump comn staging to k8s 1.30

--- aws_outshift-common-staging_us-east-2_eks_comn-staging-use2-1_main.tf
+++ aws_outshift-common-staging_us-east-2_eks_comn-staging-use2-1_main.tf
@@ -25,7 +25,7 @@ module "eks_all_in_one" {
   cidr               = "10.0.0.0/16"          # VPC CIDR
   public_subnet_mask = 196
   intra_subnet_mask  = 200
-  cluster_version    = "1.29"                 # EKS cluster version
+  cluster_version    = "1.30"                 # EKS cluster version
 
   # EKS Managed Private Node Group
   instance_types = ["m6a.2xlarge"] # EKS instance types
