7998d27826accf88d5a109d2080657d334babab2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 15:14:30 -0700

    Creating Base Branch for PR 814

---
+++ aws_eti-ci_us-east-1_ec2_eti-cloud-agent-2_route53.tf
@@ -0,0 +1,19 @@
+resource "aws_eip" "eip" {
+  instance = module.ec2-instance.id
+  vpc = true
+  tags     = {
+    "Name" = var.ec2_name
+  }
+
+  tags_all             = {
+    "Name" = var.ec2_name
+  }
+}
+
+resource "aws_route53_record" "ec2_host_record_name" {
+  name    = var.ec2_name
+  zone_id = data.aws_route53_zone.this.id
+  type    = "A"
+  ttl     = 60
+  records = [aws_eip.eip.public_ip]
+}
