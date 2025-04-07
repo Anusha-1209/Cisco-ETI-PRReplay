f30a5c09074eaae922d504869647ac971d0a1efa
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 10:51:24 -0700

    only one db

--- aws_outshift-common-dev_us-east-2_rds_rds-ostinato-dev-1-knowledgebase_backend.tf
+++
@@ -1,7 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "eticloud-tf-state-nonprod" 
-    key    = "terraform-state/aurora-postgres/us-east-2/ostinato-dev-1-knowledgebase.tfstate"
-    region = "us-east-2"
-  }
-}
--- aws_outshift-common-dev_us-east-2_rds_rds-ostinato-dev-1-knowledgebase_locals.tf
+++
@@ -1,7 +0,0 @@
-
-locals {
-  vpc_name = "common-dev-use2-vpc-data"
-  cluster_vpc_name = "comn-dev-use2-1"
-  aws_account_name = "outshift-common-dev"
-  rds_name  = "ostinato-dev-1-knowledgebase"
-}
