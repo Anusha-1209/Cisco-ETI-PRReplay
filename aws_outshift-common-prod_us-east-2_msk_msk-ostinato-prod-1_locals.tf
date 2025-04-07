9e86fa86a700cb7289fccf0dafbeb72d770a30d2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 14:02:27 -0700

    chore: adding ostinato msk for prod

---
+++ aws_outshift-common-prod_us-east-2_msk_msk-ostinato-prod-1_locals.tf
@@ -0,0 +1,7 @@
+locals {
+  vpc_name = "common-prod-use2-vpc-data"
+  cluster_vpc_name = "comn-prod-use2-1"
+  cluster_name = "comn-prod-use2-1"
+  msk_name  = "msk-ostinato-prod-1"
+  aws_account_name = "outshift-common-prod"
+}
---
+++ aws_outshift-common-prod_us-east-2_msk_msk-ostinato-prod-1_provider.tf
@@ -0,0 +1,29 @@
+provider "aws" {
+  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
+  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
+  region     = "us-east-2"
+
+  default_tags {
+    tags = {
+      ApplicationName    = "outshift_foundational_services"
+      Component          = "ostinato"
+      CiscoMailAlias     = "ostinato-team-mailer@cisco.com"
+      DataClassification = "Cisco Confidential"
+      DataTaxonomy       = "Cisco Operations Data"
+      EnvironmentName    = "NonProd"
+      ResourceOwner      = "ostinato-dev-team"
+    }
+  }
+}
+
+provider "vault" {
+  alias     = "ostinato"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud/apps/ostinato"
+}
+
+provider "vault" {
+  alias     = "eticloud"
+  address   = "https://keeper.cisco.com"
+  namespace = "eticloud"
+}
