44c9bf8e99bc45d784a308e485948a6868ea1988
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 00:10:40 -0700

    fix: terraform fmt

--- aws_outshift-common-dev_iam_roles_phoenix_phoenix-gha-cf-role.tf
+++ aws_outshift-common-dev_iam_roles_phoenix_phoenix-gha-cf-role.tf
@@ -15,10 +15,10 @@ provider "aws" {
   region     = "us-east-2"
   default_tags {
     tags = {
-      applicationname: outshift_ventures
-      component: phoenix
-      resourceowner: outshift-phoenix-admins
-      ciscomailalias: outshift-phoenix@cisco.com
+      applicationname : "outshift_ventures"
+      component : "phoenix"
+      resourceowner : "outshift-phoenix-admins"
+      ciscomailalias : "outshift-phoenix@cisco.com"
     }
   }
 }
@@ -27,42 +27,42 @@ resource "aws_iam_policy" "gha_phoenix_cf_role_policy" {
   name        = "gha-phoenix-cf-policy"
   description = "IAM Policy to grant GHA permissions to create EKS clusters "
   policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "EKS",
-            "Effect": "Allow",
-            "Action": [
-                "eks:*"
-            ],
-            "Resource": "*"
-        } 
-        {
-			"Effect": "Allow",
-			"Action": [
-				"ec2:*"
-			],
-			"Resource": "*"
-		},
-		{
-			"Effect": "Allow",
-			"Action": "iam:*",
-			"Resource": "*"
-		},
-		{
-			"Effect": "Allow",
-			"Action": [
-				"logs:*"
-			],
-			"Resource": "*"
-		},
-		{
-			"Effect": "Allow",
-			"Action": [
-				"kms:*"
-			],
-			"Resource": "*"
-		}
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Sid" : "EKS",
+        "Effect" : "Allow",
+        "Action" : [
+          "eks:*"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "ec2:*"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : "iam:*",
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "logs:*"
+        ],
+        "Resource" : "*"
+      },
+      {
+        "Effect" : "Allow",
+        "Action" : [
+          "kms:*"
+        ],
+        "Resource" : "*"
+      }
     ]
   })
 }
@@ -70,23 +70,23 @@ resource "aws_iam_policy" "gha_phoenix_cf_role_policy" {
 resource "aws_iam_role" "gha_phoenix_cf_role_role" {
   name = "gha-phoenix-cf-role"
   assume_role_policy = jsonencode({
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Effect": "Allow",
-            "Principal": {
-                "Federated": "arn:aws:iam::471112537430:oidc-provider/token.actions.githubusercontent.com"
-            },
-            "Action": "sts:AssumeRoleWithWebIdentity",
-            "Condition": {
-                "StringEquals": {
-                    "token.actions.githubusercontent.com:aud": "sts.amazonaws.com"
-                },
-                "StringLike": {
-                    "token.actions.githubusercontent.com:sub": "repo:cisco-eti/phoenix-csit:*"
-                }
-            }
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::471112537430:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : "sts.amazonaws.com"
+          },
+          "StringLike" : {
+            "token.actions.githubusercontent.com:sub" : "repo:cisco-eti/phoenix-csit:*"
+          }
         }
+      }
     ]
   })
 
