bed056525f3c9f8199211f593ae9fb23a6d9f5a9
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 12:46:55 -0700

    chore(eks): remove cluster references

--- aws_outshift-common-prod_us-west-2_eks_plat-prod-usw2-1_locals.tf
+++
@@ -1,8 +0,0 @@
-# This file was created by Outshift Platform Self-Service automation.
-locals {
-  name            = "plat-prod-usw2-1"
-  region          = "us-west-2"
-  eks_aws_account = "outshift-common-prod"
-  vpc_cidr        = "10.200.0.0/16"
-  account_id      = "058264538874"
-}
