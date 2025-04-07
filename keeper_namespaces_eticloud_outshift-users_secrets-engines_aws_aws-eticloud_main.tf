f49e242b27a65d1bf664c4abca1b8aab415edaf5
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-06 23:03:30 -0700

    Creating Base Branch for PR 677

---
+++ keeper_namespaces_eticloud_outshift-users_secrets-engines_aws_aws-eticloud_main.tf
@@ -0,0 +1,19 @@
+resource "vault_aws_secret_backend" "dev-sandbox-aws-eticloud" {
+  provider    = vault.outshift-users
+  path        = "dev-sandbox-aws-eticloud"
+  description = "AWS Secrets Engine for Dev Sandbox access"
+
+  access_key  = aws_iam_access_key.vault-secret-engine-dev-sandbox.id
+  secret_key  = aws_iam_access_key.vault-secret-engine-dev-sandbox.secret
+}
+
+resource "vault_aws_secret_backend_role" "dev-sandbox-vault-role" {
+  provider        = vault.outshift-users
+  backend         = vault_aws_secret_backend.dev-sandbox-aws-eticloud.path
+  name            = "dev-sandbox-vault-role"
+  credential_type = "assumed_role"
+
+  role_arns = [
+    aws_iam_role.dev-sandbox-ecr-access.arn,
+  ]
+}
