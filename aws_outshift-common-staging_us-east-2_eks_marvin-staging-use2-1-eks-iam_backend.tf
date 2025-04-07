001a4466ea415aaa7de81deb64e234ac17074720
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 01:26:21 -0700

    Delete aws/outshift-common-staging/us-east-2/eks/marvin-staging-use2-1-eks-iam directory

--- aws_outshift-common-staging_us-east-2_eks_marvin-staging-use2-1-eks-iam_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "eticloud-tf-state-nonprod"
-    key            = "terraform-state/outshift-common-staging/iam/role/marvin-stage-use2-1-custom-roles.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "eticloud-tf-locks"
-    encrypt        = true
-  }
-}
