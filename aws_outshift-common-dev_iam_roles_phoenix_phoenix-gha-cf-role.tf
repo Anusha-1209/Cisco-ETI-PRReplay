f99fa2066fe3607fea56c324da7599a62fbb61ae
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 00:11:37 -0700

    fix: add provider and versions configs in separate files

--- aws_outshift-common-dev_iam_roles_phoenix_phoenix-gha-cf-role.tf
+++ aws_outshift-common-dev_iam_roles_phoenix_phoenix-gha-cf-role.tf
@@ -1,27 +1,3 @@
-provider "vault" {
-  alias     = "eticloud"
-  address   = "https://keeper.cisco.com"
-  namespace = "eticloud"
-}
-
-data "vault_generic_secret" "aws_infra_credential" {
-  provider = vault.eticloud
-  path     = "secret/infra/aws/outshift-common-dev/terraform_admin"
-}
-
-provider "aws" {
-  access_key = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region     = "us-east-2"
-  default_tags {
-    tags = {
-      applicationname : "outshift_ventures"
-      component : "phoenix"
-      resourceowner : "outshift-phoenix-admins"
-      ciscomailalias : "outshift-phoenix@cisco.com"
-    }
-  }
-}
 
 resource "aws_iam_policy" "gha_phoenix_cf_role_policy" {
   name        = "gha-phoenix-cf-policy"
