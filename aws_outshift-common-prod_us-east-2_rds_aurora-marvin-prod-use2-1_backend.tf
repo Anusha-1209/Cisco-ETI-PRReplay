50ed94d1724980f55496a9357597e43b05df36ea
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 10:43:24 -0700

    chore: destroy glue-marvin-events-prod-use2-1

--- aws_outshift-common-prod_us-east-2_rds_aurora-marvin-prod-use2-1_backend.tf
+++
@@ -1,8 +0,0 @@
-
-terraform {
-  backend "s3" {
-    bucket = "eticloud-tf-state-prod"
-    key    = "terraform-state/aurora-postgres/us-east-2/marvin-prod-use2-1.tfstate"
-    region = "us-east-2"
-  }
-}
--- aws_outshift-common-prod_us-east-2_rds_aurora-marvin-prod-use2-1_locals.tf
+++
@@ -1,7 +0,0 @@
-
-locals {
-  vpc_name         = "marvin-prod-use2-data"
-  cluster_vpc_name = "marvin-prod-use2-1"
-  aws_account_name = "outshift-common-prod"
-  rds_name         = "marvin-prod-use2-1"
-}
