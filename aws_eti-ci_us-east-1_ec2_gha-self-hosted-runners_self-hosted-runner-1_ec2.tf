9b0dfcd00888e94c4b53d7793c4f4a35e5156ded
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 13:05:14 -0700

    fix(iam): clean up unused resources, unneeded

--- aws_eti-ci_us-east-1_ec2_gha-self-hosted-runners_self-hosted-runner-1_ec2.tf
+++ aws_eti-ci_us-east-1_ec2_gha-self-hosted-runners_self-hosted-runner-1_ec2.tf
@@ -34,37 +34,3 @@ module "ec2-instance" {
     hostname_type                        = "ip-name"
   }
 }
-
-resource "aws_iam_role" "ec2_ssm" {
-  name = "ec2_ssm_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Action = "sts:AssumeRole",
-      Effect = "Allow",
-      Principal = {
-        Service = "ec2.amazonaws.com"
-      },
-    }],
-  })
-}
-
-resource "aws_iam_role_policy" "ec2_ssm_policy" {
-  name = "ec2_ssm_policy"
-  role = aws_iam_role.ec2_ssm.name
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [{
-      Effect   = "Allow",
-      Action   = ["ssm:GetParameter"],
-      Resource = "arn:aws:ssm:us-east-1:${data.aws_caller_identity.current.account_id}:parameter/outshift-platform-gha-token"
-    }]
-  })
-}
-
-resource "aws_iam_instance_profile" "ec2_ssm" {
-  name = "ec2_ssm_instance_profile"
-  role = aws_iam_role.ec2_ssm.name
-}
