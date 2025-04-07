0390f20411ebe4f701670a5373fe8cf5d6270f53
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 01:58:32 -0700

    fix: backend

--- aws_outshift-common-dev_us-east-2_eks_comn-dev-use2-1_eso-a3po_eso.tf
+++ aws_outshift-common-dev_us-east-2_eks_comn-dev-use2-1_eso-a3po_eso.tf
@@ -3,7 +3,7 @@ terraform {
     # This is the name of the backend S3 bucket.
     bucket = "eticloud-tf-state-nonprod"
     # This is the path to the Terraform state file in the backend S3 bucket.
-    key = "terraform-state/aws/eticloud/us-east-2/eks/comn-dev-use2-1.tfstate"
+    key = "terraform-state/aws/eticloud/us-east-2/eks/eso-a3po-comn-dev-use2-1.tfstate"
     # This is the region where the backend S3 bucket is located.
     region = "us-east-2" # DO NOT CHANGE.
   }
