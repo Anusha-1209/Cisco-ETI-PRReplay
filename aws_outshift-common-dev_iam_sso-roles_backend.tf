4e71ad12cd195a4ef8e0f1328df4c4ca559cbf2a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 01:00:23 -0700

    fix(outshift-common-dev): updates

--- aws_outshift-common-dev_iam_sso-roles_backend.tf
+++ aws_outshift-common-dev_iam_sso-roles_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "eticloud-tf-state-dev"
+    bucket         = "eticloud-tf-state-nonprod"
     key            = "terraform-state/outshift-common-dev/iam/role/sso-roles.tfstate"
     region         = "us-east-2"
     dynamodb_table = "eticloud-tf-locks"
