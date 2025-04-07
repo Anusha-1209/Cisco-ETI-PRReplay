256ad38aba3bd0e96c650fe9b1f86887d391cf92
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 15:42:27 -0700

    test: edit file

--- aws_sscs-dev_us-east-2_fargate-test_eks.tf
+++ aws_sscs-dev_us-east-2_fargate-test_eks.tf
@@ -21,17 +21,11 @@ terraform {
   }
 }
 
-variable "AWS_INFRA_REGION" {
-  description = "AWS Region"
-  default     = "us-east-2" #Set the region for the resources to be created.
-}
-
-
 # Infra AWS Provider
 provider "aws" {
   access_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_ACCESS_KEY_ID"]
   secret_key  = data.vault_generic_secret.aws_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region      = var.AWS_INFRA_REGION
+  region      = "us-east-2"
   max_retries = 3
   default_tags {
     tags = {
