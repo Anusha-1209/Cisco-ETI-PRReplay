3f2ff27bb515eb69cf38bc639b2fc19d39e36d5c
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 12:48:44 -0700

    fix(files): remove files from another branch

--- aws_outshift-common-prod_us-west-2_eks_comn-prod-usw2-1_provider.tf
+++
@@ -1,7 +0,0 @@
-provider "aws" {
-  alias       = "argocd"
-  access_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_ACCESS_KEY_ID"]
-  secret_key  = data.vault_generic_secret.aws_argocd_infra_credential.data["AWS_SECRET_ACCESS_KEY"]
-  region      = "us-east-2"
-  max_retries = 3
-}
