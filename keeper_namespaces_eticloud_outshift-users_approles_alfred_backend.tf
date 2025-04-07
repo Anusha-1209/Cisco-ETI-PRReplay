6c038e6bfa8833cebd260a715cf2c834c80d6a8e
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 09:05:43 -0700

    chore: deploy alfred approle to access secrets on outshift-users

--- keeper_namespaces_eticloud_outshift-users_approles_alfred_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "eticloud-tf-state-prod"
-    key            = "backend/keeper/eticloud/outshift-users/prcoach-auth-backend.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "eticloud-tf-locks"
-    encrypt        = true
-  }
-}
--- keeper_namespaces_eticloud_outshift-users_approles_alfred_provider.tf
+++
@@ -1,4 +0,0 @@
-provider "vault" {
-  alias     = "prcoach"
-  namespace = "eticloud/outshift-users"
-}
