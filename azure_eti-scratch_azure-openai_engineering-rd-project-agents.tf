1f436f11b0171bf4b821db3088901b362a91e5bf
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-06 20:59:13 -0700

    Creating Base Branch for PR 657

---
+++ azure_eti-scratch_azure-openai_engineering-rd-project-agents.tf
@@ -0,0 +1,42 @@
+locals {
+  engineering-rd-project-agents   = "engineering-rd-project-agents"
+  engineering-rd-project-agents-tags = {
+    ApplicationName    = "engineering_rd"
+    Component          = "test-agent"
+    ResourceOwner      = "tiswanso"
+    CiscoMailAlias     = "tiswanso@cisco.com"
+    DataClassification = "Cisco Restricted"
+    DataTaxonomy       = "Cisco Operations Data"
+    Environment        = "NonProd"
+  }
+}
+
+resource "azurerm_resource_group" "engineering-rd-project-agents" {
+  name     = local.engineering-rd-project-agents
+  location = local.region_eastus
+}
+
+resource "azurerm_cognitive_account" "engineering-rd-project-agents" {
+  name                  = "${local.engineering-rd-project-agents}"
+  custom_subdomain_name = "${local.engineering-rd-project-agents}"
+  location              = azurerm_resource_group.engineering-rd-project-agents.location
+  resource_group_name   = azurerm_resource_group.engineering-rd-project-agents.name
+  kind                  = "OpenAI"
+  sku_name              = "S0"
+  tags                  = local.engineering-rd-project-agents-tags
+}
+
+resource "azurerm_cognitive_deployment" "engineering-rd-project-agents-gpt4o" {
+  name                 = "gpt-4o"
+  cognitive_account_id = azurerm_cognitive_account.engineering-rd-project-agents.id
+  model {
+    format  = "OpenAI"
+    name    = "gpt-4o"
+    version = "2024-05-13"
+  }
+
+  sku {
+    name = "GlobalStandard"
+    capacity = 1000
+  }
+}
