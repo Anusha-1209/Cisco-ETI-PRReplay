93fb0aa4a80538af510b8dd36c79e2b3146c5be4
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 00:16:55 -0700

    Creating Base Branch for PR 678

---
+++ keeper_namespaces_eticloud_outshift-users_secrets-engines_aws_aws-eticloud_iam.tf
@@ -0,0 +1,91 @@
+# Create the IAM user secret engine will use to auth against AWS
+resource "aws_iam_user" "vault-secret-engine-dev-sandbox" {
+  name = "vault-secret-engine-dev-sandbox"
+}
+
+resource "aws_iam_user_policy" "vault-secret-engine-dev-sandbox-policy" {
+  name = "vault-secret-engine-dev-sandbox-policy"
+  user = aws_iam_user.vault-secret-engine-dev-sandbox.name
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+          "iam:CreateUser",
+          "iam:PutUserPolicy",
+          "iam:CreateAccessKey",
+          "iam:DeleteAccessKey",
+          "iam:DeleteUser"
+
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_access_key" "vault-secret-engine-dev-sandbox" {
+  user = aws_iam_user.vault-secret-engine-dev-sandbox.name
+}
+
+resource "aws_iam_policy" "dev-sandbox-ecr-access-policy" {
+  name        = "dev-sandbox-ecr-access-policy"
+  description = "Write access to ECR for Dev Sandbox storage"
+
+  policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+          "ecr:BatchGetRepositoryScanningConfiguration",
+          "ecr:TagResource",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:GetLifecyclePolicy",
+          "ecr:DescribeImageScanFindings",
+          "ecr:CreateRepository",
+          "ecr:PutImageScanningConfiguration",
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:DescribePullThroughCacheRules",
+          "ecr:GetAuthorizationToken",
+          "ecr:PutImage",
+          "ecr:UntagResource",
+          "ecr:BatchGetImage",
+          "ecr:DescribeImages",
+          "ecr:InitiateLayerUpload",
+          "ecr:PutImageTagMutability",
+          "ecr:DescribeImageReplicationStatus",
+          "ecr:ListTagsForResource",
+          "ecr:UploadLayerPart",
+          "ecr:CreatePullThroughCacheRule",
+          "ecr:ListImages",
+          "ecr:PutRegistryPolicy",
+          "ecr:GetRegistryScanningConfiguration",
+          "ecr:CompleteLayerUpload",
+          "ecr:DescribeRepositories",
+          "sts:GetServiceBearerToken",
+          "ecr:ReplicateImage",
+          "ecr:DescribeRegistry",
+          "ecr:BatchImportUpstreamImage",
+          "ecr:SetRepositoryPolicy",
+          "ecr:GetRepositoryPolicy",
+          "ecr:PutReplicationConfiguration"
+      ],
+      "Effect": "Allow",
+      "Resource": "arn:aws:ecr:us-east-2:626007623524:repository/sandbox/*"
+    },
+    {
+      "Action": [
+        "sts:GetCallerIdentity"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
