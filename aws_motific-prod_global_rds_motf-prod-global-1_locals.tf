efee94fa815d4dee1ffdff5ec011612c2e8b0d76
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-06 23:35:02 -0700

    Delete aws/motific-prod directory

--- aws_motific-prod_global_rds_motf-prod-global-1_locals.tf
+++
@@ -1,7 +0,0 @@
-locals {
-  vpc_name                = "motf-prod-use2-data"
-  cluster_vpc_name        = "motf-prod-use2-1"
-  data_secondary_vpc_name = "motf-prod-usw2-data"
-  aws_account_name        = "motific-prod"
-  rds_name                = "motf-prod-use2-1"
-}
--- aws_motific-prod_us-east-2_sagemaker_motf-prod-sagemaker_locals.tf
+++
@@ -1,5 +0,0 @@
-locals {
-  account_id = data.aws_caller_identity.current.account_id
-  name     = "motf-prod-sagemaker"
-  vpc_name = "motf-prod-use2-1"
-}
