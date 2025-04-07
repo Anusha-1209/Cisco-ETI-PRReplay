036fbd4e3ada9d2fc0ce791a6ad573ab53ae34c6
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 11:18:45 -0700

    chore: remove s3 files

--- aws_outshift-common-prod_us-east-2_eks_pi-prod-use2-1_pi-prod-use2-1-eks-iam_backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "eticloud-tf-state-prod"
-    key            = "terraform-state/outshift-common-prod/iam/role/pi-prod-use2-1-s3-admin-role.tfstate"
-    region         = "us-east-2"
-    dynamodb_table = "eticloud-tf-locks"
-    encrypt        = true
-  }
-}
