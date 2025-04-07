5b1e9946bc54bd1ea13030e13fb667108418b794
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-06 23:43:32 -0700

    Delete aws/eticloud-ondemand-labs directory

--- aws_eticloud-ondemand-labs_us-east-2_eks_motific-sandbox_locals.tf
+++
@@ -1,9 +0,0 @@
-# This file was created by Outshift Platform Self-Service automation.
-locals {
-  name               = "motific-sandbox"
-  region             = "us-east-2"
-  eks_aws_account    = "eticloud-ondemand-labs"
-  vpc_cidr           = "10.0.0.0/16"
-  argocd_k8s_name    = "eks-gitops-1"
-  argocd_aws_account = "eti-ci"
-}
