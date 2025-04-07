af6d6e85e077698fd403dd88bd27100581a302c2
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 01:59:15 -0700

    removing duplicated ressource

--- aws_outshift-common-dev_us-east-2_eks_comn-dev-use2-1_eso.tf
+++ aws_outshift-common-dev_us-east-2_eks_comn-dev-use2-1_eso.tf
@@ -151,12 +151,3 @@ module "eso_eticloud_apps_aether" {
   kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
   policies        = ["external-secrets-${local.name}"]
 }
-
-module "eso_eticloud_apps_a3po" {
-  source          = "git::https://github.com/cisco-eti/sre-tf-module-eso-access.git?ref=1.0.0"
-  cluster_name    = local.name
-  vault_namespace = "eticloud/apps/a3po"
-  kubernetes_host = data.aws_eks_cluster.cluster.endpoint
-  kubernetes_ca   = base64decode(data.vault_generic_secret.cluster_certificate.data["b64certificate"])
-  policies        = ["external-secrets-${local.name}"]
-}
