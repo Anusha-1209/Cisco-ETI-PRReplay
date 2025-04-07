f439e7d19c4abbde9800f9d6e15dcec1a2cd219a
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 16:52:53 -0700

    Rename s3 bucket to indicate prod env

--- aws_outshift-common-prod_us-east-2_rds_rds-ostinato-rag-prod-1_backend.tf
+++ aws_outshift-common-prod_us-east-2_rds_rds-ostinato-rag-prod-1_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "eticloud-tf-state-nonprod"
+    bucket = "eticloud-tf-state-prod"
     key    = "terraform-state/aurora-postgres/us-east-2/ostinato-rag-dev-1.tfstate"
     region = "us-east-2"
   }
