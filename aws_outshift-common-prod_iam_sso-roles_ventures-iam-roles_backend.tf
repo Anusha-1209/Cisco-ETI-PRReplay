30cef85c3e94da6457f7eb092ae0efb067d82de3
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-07 17:03:02 -0700

    Update aws/outshift-common-prod/iam/sso-roles/ventures-iam-roles/backend.tf
    Co-authored-by: David B. <5034531+dabcoder@users.noreply.github.com>

--- aws_outshift-common-prod_iam_sso-roles_ventures-iam-roles_backend.tf
+++ aws_outshift-common-prod_iam_sso-roles_ventures-iam-roles_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "eticloud-tf-state-nonprod"
+    bucket         = "eticloud-tf-state-prod"
     key            = "terraform-state/outshift-common-prod/iam/role/teams-sso-roles.tfstate"
     region         = "us-east-2"
     dynamodb_table = "eticloud-tf-locks"
