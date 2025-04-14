6deef7758bc12a45edf648cb091087aa233563ac
anushasuresh <148524049+Anusha-1209@users.noreply.github.com>
2025-04-14 15:22:46 -0700

    fix: vpc vars

--- aws_rosey-test_eu-west-1_vpc_rosey-dev-euw1-data.tf
+++ aws_rosey-test_eu-west-1_vpc_rosey-dev-euw1-data.tf
@@ -29,7 +29,7 @@ provider "vault" {
 }
 
 data "vault_generic_secret" "aws_infra_credential" {
-  provider = vault.eticcprod
+  provider = vault.eticloud
   path     = "secret/infra/aws/${local.aws_account_name}/terraform_admin"
 }
 
